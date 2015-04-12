4e480013186e418d3c668c300929076886f007a6
Tian <tian@dominopos.com>
2015-04-13 02:17:39 +0800

    Models for Lucky Draw tables related

---
+++ app/models/LuckyDrawReceipt.php
@@ -0,0 +1,50 @@
+<?php
+class LuckyDrawReceipt extends Eloquent
+{
+    /**
+     * LuckyDrawReceipt Model
+     *
+     * @author Tian <tian@dominopos.com>
+     */
+
+    /**
+     * Import trait ModelStatusTrait so we can use some common scope dealing
+     * with `status` field.
+     */
+    use ModelStatusTrait;
+
+    protected $table = 'lucky_draw_receipts';
+
+    protected $primaryKey = 'lucky_draw_receipt_id';
+
+    public function mall()
+    {
+        return $this->belongsTo('Retailer', 'mall_id', 'merchant_id');
+    }
+
+    public function user()
+    {
+        return $this->belongsTo('User', 'user_id', 'user_id');
+    }
+
+    public function receiptRetailer()
+    {
+        return $this->belongsTo('Retailer', 'receipt_retailer_id', 'merchant_id')->isMall('no', 'yes');
+    }
+
+    public function numbers()
+    {
+        return $this->belongsToMany('LuckyDrawNumber', 'lucky_draw_number_receipt', 'lucky_draw_number_id', 'lucky_draw_receipt_id');
+    }
+
+    public function creator()
+    {
+        return $this->belongsTo('User', 'created_by', 'user_id');
+    }
+
+    public function modifier()
+    {
+        return $this->belongsTo('User', 'modified_by', 'user_id');
+    }
+
+}
