b18ac0f5b8ec225b9de6b4b7977186d1bd70913e
Ahmad Anshori <ahmad@dominopos.com>
2015-04-14 17:58:45 +0800

    MallCI: Add login redirection if email is provided on url param

--- app/views/mobile-ci/welcome.blade.php
+++ app/views/mobile-ci/welcome.blade.php
@@ -139,6 +139,42 @@
         var pattern = new RegExp(/^((([a-z]|\d|[!#\$%&'\*\+\-\/=\?\^_`{\|}~]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])+(\.([a-z]|\d|[!#\$%&'\*\+\-\/=\?\^_`{\|}~]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])+)*)|((\x22)((((\x20|\x09)*(\x0d\x0a))?(\x20|\x09)+)?(([\x01-\x08\x0b\x0c\x0e-\x1f\x7f]|\x21|[\x23-\x5b]|[\x5d-\x7e]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(\\([\x01-\x09\x0b\x0c\x0d-\x7f]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]))))*(((\x20|\x09)*(\x0d\x0a))?(\x20|\x09)+)?(\x22)))@((([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])*([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])))\.)+(([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])*([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])))\.?$/i);
         return pattern.test(emailAddress);
       };
+
+      if(get('email')){
+        if(isValidEmailAddress(get('email').trim())){
+          //console.log('asd');
+          $.ajax({
+            method:'POST',
+            url:apiPath+'customer/login',
+            data:{
+              email: get('email').trim()
+            }
+          }).done(function(data, status, xhr){
+            if(data.status==='error'){
+              console.log(data);
+            }
+            if(data.data){
+              // console.log(data.data);
+              $.cookie('orbit_email', data.data.user_email, { expires: 5 * 365, path: '/' });
+              if(data.data.user_firstname) {
+                $.cookie('orbit_firstname', data.data.user_firstname, { expires: 5 * 365, path: '/' });
+              }
+
+              // Check if we are redirected from captive portal
+              // The query string 'from_captive' are from apache configuration
+              if (get('from_captive') == 'yes') {
+                  afterLogin(xhr);
+              } else {
+                  window.location.replace(homePath);
+              }
+            }
+          }).fail(function(data){
+            $('#errorModalText').text(data.responseJSON.message);
+            $('#errorModal').modal();
+          });
+        }
+      }
+      
       if(user_em != ''){
         $('#signedIn').show();
         $('#signIn').hide();
