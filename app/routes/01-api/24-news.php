3c416c70c0488eed30afc6a8891fb7e9cb0efefa
Tian <tian@dominopos.com>
2015-04-15 22:25:40 +0800

    Route for News controller

---
+++ app/routes/01-api/24-news.php
@@ -0,0 +1,52 @@
+<?php
+/**
+ * Routes file for News related API
+ */
+
+/**
+ * Create new news
+ */
+Route::post('/api/v1/news/new', function()
+{
+    return NewsAPIController::create()->postNewNews();
+});
+
+/**
+ * Delete news
+ */
+Route::post('/api/v1/news/delete', function()
+{
+    return NewsAPIController::create()->postDeleteNews();
+});
+
+/**
+ * Update news
+ */
+Route::post('/api/v1/news/update', function()
+{
+    return NewsAPIController::create()->postUpdateNews();
+});
+
+/**
+ * List/Search news
+ */
+Route::get('/api/v1/news/{search}', function()
+{
+    return NewsAPIController::create()->getSearchNews();
+})->where('search', '(list|search)');
+
+/**
+ * Upload news image
+ */
+Route::post('/api/v1/news-image/upload', function()
+{
+    return UploadAPIController::create()->postUploadNewsImage();
+});
+
+/**
+ * Delete news image
+ */
+Route::post('/api/v1/news-image/delete', function()
+{
+    return UploadAPIController::create()->postDeleteNewsImage();
+});
---
+++ app/routes/02-intermediate/24-news.php
@@ -0,0 +1,35 @@
+<?php
+/**
+ * Routes file for Intermediate News API
+ */
+
+/**
+ * Create new news
+ */
+Route::post('/app/v1/news/new', 'IntermediateAuthController@News_postNewNews');
+
+/**
+ * Delete news
+ */
+Route::post('/app/v1/news/delete', ['before' => 'orbit-settings', 'uses' => 'IntermediateAuthController@News_postDeleteNews']);
+
+/**
+ * Update news
+ */
+Route::post('/app/v1/news/update', 'IntermediateAuthController@News_postUpdateNews');
+
+/**
+ * List and/or Search news
+ */
+Route::get('/app/v1/news/{search}', 'IntermediateAuthController@News_getSearchNews')
+     ->where('search', '(list|search)');
+
+/**
+ * Upload news image
+ */
+Route::post('/app/v1/news-image/upload', 'IntermediateAuthController@Upload_postUploadNewsImage');
+
+/**
+ * Delete news image
+ */
+Route::post('/app/v1/news-image/delete', 'IntermediateAuthController@Upload_postDeleteNewsImage');
