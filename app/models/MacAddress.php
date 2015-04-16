77992fb0726ca4598c604988bc385e5f261a3799
Yudi Rahono <yudi@rahono.com>
2015-04-16 11:58:43 +0800

    ADD: Mac Address model and their respective table

---
+++ app/models/MacAddress.php
@@ -0,0 +1,30 @@
+<?php
+/**
+ * Created by IntelliJ IDEA.
+ * User: palanglung
+ * Date: 16/04/15
+ * Time: 11:23
+ */
+
+class MacAddress extends Eloquent {
+    /**
+     * LuckyDraw Model
+     *
+     * @author Yudi <yudi.rahono@dominopos.com>
+     */
+
+    /**
+     * Import trait ModelStatusTrait so we can use some common scope dealing
+     * with `status` field.
+     */
+    use ModelStatusTrait;
+
+    protected $table = 'mac_addresses';
+
+    protected $primaryKey = 'mac_address_id';
+
+    public function user()
+    {
+        return $this->belongsTo('User', 'user_email', 'user_email');
+    }
+}
