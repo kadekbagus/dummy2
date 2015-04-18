a157468b47d2f036a67422fb1b896b4a11d7e9b9
Rio Astamal <me@rioastamal.net>
2015-04-18 10:38:00 +0800

    Seeder LippoTenantSeeder: Add lucky draws record

--- app/database/seeds/LippoPuriTenantSeeder.php
+++ app/database/seeds/LippoPuriTenantSeeder.php
@@ -18,9 +18,13 @@ class LippoPuriTenantSeeder extends Seeder
 
         $init = <<<INIT
 TRUNCATE TABLE {$prefix}category_merchant;
-ALTER TABLE `{{$prefix}}category_merchant` AUTO_INCREMENT =1;
+ALTER TABLE {$prefix}category_merchant AUTO_INCREMENT=1;
+
 DELETE FROM {$prefix}merchants WHERE merchant_id >= 3;
-ALTER TABLE {$prefix}merchants SET AUTO_INCREMENT=3;
+ALTER TABLE {$prefix}merchants AUTO_INCREMENT=3;
+
+TRUNCATE TABLE {$prefix}lucky_draws;
+ALTER TABLE {$prefix}lucky_draws AUTO_INCREMENT=2;
 INIT;
         DB::unprepared($init);
 
@@ -229,6 +233,15 @@ CAT;
 
         $this->command->info('Seeding category_merchant table with lippo puri tenants...');
         DB::unprepared($categories);
-        $this->command->info('merchants category_merchant seeded.');
+        $this->command->info('table category_merchant seeded.');
+
+        $luckydraws = <<<LUCKY
+INSERT INTO `{$prefix}lucky_draws` (`lucky_draw_id`, `mall_id`, `lucky_draw_name`, `description`, `image`, `start_date`, `end_date`, `minimum_amount`, `grace_period_date`, `grace_period_in_days`, `min_number`, `max_number`, `status`, `created_by`, `modified_by`, `created_at`, `updated_at`) VALUES
+(1, 2, 'Lippo Mall Puri Lucky Draw', 'Lippo Mall Puri Lucky Draw.', NULL, '2015-04-01 00:00:00', '2015-04-30 23:59:59', 100000.00, NULL, 30, 100000, 200000, 'active', 0, 3, '0000-00-00 00:00:00', '2015-04-17 18:08:43');
+LUCKY;
+
+        $this->command->info('Seeding lucky_draws table...');
+        DB::unprepared($luckydraws);
+        $this->command->info('table lucky_draws seeded.');
     }
 }
