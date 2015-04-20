321bb974f1451953f60144dde25f64c47c40b628
Rio Astamal <me@rioastamal.net>
2015-04-20 23:35:12 +0800

    Sign in View: Inactive user status now shows pop up

--- app/views/mobile-ci/signin.blade.php
+++ app/views/mobile-ci/signin.blade.php
@@ -128,7 +128,7 @@
      * Get Query String from the URL
      *
      * @author Rio Astamal <me@rioastamal.net>
-     * @param string n - Name of the parameter 
+     * @param string n - Name of the parameter
      */
     function get(n) {
         var half = location.search.split(n + '=')[1];
@@ -169,7 +169,7 @@
         var pattern = new RegExp(/^((([a-z]|\d|[!#\$%&'\*\+\-\/=\?\^_`{\|}~]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])+(\.([a-z]|\d|[!#\$%&'\*\+\-\/=\?\^_`{\|}~]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])+)*)|((\x22)((((\x20|\x09)*(\x0d\x0a))?(\x20|\x09)+)?(([\x01-\x08\x0b\x0c\x0e-\x1f\x7f]|\x21|[\x23-\x5b]|[\x5d-\x7e]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(\\([\x01-\x09\x0b\x0c\x0d-\x7f]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]))))*(((\x20|\x09)*(\x0d\x0a))?(\x20|\x09)+)?(\x22)))@((([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])*([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])))\.)+(([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])*([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])))\.?$/i);
         return pattern.test(emailAddress);
       };
-      
+
       if(get('email')){
         if(isValidEmailAddress(get('email').trim())){
           //console.log('asd');
@@ -182,6 +182,8 @@
           }).done(function(data, status, xhr){
             if(data.status==='error'){
               console.log(data);
+                $('#errorModalText').text(data.message);
+                $('#errorModal').modal();
             }
             if(data.data){
               // console.log(data.data);
@@ -256,7 +258,7 @@
           $('#errorModalText').text('{{ Lang::get('mobileci.modals.email_error') }}');
           $('#errorModal').modal();
         }else{
-          
+
           if(isValidEmailAddress($('#email').val().trim())){
             $.ajax({
               method:'POST',
@@ -267,6 +269,8 @@
             }).done(function(data, status, xhr){
               if(data.status==='error'){
                 console.log(data);
+                $('#errorModalText').text(data.message);
+                $('#errorModal').modal();
               }
               if(data.data){
                 // console.log(data.data);
