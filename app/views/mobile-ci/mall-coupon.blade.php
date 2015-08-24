035d4e647e3e6a7ae0b1714bfe5272c73c2cec4b
Ahmad Anshori <ahmad@dominopos.com>
2015-08-24 17:00:07 +0800

    Fix hide coupon value

--- app/views/mobile-ci/mall-coupon.blade.php
+++ app/views/mobile-ci/mall-coupon.blade.php
@@ -66,7 +66,7 @@
     </div>
     
     <div class="col-xs-12 main-theme-mall product-detail where">
-        @if($product->couponRule->discount_value)
+        @if(! empty((float) $product->couponRule->discount_value))
         <div class="row">
             <div class="col-xs-12 text-center">
                 <h4>Coupon Value</h4>
