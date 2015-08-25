3d19bdc716cf95950a2483d3615819eeb40272c2
Irianto Pratama <irianto@dominopos.com>
2015-08-25 21:43:18 +0800

    Alter table language add name long and index with seeder

--- app/database/seeds/LanguageSeeder.php
+++ app/database/seeds/LanguageSeeder.php
@@ -13,10 +13,17 @@ class LanguageSeeder extends Seeder
             DB::table('languages')->truncate();
         } catch (Illuminate\Database\QueryException $e) {
         }
-        $languages = ['en', 'zh', 'ja'];
-        foreach ($languages as $language_name) {
+
+        $languages = [
+            'en' => 'English', 
+            'zh' => 'Chinese', 
+            'ja' => 'Japan'
+        ];
+
+        foreach ($languages as $code=>$language_name) {
             $language = new Language();
-            $language->name = $language_name;
+            $language->name = $code;
+            $language->name_long = $language_name;
             $language->save();
         }
     }
