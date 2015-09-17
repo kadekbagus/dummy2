166ccf8ea5d4fcb805e661021b5e8f30c01c17fb
William Shallum <william@dominopos.com>
2015-09-17 15:17:54 +0800

    OM-161: fix heading in HTML print view

--- app/views/printer/list-captiveportalreport-view.php
+++ app/views/printer/list-captiveportalreport-view.php
@@ -105,7 +105,7 @@
 </div>
 
 <div id="main">
-    <h2 style="margin-bottom:0.5em;">Captive Portal Report</h2>
+    <h2 style="margin-bottom:0.5em;"><?php echo htmlspecialchars($pageTitle); ?></h2>
     <table style="width:100%; margin-bottom:1em;" class="noborder">
         <tr>
             <td style="width:150px"></td>
