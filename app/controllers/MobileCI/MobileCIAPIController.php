9b3af4420118c47d1c7f38af26afe69a12d610f0
William Shallum <william@dominopos.com>
2015-09-23 19:10:40 +0800

    mobile ci home: do not put view data in a single line

--- app/controllers/MobileCI/MobileCIAPIController.php
+++ app/controllers/MobileCI/MobileCIAPIController.php
@@ -274,7 +274,16 @@ class MobileCIAPIController extends ControllerAPI
                 ->responseOK()
                 ->save();
 
-            return View::make('mobile-ci.home', array('page_title'=>Lang::get('mobileci.page_title.home'), 'retailer' => $retailer, 'events' => $events, 'event_families' => $event_families, 'event_family_url_param' => $event_family_url_param, 'widgets' => $widgets, 'widget_flags' => $widget_flags))->withCookie($event_store);
+            $data = array(
+                'page_title' => Lang::get('mobileci.page_title.home'),
+                'retailer' => $retailer,
+                'events' => $events,
+                'event_families' => $event_families,
+                'event_family_url_param' => $event_family_url_param,
+                'widgets' => $widgets,
+                'widget_flags' => $widget_flags
+            );
+            return View::make('mobile-ci.home', $data)->withCookie($event_store);
         } catch (Exception $e) {
             $activityPageNotes = sprintf('Failed to view Page: %s', 'Home');
             $activityPage->setUser($user)
