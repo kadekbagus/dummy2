58e33430044cbd36db6c3bb4f0b5599dcaf33722
Firmansyah <firmansyah@dominopos.com>
2015-09-30 15:39:29 +0800

    Fix upload image for add tenant OM-177

--- app/events/enabled/08-tenant.php
+++ app/events/enabled/08-tenant.php
@@ -6,6 +6,83 @@
  */
 use OrbitShop\API\v1\Helper\Input as OrbitInput;
 
+
+/**
+ * Listen on:    `orbit.tenant.postnewtenant.after.save`
+ * Purpose:      Handle file upload on tenant creation
+ *
+ * @author Tian <tian@dominopos.com>
+ *
+ * @param EventAPIController $controller - The instance of the EventAPIController or its subclass
+ * @param Event $tenant - Instance of object Event
+ */
+Event::listen('orbit.tenant.postnewtenant.after.save', function($controller, $tenant)
+{
+    $logo = OrbitInput::files('logo');
+
+    if (! empty($logo)) {
+        $_POST['merchant_id'] = $tenant->merchant_id;
+
+        // This will be used on UploadAPIController
+        App::instance('orbit.upload.user', $controller->api->user);
+
+        $response = UploadAPIController::create('raw')
+                                       ->setCalledFrom('tenant.update')
+                                       ->postUploadTenantLogo();
+
+        if ($response->code !== 0)
+        {
+            throw new \Exception($response->message, $response->code);
+        }
+    }
+    $tenant->load('mediaLogo');
+
+    $maps = OrbitInput::files('maps');
+
+    if (! empty($maps)) {
+        $_POST['merchant_id'] = $tenant->merchant_id;
+
+        // This will be used on UploadAPIController
+        App::instance('orbit.upload.user', $controller->api->user);
+
+        $response = UploadAPIController::create('raw')
+                                       ->setCalledFrom('tenant.update')
+                                       ->postUploadTenantMap();
+
+        if ($response->code !== 0)
+        {
+            throw new \Exception($response->message, $response->code);
+        }
+
+        $tenant->setRelation('media_map', $response->data);
+        $tenant->media_map = $response->data;
+    }
+    $tenant->load('mediaMap');
+
+    $pictures = OrbitInput::files('pictures');
+
+    if (! empty($pictures)) {
+        $_POST['merchant_id'] = $tenant->merchant_id;
+
+        // This will be used on UploadAPIController
+        App::instance('orbit.upload.user', $controller->api->user);
+
+        $response = UploadAPIController::create('raw')
+                                       ->setCalledFrom('tenant.update')
+                                       ->postUploadTenantImage();
+
+        if ($response->code !== 0)
+        {
+            throw new \Exception($response->message, $response->code);
+        }
+
+        $tenant->setRelation('media_image', $response->data);
+        $tenant->media_image = $response->data;
+    }
+    $tenant->load('mediaImage');
+});
+
+
 /**
  * Listen on:       `orbit.tenant.postupdatetenant.after.save`
  *   Purpose:       Handle file upload on tenant update
