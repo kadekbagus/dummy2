9c426ae4b627def8df23ddd406cd198229b58416
Firmansyah <firmansyah@dominopos.com>
2015-10-09 18:35:30 +0800

    Fix migrate UUID encodedId widget translations and setting translations

--- app/database/migrations/2015_10_06_040030_create_table_widget_translations.php
+++ app/database/migrations/2015_10_06_040030_create_table_widget_translations.php
@@ -5,37 +5,42 @@ use Illuminate\Database\Migrations\Migration;
 
 class CreateTableWidgetTranslations extends Migration {
 
-	/**
-	 * Run the migrations.
-	 *
-	 * @return void
-	 */
-	public function up()
-	{
-        Schema::create('widget_translations', function (Blueprint $table) {
-            $table->char('widget_translation_id',16);
-            $table->char('widget_id',16);
-            $table->char('merchant_language_id',16);
-            $table->string('widget_slogan', 255)->nullable();
-            $table->string('status', 15)->default('active');
-            $table->timestamps();
-            $table->index(['widget_id', 'merchant_language_id'], 'widget_language_idx');
-            $table->index(['status'], 'status_idx');
-            $table->char('created_by',16)->nullable();
-            $table->char('modified_by',16)->nullable();
-            $table->index(['created_by'], 'created_by_idx');
-            $table->index(['modified_by'], 'modified_by_idx');            
-        });		
-	}
+  /**
+   * Run the migrations.
+   *
+   * @return void
+   */
+  public function up()
+  {
+    $builder = DB::connection()->getSchemaBuilder();
+    $builder->blueprintResolver(function ($table, $callback) {
+      return new OrbitBlueprint($table, $callback);
+    });
+    $builder->create('widget_translations', function(OrbitBlueprint $table)
+    {
+      $table->encodedId('widget_translation_id');
+      $table->encodedId('widget_id');
+      $table->encodedId('merchant_language_id');
+      $table->string('widget_slogan', 255)->nullable();
+      $table->string('status', 15)->default('active');
+      $table->timestamps();
+      $table->index(['widget_id', 'merchant_language_id'], 'widget_language_idx');
+      $table->index(['status'], 'status_idx');
+      $table->encodedId('created_by')->nullable();
+      $table->encodedId('modified_by')->nullable();
+      $table->index(['created_by'], 'created_by_idx');
+      $table->index(['modified_by'], 'modified_by_idx');
+    });
+  }
 
-	/**
-	 * Reverse the migrations.
-	 *
-	 * @return void
-	 */
-	public function down()
-	{
-		Schema::drop('widget_translations');
-	}
+  /**
+   * Reverse the migrations.
+   *
+   * @return void
+   */
+  public function down()
+  {
+    Schema::drop('widget_translations');
+  }
 
 }
