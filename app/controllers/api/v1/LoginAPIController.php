f9abfc2bbfb06ec489c0191f8aa915077155cab4
Irianto Pratama <irianto@dominopos.com>
2015-10-20 19:11:24 +0800

    Fix OM-498 error another login

--- app/controllers/api/v1/LoginAPIController.php
+++ app/controllers/api/v1/LoginAPIController.php
@@ -499,6 +499,7 @@ class LoginAPIController extends ControllerAPI
      *
      * List of API Parameters
      * ----------------------
+     * @param string    `token`          (required) - Token to be check
      * @param string    `first_name`     (required) - value of first name
      * @param string    `last_name`      (required) - value of last name
      * @return Illuminate\Support\Facades\Response
@@ -510,19 +511,28 @@ class LoginAPIController extends ControllerAPI
         try {
             $this->registerCustomValidation();
 
+            $tokenValue = trim(OrbitInput::post('token'));
             $first_name = OrbitInput::post('first_name');
             $last_name = OrbitInput::post('last_name');
 
             $validator = Validator::make(
                 array(
-                    'first_name'      => $first_name,
-                    'last_name' => $last_name
+                    'token_value'   => $tokenValue,
+                    'first_name'    => $first_name,
+                    'last_name'     => $last_name
                 ),
                 array(
-                    'first_name'      => 'required|min:3',
-                    'last_name'      => 'required|min:3',
+                    'token_value'   => 'required|orbit.empty.token',
+                    'first_name'    => 'required|min:3',
+                    'last_name'     => 'required|min:3',
                 )
             );
+
+            $token = App::make('orbit.empty.token');
+            $user = User::with('userdetail')
+                        ->excludeDeleted()
+                        ->where('user_id', $token->user_id)
+                        ->first();
 
             // Run the validation
             if ($validator->fails()) {
@@ -533,7 +543,15 @@ class LoginAPIController extends ControllerAPI
             // Begin database transaction
             $this->beginTransaction();
 
-            $
+            // update the token status so it cannot be use again
+            $token->status = 'deleted';
+            $token->save();
+
+            // Update user password and activate them
+            $user->user_password = Hash::make($password);
+            $user->status = 'active';
+            $user->save();
+
             $this->response->message = Lang::get('statuses.orbit.updated.your_password');
             $this->response->data = $user;
 
@@ -778,19 +796,21 @@ class LoginAPIController extends ControllerAPI
 
             $this->response->data = $user;
 
-            // @author Irianto Pratama <irianto@dominopos.com>
-            $agreement_accepted = $mall->settings()
-                                       ->where('setting_name', 'agreement_accepted')
-                                       ->where('setting_value', 'true')
-                                       ->where('object_id', $mall->merchant_id)
-                                       ->where('object_type', 'merchant')
-                                       ->first();
+            if ($from === 'mall') {
+                // @author Irianto Pratama <irianto@dominopos.com>
+                $agreement_accepted = $mall->settings()
+                                           ->where('setting_name', 'agreement_accepted')
+                                           ->where('setting_value', 'true')
+                                           ->where('object_id', $mall->merchant_id)
+                                           ->where('object_type', 'merchant')
+                                           ->first();
 
-            if (empty($agreement_accepted) || $agreement_accepted->setting_value !== 'true') {
-                $this->response->code = 302;
-                $this->response->status = 'redirect';
-                $this->response->message = Lang::get('validation.orbit.access.agreement');
-                $this->response->data = Config::get('orbit.agreement.url');
+                if (empty($agreement_accepted) || $agreement_accepted->setting_value !== 'true') {
+                    $this->response->code = 302;
+                    $this->response->status = 'redirect';
+                    $this->response->message = Lang::get('validation.orbit.access.agreement');
+                    $this->response->data = Config::get('orbit.agreement.url');
+                }
             }
         } catch (ACLForbiddenException $e) {
             $this->response->code = $e->getCode();
