7a491815fe21f7d8257fec9d3246cc03ac9419fd
kadekbagus <kadek@dominopos.com>
2015-10-21 16:35:55 +0800

    fix image not showing on list coupon,news,promo OM-549

--- app/views/mobile-ci/mall-coupon-list.blade.php
+++ app/views/mobile-ci/mall-coupon-list.blade.php
@@ -14,7 +14,7 @@
                             @if(!empty($coupon->promo_image))
                             <a href="{{ asset($coupon->promo_image) }}" data-featherlight="image" class="text-left"><img class="img-responsive" alt="" src="{{ asset($coupon->promo_image) }}"></a>
                             @else
-                            <a class="img-responsive" src="{{ asset('mobile-ci/images/default_product.png') }}"></a>
+                            <img class="img-responsive" src="{{ asset('mobile-ci/images/default_product.png') }}"/>
                             @endif
                         </div>
                         <div class="col-xs-6">
