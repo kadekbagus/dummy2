73607371cc4d073bc0c9851901cb8f9f78de8d44
Firmansyah <firmansyah@dominopos.com>
2015-10-23 15:12:00 +0800

    Fixing conflict signin

--- app/controllers/MobileCI/MobileCIAPIController.php
+++ app/controllers/MobileCI/MobileCIAPIController.php
@@ -426,13 +426,15 @@ class MobileCIAPIController extends ControllerAPI
      *
      * @author Ahmad Anshori <ahmad@dominopos.com>
      * @author Rio Astamal <me@rioastamal.net>
+     * @author Irianto Pratama <irianto@dominopos.com>
      *
      * @return \Illuminate\View\View
      */
     public function getSignInView()
     {
         $bg = null;
-        $login_start_button = Config::get('shop.start_button_label');
+        $start_button_label = Config::get('shop.start_button_label');
+
         if (\Input::get('payload')) {
             // has payload, clear out prev cookies
             $_COOKIE['orbit_firstname'] = '';
@@ -484,7 +486,6 @@ class MobileCIAPIController extends ControllerAPI
             } catch (Exception $e) {
             }
 
-            //dd($merchant_language_id);
             $mall = Mall::with(array('settings.translations' => function($q) use ($merchant_language_id) {
                     $q->where('setting_translations.merchant_language_id', $merchant_language_id);
                 }))->where('merchant_id', $retailer->merchant_id)
@@ -525,7 +526,6 @@ class MobileCIAPIController extends ControllerAPI
 
                 return Redirect::to($this->addParamsToUrl($landing_url, $internet_info));
             }
-// dd($start_button_label);
 
             $viewData = array_merge($viewData, array(
                 'retailer' => $retailer,
@@ -534,7 +534,7 @@ class MobileCIAPIController extends ControllerAPI
                 'landing_url' => $this->addParamsToUrl($landing_url, $internet_info),
                 'display_name' => $display_name,
                 'languages' => $languages,
-                'login_start_button' => $start_button_label,
+                'start_button_login' => $start_button_label,
             ));
         } catch (Exception $e) {
             $retailer = $this->getRetailerInfo();
@@ -548,7 +548,7 @@ class MobileCIAPIController extends ControllerAPI
                 'landing_url' => $this->addParamsToUrl($landing_url, $internet_info),
                 'display_name' => $display_name,
                 'languages' => $languages,
-                'login_start_button' => $start_button_label,
+                'start_button_login' => $start_button_label,
             ));
         }
 
