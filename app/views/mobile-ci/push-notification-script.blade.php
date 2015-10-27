d7328fb3f4c56f0688dbcfa6cb3b161be5b2ebaf
Ahmad <ahmad@Dominopos.com>
2015-10-27 14:54:32 +0800

    Change popup layout for inbox, using language placeholder

--- app/views/mobile-ci/push-notification-script.blade.php
+++ app/views/mobile-ci/push-notification-script.blade.php
@@ -5,12 +5,33 @@
 <script>
 
     $(document).ready(function() {
-        var txt_congrats = '{{ Lang::get('mobileci.coupon.congratulations_you_get') }}';
-        var txt_coupons = '{{ Lang::get('mobileci.coupon.here_are_your_coupons') }}';
-        var txt_coupon = '{{ Lang::get('mobileci.coupon.here_is_your_coupon') }}';
-        var txt_check = '{{ Lang::get('mobileci.coupon.check_coupon') }}';
-        var txt_happy = '{{ Lang::get('mobileci.coupon.happy_shopping') }}';
-        var txt_close = '{{ Lang::get('mobileci.coupon.close') }}';
+        var langs = {};
+        langs.coupon = {};
+        langs.lucky_draw = {};
+        langs = {
+            coupon : {
+                txt_subject: '{{ Lang::get('mobileci.inbox.coupon.subject') }}',
+                txt_hello: '{{ Lang::get('mobileci.lucky_draw.hello') }}',
+                txt_congrats: '{{ Lang::get('mobileci.coupon.congratulations_you_get') }}',
+                txt_coupons: '{{ Lang::get('mobileci.coupon.here_are_your_coupons') }}',
+                txt_coupon: '{{ Lang::get('mobileci.coupon.here_is_your_coupon') }}',
+                txt_check: '{{ Lang::get('mobileci.coupon.check_coupon') }}',
+                txt_happy: '{{ Lang::get('mobileci.coupon.happy_shopping') }}',
+                txt_close: '{{ Lang::get('mobileci.coupon.close') }}'
+            },
+            lucky_draw : {
+                txt_subject: '{{ Lang::get('mobileci.inbox.lucky_draw.subject') }}',
+                txt_congrats: '{{ Lang::get('mobileci.lucky_draw.congratulation') }}',
+                txt_no_lucky_draw: '{{ Lang::get('mobileci.lucky_draw.no_lucky_draw') }}',
+                txt_lucky_draw_info_1: '{{ Lang::get('mobileci.lucky_draw.lucky_draw_info_1') }}',
+                txt_lucky_draw_info_2: '{{ Lang::get('mobileci.lucky_draw.lucky_draw_info_2') }}',
+                txt_lucky_draw_info_3: '{{ Lang::get('mobileci.lucky_draw.lucky_draw_info_3') }}',
+                txt_lucky_draw_info_4: '{{ Lang::get('mobileci.lucky_draw.lucky_draw_info_4') }}',
+                txt_lucky_draw_info_5: '{{ Lang::get('mobileci.lucky_draw.lucky_draw_info_5') }}',
+                txt_lucky_draw: '{{ Lang::get('mobileci.lucky_draw.lucky_draw') }}',
+                txt_goodluck: '{{ Lang::get('mobileci.lucky_draw.goodluck') }}'
+            }
+        };
 
         var pushNotificationDelay = 1000 * {{ Config::get('orbit.shop.poll_interval', 5) }}
         // Flag to see whether this notification is viewing by user
@@ -57,13 +78,31 @@
                         orbitIsViewing = true;
 
                         // Show the notification to the user
-                        $('#orbit-push-notification-wrapper').html(notif.content);
+                        var new_content = notif.content;
 
-                        $('#orbit-push-notification-wrapper').replaceText('|#|congratulations_you_get|#|', txt_congrats);
-                        $('#orbit-push-notification-wrapper').replaceText('|#|here_are_your_coupons|#|', txt_coupons);
-                        $('#orbit-push-notification-wrapper').replaceText('|#|check_coupon|#|', txt_check);
-                        $('#orbit-push-notification-wrapper').replaceText('|#|happy_shopping|#|', txt_happy);
-                        $('#orbit-push-notification-wrapper').replaceText('|#|close|#|', txt_close);
+                        // replace text placeholder for coupon popup
+                        new_content = new_content.replace('|#|hello|#|', langs.coupon.hello);
+                        new_content = new_content.replace('|#|coupon_subject|#|', langs.coupon.txt_subject);
+                        new_content = new_content.replace('|#|congratulations_you_get|#|', langs.coupon.txt_congrats);
+                        new_content = new_content.replace('|#|here_are_your_coupons|#|', langs.coupon.txt_coupons);
+                        new_content = new_content.replace('|#|here_is_your_coupon|#|', langs.coupon.txt_coupons);
+                        new_content = new_content.replace('|#|check_coupon|#|', langs.coupon.txt_check);
+                        new_content = new_content.replace('|#|happy_shopping|#|', langs.coupon.txt_happy);
+                        new_content = new_content.replace('|#|close|#|', langs.coupon.txt_close);
+
+                        // replace text placeholder for lucky draw popup
+                        new_content = new_content.replace('|#|lucky_draw_subject|#|', langs.lucky_draw.txt_subject);
+                        new_content = new_content.replace('|#|ld_congratulations_you_get|#|', langs.lucky_draw.txt_congrats);
+                        new_content = new_content.replace('|#|no_lucky_draw|#|', langs.lucky_draw.txt_no_lucky_draw);
+                        new_content = new_content.replace('|#|lucky_draw_info_1|#|', langs.lucky_draw.txt_lucky_draw_info_1);
+                        new_content = new_content.replace('|#|lucky_draw_info_2|#|', langs.lucky_draw.txt_lucky_draw_info_2);
+                        new_content = new_content.replace('|#|lucky_draw_info_3|#|', langs.lucky_draw.txt_lucky_draw_info_3);
+                        new_content = new_content.replace('|#|lucky_draw_info_4|#|', langs.lucky_draw.txt_lucky_draw_info_4);
+                        new_content = new_content.replace('|#|lucky_draw_info_5|#|', langs.lucky_draw.txt_lucky_draw_info_5);
+                        new_content = new_content.replace('|#|lucky_draw|#|', langs.lucky_draw.txt_lucky_draw);
+                        new_content = new_content.replace('|#|goodluck|#|', langs.lucky_draw.txt_goodluck);
+
+                        $('#orbit-push-notification-wrapper').html(new_content);
 
                         // Fire event when the pop up closed
                         $('#orbit-push-modal-' + notif.inbox_id).on('hidden.bs.modal', function(e) {
