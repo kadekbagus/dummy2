9661ef2009e454d10b65f91a1c2a4a8f2633e47e
Firmansyah <firmansyah@dominopos.com>
2015-10-28 19:03:20 +0800

    Fix list customer per mall on cs customer

--- app/controllers/api/v1/UserAPIController.php
+++ app/controllers/api/v1/UserAPIController.php
@@ -1484,12 +1484,19 @@ class UserAPIController extends ControllerAPI
                 $users->select('users.*');
             }
 
-            // join to user_acquisitions if the request come from mall portal
+            // join to activities for view user login any mall in ci
             $from_cs = OrbitInput::get('from_cs');
-            if(empty($from_cs)) {
-                $users->join('user_acquisitions', 'user_acquisitions.user_id', '=', 'users.user_id');
+            if(!empty($from_cs)) {
+                $users->join('activities', 'activities.user_id', '=', 'users.user_id')
+                      ->where('activities.activity_name', 'login_ok')
+                      ->where('activities.activity_name_long', 'Sign In')
+                      ->where('activities.activity_type', 'login')
+                      ->where('activities.role', 'Consumer')
+                      ->where('activities.group', 'mobile-ci')
+                      ->groupBy('activities.user_email')
+                      ->groupBy('activities.location_id');
                 OrbitInput::get('merchant_id', function($merchantIds) use ($users) {
-                    $users->whereIn('user_acquisitions.acquirer_id', $merchantIds);
+                    $users->whereIn('activities.location_id', $merchantIds);
                 });
             }
 
