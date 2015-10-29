3494f6ea1c69720e96efb866148f11333e7aa7cf
Ahmad <ahmad@Dominopos.com>
2015-10-29 21:21:25 +0800

    Fix selected language on dropdown if the browser lang is not supported

--- app/views/mobile-ci/commonscripts.blade.php
+++ app/views/mobile-ci/commonscripts.blade.php
@@ -77,7 +77,7 @@
                     <select class="form-control" name="lang" id="selected-lang">
                         @if (isset($languages))
                                 @foreach ($languages as $lang)
-                                    <option value="{{{ $lang->language->name }}}" @if (isset($_COOKIE['orbit_preferred_language'])) @if ($lang->language->name === $_COOKIE['orbit_preferred_language']) selected @endif @endif>{{{ $lang->language->name_long }}} @if($lang->language->name === $default_lang) (Default) @endif</option>
+                                    <option value="{{{ $lang->language->name }}}" @if (isset($_COOKIE['orbit_preferred_language'])) @if ($lang->language->name === $_COOKIE['orbit_preferred_language']) selected @endif @else @if($lang->language->name === $default_lang) selected @endif @endif>{{{ $lang->language->name_long }}} @if($lang->language->name === $default_lang) (Default) @endif</option>
                                 @endforeach
                         @endif
                     </select>
