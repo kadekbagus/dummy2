b3651cceae16d60810e02045cab33705afb34b80
Ahmad <ahmad@Dominopos.com>
2015-11-11 11:35:11 +0800

    Add default png icon because apparently firefox does not recognize .ico on add to homescreen

--- app/views/mobile-ci/head.blade.php
+++ app/views/mobile-ci/head.blade.php
@@ -9,13 +9,14 @@
 <link rel="apple-touch-icon-precomposed" sizes="72x72" href="{{ asset($this_mall->mediaIcon->path) }}" />
 <link rel="apple-touch-icon-precomposed" sizes="114x114" href="{{ asset($this_mall->mediaIcon->path) }}" />
 <link rel="apple-touch-icon-precomposed" sizes="144x144" href="{{ asset($this_mall->mediaIcon->path) }}" />
+<link rel="icon" type="image/ico" href="{{ asset($this_mall->mediaIcon->path) }}"/>
 @else
-<link rel="apple-touch-icon-precomposed" sizes="57x57" href="{{ asset('mobile-ci/images/favicon.ico') }}" />
-<link rel="apple-touch-icon-precomposed" sizes="72x72" href="{{ asset('mobile-ci/images/favicon.ico') }}" />
-<link rel="apple-touch-icon-precomposed" sizes="114x114" href="{{ asset('mobile-ci/images/favicon.ico') }}" />
-<link rel="apple-touch-icon-precomposed" sizes="144x144" href="{{ asset('mobile-ci/images/favicon.ico') }}" />
+<link rel="apple-touch-icon-precomposed" sizes="57x57" href="{{ asset('mobile-ci/images/orbit-icon-default.png') }}" />
+<link rel="apple-touch-icon-precomposed" sizes="72x72" href="{{ asset('mobile-ci/images/orbit-icon-default.png') }}" />
+<link rel="apple-touch-icon-precomposed" sizes="114x114" href="{{ asset('mobile-ci/images/orbit-icon-default.png') }}" />
+<link rel="apple-touch-icon-precomposed" sizes="144x144" href="{{ asset('mobile-ci/images/orbit-icon-default.png') }}" />
+<link rel="icon" type="image/ico" href="{{ asset('mobile-ci/images/orbit-icon-default.png') }}"/>
 @endif
-<link rel="shortcut icon" type="image/ico" href="{{ asset('mobile-ci/images/favicon.ico') }}"/>
 
 {{ HTML::style('mobile-ci/stylesheet/bootstrap-tour.min.css') }}
 {{ HTML::style('mobile-ci/stylesheet/' . Orbit\Helper\Asset\Stylesheet::create()->getMallCss()) }}
