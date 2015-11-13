c1f39b2b95aec59dd66d174d6fbfe6af3299aa99
Irianto Pratama <irianto@dominopos.com>
2015-11-13 11:02:15 +0800

    Fix duplicate email if sign up via cs

--- app/controllers/api/v1/LoginAPIController.php
+++ app/controllers/api/v1/LoginAPIController.php
@@ -249,6 +249,12 @@ class LoginAPIController extends ControllerAPI
                 $activity = Activity::csportal()
                                     ->setActivityType('registration');
             } else {
+                 // Send email process to the queue
+                 \Queue::push('Orbit\\Queue\\RegistrationMail', [
+                     'user_id' => $newuser->user_id,
+                     'merchant_id' => $mall_id
+                 ]);
+
                 $activity = Activity::mobileci()
                     ->setActivityType('registration');
             }
@@ -260,12 +266,6 @@ class LoginAPIController extends ControllerAPI
                      ->setModuleName('Application')
                      ->setLocation($mall)
                      ->responseOK();
-
-             // Send email process to the queue
-             \Queue::push('Orbit\\Queue\\RegistrationMail', [
-                 'user_id' => $newuser->user_id,
-                 'merchant_id' => $mall_id
-             ]);
 
         } catch (ACLForbiddenException $e) {
             $this->response->code = $e->getCode();
