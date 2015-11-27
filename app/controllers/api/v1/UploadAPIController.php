7f881a4bbf4e3e056aac7632e8e3833d680ea782
Tian <tian@dominopos.com>
2015-11-27 14:37:42 +0800

    Fix postDeleteMembershipImage to using mall_id

--- app/controllers/api/v1/UploadAPIController.php
+++ app/controllers/api/v1/UploadAPIController.php
@@ -6645,14 +6645,14 @@ class UploadAPIController extends ControllerAPI
             $this->registerCustomValidation();
 
             // Application input
-            $membership_id = OrbitInput::post('membership_id');
+            $mallId = OrbitInput::post('mall_id');
 
             $validator = Validator::make(
                 array(
-                    'membership_id'   => $membership_id,
+                    'mall_id'   => $mallId,
                 ),
                 array(
-                    'membership_id'   => 'required|orbit.empty.membership',
+                    'mall_id'   => 'required|orbit.empty.mall',
                 )
             );
 
@@ -6670,9 +6670,32 @@ class UploadAPIController extends ControllerAPI
             }
             Event::fire('orbit.upload.postdeletemembershipimage.after.validation', array($this, $validator));
 
-            // We already had Membership instance on the RegisterCustomValidation
-            // get it from there no need to re-query the database
-            $membership = App::make('orbit.empty.membership');
+            /*
+             * Get membership card id based on mall id
+             */
+            // get user mall_ids
+            $listOfMallIds = $user->getUserMallIds($mallId);
+            if (empty($listOfMallIds)) { // invalid mall id
+                $errorMessage = 'Invalid mall id.';
+                OrbitShopAPI::throwInvalidArgument($errorMessage);
+            }
+
+            $membershipCard = Membership::active()
+                                        ->whereIn('merchant_id', $listOfMallIds)
+                                        ->first();
+
+            // create membership card if not exists
+            if (empty($membershipCard)) {
+                // create
+                $membershipCard = new Membership();
+                $membershipCard->merchant_id = $mallId;
+                $membershipCard->membership_name = 'Standard Card';
+                $membershipCard->status = 'active';
+                $membershipCard->created_by = $user->user_id;
+                $membershipCard->save();
+            }
+
+            $membership = $membershipCard;
 
             // Delete old membership image
             $pastMedia = Media::where('object_id', $membership->membership_id)
