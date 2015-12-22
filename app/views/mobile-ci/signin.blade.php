4a208433fa3111ff4dbdd1b4c652191513c11f7b
Rio Astamal <rio@dominopos.com>
2015-12-22 09:06:37 +0800

    Switch form to sign up and and sign in

--- app/views/mobile-ci/signin.blade.php
+++ app/views/mobile-ci/signin.blade.php
@@ -211,7 +211,7 @@
                     </div>
                     <div class="form-group">
                         <button style="background-color:#3B5998;" type="submit"
-                                class="btn btn-info btn-block submit-btn" id="btn-login-form-google"><i 
+                                class="btn btn-info btn-block submit-btn" id="btn-login-form-google"><i
                                 class="fa fa-google-plus"></i> {{{ trans('mobileci.signin.login_via_google') }}}
                         </button>
                     </div>
@@ -573,11 +573,6 @@
             var em;
             var user_em = '{{ strtolower($user_email) }}';
 
-            function isValidEmailAddress(emailAddress) {
-                var pattern = new
-                        RegExp(/^((([a-z]|\d|[!#\$%&'\*\+\-\/=\?\^_`{\|}~]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])+(\.([a-z]|\d|[!#\$%&'\*\+\-\/=\?\^_`{\|}~]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])+)*)|((\x22)((((\x20|\x09)*(\x0d\x0a))?(\x20|\x09)+)?(([\x01-\x08\x0b\x0c\x0e-\x1f\x7f]|\x21|[\x23-\x5b]|[\x5d-\x7e]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(\\([\x01-\x09\x0b\x0c\x0d-\x7f]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]))))*(((\x20|\x09)*(\x0d\x0a))?(\x20|\x09)+)?(\x22)))@((([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])*([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])))\.)+(([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])*([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])))\.?$/i);
-                return pattern.test(emailAddress);
-            };
             if (user_em != '') {
                 $('#signedIn').show();
                 $('#signIn').hide();
