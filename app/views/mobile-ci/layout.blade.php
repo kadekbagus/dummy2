70eca794ace601a668896903398b51834414641a
Ahmad <ahmad@dominopos.com>
2016-01-22 19:01:51 +0800

    Add ubuntu font from config

--- app/views/mobile-ci/layout.blade.php
+++ app/views/mobile-ci/layout.blade.php
@@ -4,7 +4,19 @@
         @include('mobile-ci.head')
         {{ HTML::style('mobile-ci/stylesheet/jquery-ui.min.css') }}
         {{ HTML::style('mobile-ci/stylesheet/lightslider.min.css') }}
-        <link href='https://fonts.googleapis.com/css?family=Ubuntu:400,700' rel='stylesheet' type='text/css'>
+        @if (! empty(Config::get('orbit.cdn.fonts.ubuntu')))
+        <link href="{{Config::get('orbit.cdn.fonts.ubuntu')}}" rel="stylesheet" type="text/css">
+        @else 
+        <style type="text/css">
+            @font-face {
+              font-family: 'Ubuntu';
+              font-style: normal;
+              font-weight: 400;
+              src: url("{{asset('mobile-ci/fonts/ubuntu-latin.woff2')}}") format('woff2'), url("{{asset('mobile-ci/fonts/ubuntu-latin.ttf')}}") format('truetype');
+              unicode-range: U+0000-00FF, U+0131, U+0152-0153, U+02C6, U+02DA, U+02DC, U+2000-206F, U+2074, U+20AC, U+2212, U+2215, U+E0FF, U+EFFD, U+F000;
+            }
+        </style>
+        @endif
         @yield('ext_style')
     </head>
     <body>
