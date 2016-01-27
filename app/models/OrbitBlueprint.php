6eea6329e33f90b322c0df547f8e50df14d3ec19
Rio Astamal <rio@dominopos.com>
2016-01-27 20:53:10 +0800

    OrbitBlueprint: Add second argument on encodedId()

--- app/models/OrbitBlueprint.php
+++ app/models/OrbitBlueprint.php
@@ -5,8 +5,8 @@
  */
 class OrbitBlueprint extends \Illuminate\Database\Schema\Blueprint
 {
-    public function encodedId($column)
+    public function encodedId($column, $length=16)
     {
-        return $this->addColumn('char', $column, ['length' => 16, 'character_set' => 'ascii', 'collation' => 'ascii_bin']);
+        return $this->addColumn('char', $column, ['length' => $length, 'character_set' => 'ascii', 'collation' => 'ascii_bin']);
     }
 }
