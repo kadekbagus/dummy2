8505142a8b287b53b2d09771ff2573bce79b005c
Ahmad <ahmad@dominopos.com>
2016-02-05 19:11:16 +0800

    Change method name from getPollAlert to getPollMessages

--- app/routes/03-mobile-ci/01-login.php
+++ app/routes/03-mobile-ci/01-login.php
@@ -566,7 +566,7 @@ Route::group(
          */
         Route::get('/app/v1/inbox/unread-count', function()
         {
-            return InboxAPIController::create()->getPollAlert();
+            return InboxAPIController::create()->getPollMessages();
         });
 
         /**
