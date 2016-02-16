aec5baef7792e8e5612ee2de778c5decf3f053d8
shelgi <shelgi@dominopos.com>
2016-02-16 14:16:18 +0800

    fix bug on coupon search by name

--- app/controllers/api/v1/CouponAPIController.php
+++ app/controllers/api/v1/CouponAPIController.php
@@ -2154,10 +2154,10 @@ class CouponAPIController extends ControllerAPI
             // Filter coupon by matching promotion name pattern
             OrbitInput::get('promotion_name_like', function($promotionName) use ($coupons)
             {
-                $coupons->leftJoin('promotion_translations', 'promotion_translations.promotion_id', '=', 'promotions.promotion_id')
-                    ->leftJoin('merchant_languages', 'merchant_languages.merchant_language_id', '=', 'promotion_translations.merchant_language_id')
+                $coupons->leftJoin('coupon_translations', 'coupon_translations.promotion_id', '=', 'promotions.promotion_id')
+                    ->leftJoin('merchant_languages', 'merchant_languages.merchant_language_id', '=', 'coupon_translations.merchant_language_id')
                     ->leftJoin('languages', 'languages.language_id', '=', 'merchant_languages.language_id')
-                    ->where('promotions.promotion_name', 'like', "%$promotionName%")
+                    ->where('coupon_translations.promotion_name', 'like', "%$promotionName%")
                     ->where('languages.name', '=', 'en');
             });
 
