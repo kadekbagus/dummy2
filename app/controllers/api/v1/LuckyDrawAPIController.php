d0a19348d82dd8918b55b6978528a849eddfdba3
shelgi <shelgi@dominopos.com>
2016-03-03 11:03:04 +0800

    fix luckydraw filter and sort (using english translation)

--- app/controllers/api/v1/LuckyDrawAPIController.php
+++ app/controllers/api/v1/LuckyDrawAPIController.php
@@ -992,8 +992,11 @@ class LuckyDrawAPIController extends ControllerAPI
             $prefix = DB::getTablePrefix();
             $luckydraws = LuckyDraw::excludeDeleted('lucky_draws')
                                     ->leftJoin('campaign_status', 'campaign_status.campaign_status_id', '=', 'lucky_draws.campaign_status_id')
-                                    ->select('lucky_draws.*', 'campaign_status.order', DB::raw("CASE WHEN {$prefix}campaign_status.campaign_status_name = 'expired' THEN {$prefix}campaign_status.campaign_status_name ELSE (CASE WHEN {$prefix}lucky_draws.end_date < {$this->quote($now)} THEN 'expired' ELSE {$prefix}campaign_status.campaign_status_name END) END AS campaign_status"));
-
+                                    ->select('lucky_draws.*', 'campaign_status.order', DB::raw("CASE WHEN {$prefix}campaign_status.campaign_status_name = 'expired' THEN {$prefix}campaign_status.campaign_status_name ELSE (CASE WHEN {$prefix}lucky_draws.end_date < {$this->quote($now)} THEN 'expired' ELSE {$prefix}campaign_status.campaign_status_name END) END AS campaign_status"))
+                                    ->leftJoin('lucky_draw_translations', 'lucky_draw_translations.lucky_draw_id', '=', 'lucky_draws.lucky_draw_id')
+                                    ->leftJoin('merchant_languages', 'merchant_languages.merchant_language_id', '=', 'lucky_draw_translations.merchant_language_id')
+                                    ->leftJoin('languages', 'languages.language_id', '=', 'merchant_languages.language_id')
+                                    ->where('languages.name', '=', 'en');
             if ($details_view === 'yes') {
                 $luckydraws->select('lucky_draws.*', 'campaign_status.order', DB::raw("CASE WHEN {$prefix}campaign_status.campaign_status_name = 'expired' THEN {$prefix}campaign_status.campaign_status_name ELSE (CASE WHEN {$prefix}lucky_draws.end_date < {$this->quote($now)} THEN 'expired' ELSE {$prefix}campaign_status.campaign_status_name END) END  AS campaign_status"), 'merchants.name',
                                     DB::raw("count({$prefix}lucky_draw_numbers.lucky_draw_number_id) as total_issued_lucky_draw_number"))
@@ -1033,13 +1036,13 @@ class LuckyDrawAPIController extends ControllerAPI
             // Filter lucky draw by name
             OrbitInput::get('lucky_draw_name', function($name) use ($luckydraws)
             {
-                $luckydraws->whereIn('lucky_draws.lucky_draw_name', $name);
+                $luckydraws->whereIn('lucky_draw_translations.lucky_draw_name', $name);
             });
 
             // Filter lucky draw by matching name pattern
             OrbitInput::get('lucky_draw_name_like', function($name) use ($luckydraws)
             {
-                $luckydraws->where('lucky_draws.lucky_draw_name', 'like', "%$name%");
+                $luckydraws->where('lucky_draw_translations.lucky_draw_name', 'like', "%$name%");
             });
 
             // Filter lucky draw by description
@@ -1219,7 +1222,7 @@ class LuckyDrawAPIController extends ControllerAPI
                 // Map the sortby request to the real column name
                 $sortByMapping = array(
                     'registered_date'                => 'lucky_draws.created_at',
-                    'lucky_draw_name'                => 'lucky_draws.lucky_draw_name',
+                    'lucky_draw_name'                => 'lucky_draw_translations.lucky_draw_name',
                     'description'                    => 'lucky_draws.description',
                     'start_date'                     => 'lucky_draws.start_date',
                     'end_date'                       => 'lucky_draws.end_date',
@@ -1248,8 +1251,8 @@ class LuckyDrawAPIController extends ControllerAPI
             $luckydraws->orderBy($sortBy, $sortMode);
 
             //with name
-            if ($sortBy !== 'lucky_draws.lucky_draw_name') {
-                $luckydraws->orderBy('lucky_draws.lucky_draw_name', 'asc');
+            if ($sortBy !== 'lucky_draw_translations.lucky_draw_name') {
+                $luckydraws->orderBy('lucky_draw_translations.lucky_draw_name', 'asc');
             }
 
             $totalLuckyDraws = RecordCounter::create($_luckydraws)->count();
