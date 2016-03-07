c79f3efa31fae89672ee0c94c99783eb9952e5d2
Ahmad <ahmad@dominopos.com>
2016-03-07 14:00:43 +0800

    Add new model ViewItemUser

---
+++ app/models/ViewItemUser.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * Model for View Item User
+ *
+ * @author Ahmad Anshori <ahmad@dominopos.com>
+ */
+class ViewItemUser extends Eloquent {
+	protected $table = 'view_item_user';
+    protected $primaryKey = 'view_item_user_id';
+}
