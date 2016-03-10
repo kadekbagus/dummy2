76d17cd3668c75d9ed88fc563c19e16f0670478f
Firmansyah <firmansyah@dominopos.com>
2016-03-10 22:28:46 +0800

    Make artisan command for migrate and calculate spending and save to tabel campaign_spending

---
+++ app/models/CampaignSpendingCount.php
@@ -0,0 +1,14 @@
+<?php
+
+class CampaignSpendingCount extends Eloquent
+{
+    /**
+     * CampaignSpending Model
+     *
+     * @author Firmansyah <firmansyah@dominopos.com>
+     */
+
+    protected $table = 'campaign_spending';
+    protected $primaryKey = 'campaign_spending_id';
+
+}
