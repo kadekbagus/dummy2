ff98a9407a8fc02dbfdc031e1585e6a3e2a54ea9
Qosdil A <qosdil@dominopos.com>
2016-03-17 11:56:57 +0800

    Initiate the "PMP Account List" with dummy data.

---
+++ app/controllers/api/v1/AccountAPIController.php
@@ -0,0 +1,63 @@
+<?php
+
+use OrbitShop\API\v1\ControllerAPI;
+
+class AccountAPIController extends ControllerAPI
+{
+    protected $columns = [
+        'name' => [
+            'title' => 'Account Name',
+            'sort_key' => 'name',
+        ],
+        'company_name' => [
+            'title' => 'Company Name',
+            'sort_key' => 'company_name',
+        ],
+        'location' => [
+            'title' => 'Location',
+            'sort_key' => 'location',
+        ],
+        'number_of_tenant' => [
+            'title' => 'Number of Tenant(s)',
+            'sort_key' => 'number_of_tenant',
+        ],
+        'tenants' => [
+            'title' => 'Tenant(s)',
+            'sort_key' => 'tenants',
+        ],
+        'creation_date' => [
+            'title' => 'Creation Date',
+            'sort_key' => 'creation_date',
+        ],
+        'status' => [
+            'title' => 'Status',
+            'sort_key' => 'status',
+        ],
+    ];
+
+    public function getAccount()
+    {
+        $records = [
+            [
+                'name' => 'Starbucks Jakarta',
+                'company_name' => 'Visionet',
+                'location' => 'Jakarta, Indonesia',
+                'number_of_tenant' => '2',
+                'tenants' => ['Starbucks@Lippo Mall Puri', 'Starbucks@Lippo Mall Kemang'],
+                'creation_date' => '26 January 2016 14:51:35',
+                'status' => 'active',
+            ],
+        ];
+
+        $data = new stdClass();
+        $data->columns = $this->columns;
+        $data->records = $records;
+
+        return [
+            'code' => 0,
+            'status' => 'success',
+            'message' => 'Request OK',
+            'data' => $data,
+        ];
+    }
+}
