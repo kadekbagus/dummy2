ce26c288c49fe2b7dc63a8138bf9861f90b03258
Irianto Pratama <irianto@dominopos.com>
2016-03-18 16:50:14 +0800

    Fix create pmp account Create model campaign account

---
+++ app/models/CampaignAccount.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Model for table `campaign_account`.
+ *
+ * @author Irianto Pratama <irianto@dominopos.com>
+ */
+class CampaignAccount extends Eloquent
+{
+    protected $primaryKey = 'campaign_account_id';
+    protected $table = 'campaign_account';
+
+}
