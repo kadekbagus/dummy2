530cfb2e13fe9c042f1c0068fcaaa55561a0d7ff
Qosdil A <qosdil@dominopos.com>
2016-03-22 14:08:14 +0800

    Refactor PMP Account export controller.

--- app/controllers/Report/AccountExportController.php
+++ app/controllers/Report/AccountExportController.php
@@ -6,13 +6,6 @@ class AccountExportController extends \AccountAPIController
 {
     use ExportControllerTrait;
 
-    public function __construct()
-    {
-        // Call a parent controller method to prepare the data
-        $this->prepareData();
-
-        // Set the page title
-        $this->data->pageTitle = 'PMP Accounts';
-    }
+    protected $pageTitle = 'PMP Accounts';
 
 }
