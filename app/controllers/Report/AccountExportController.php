d6d82604b2c43d06bf85e47c86fc3ece7da48763
Qosdil A <qosdil@dominopos.com>
2016-03-22 11:01:45 +0800

    Create a trait for print and CSV exports.

--- app/controllers/Report/AccountExportController.php
+++ app/controllers/Report/AccountExportController.php
@@ -2,69 +2,17 @@
 
 namespace Report;
 
-class AccountExportController extends \AccountAPIController {
+class AccountExportController extends \AccountAPIController
+{
+    use ExportControllerTrait;
 
     public function __construct()
     {
+        // Call a parent controller method to prepare the data
         $this->prepareData();
 
         // Set the page title
         $this->data->pageTitle = 'PMP Accounts';
-    }
-
-    public function getList()
-    {
-        if (\Input::get('export') == 'csv') {
-            return $this->getCsv();
-        };
-
-        return \View::make('report.print_friendly', (array) $this->data);
-    }
-
-    public function getCsv()
-    {
-        $csv = ',,,,,,';
-        $csv .= "\r\n";
-        $csv .= $this->data->pageTitle;
-        $csv .= ',,,,,,';
-        $csv .= "\r\n";
-        $csv .= ',,,,,,';
-        
-        $csv .= "\r\n";
-        $csv .= "\r\n";
-
-        foreach ($this->data->columns as $column) {
-            $csv .= $column['title'].',';
-        }
-
-        $csv .= "\r\n";
-
-        foreach ($this->data->records as $row) {
-            foreach (array_keys($this->data->columns) as $fieldName) {
-                if ( ! is_array($row[$fieldName])) {
-
-                    // Replace comma with dash
-                    $csv .= str_replace(',', ' -', $row[$fieldName]);
-
-                } else {
-
-                    // Show array values as string with a semicolon separator
-                    $csv .= implode('; ', $row[$fieldName]);
-                }
-
-                $csv .= ',';
-            }
-
-            $csv .= "\r\n";
-        }
-
-        $response = \Response::make($csv, 200);
-        $response->header('Content-Type', 'text/csv');
-
-        $fileName = 'orbit-export-'.str_replace(' ', '-', $this->data->pageTitle.'-'.date('D_d_M_Y_').rand(10000, 99999)).'.csv';
-        $response->header('Content-Disposition', 'inline; filename="'.$fileName.'"');
-
-        return $response;
     }
 
 }
