a54c3029bd572490cd3e3e7cae79b1c7e4a54c7d
Qosdil A <qosdil@dominopos.com>
2016-03-23 16:12:10 +0800

    Clean up user_merchant before insert (PMP Account).

--- app/controllers/api/v1/AccountAPIController.php
+++ app/controllers/api/v1/AccountAPIController.php
@@ -126,6 +126,7 @@ class AccountAPIController extends ControllerAPI
         $campaignAccount->save();
 
         // Clean up user_merchant first
+        UserMerchant::whereIn('merchant_id', Input::get('merchant_ids'));
         UserMerchant::whereUserId($user->user_id)->delete();
 
         // Save to user_merchant (1 to M)
