6def50de00643cf244b1fe7ce9c40e5d0d1a211e
Qosdil A <qosdil@dominopos.com>
2016-03-24 11:13:34 +0800

    Remove an uneccessary query on PMP Accounts.

--- app/controllers/api/v1/AccountAPIController.php
+++ app/controllers/api/v1/AccountAPIController.php
@@ -130,7 +130,6 @@ class AccountAPIController extends ControllerAPI
         $campaignAccount->save();
 
         // Clean up user_merchant first
-        UserMerchant::whereIn('merchant_id', Input::get('merchant_ids'))->delete();
         UserMerchant::whereUserId($user->user_id)->delete();
 
         // Save to user_merchant (1 to M)
