253d6352a2a4b27b2af467218a86253043ff90ce
Tian <tian@dominopos.com>
2016-03-30 12:43:24 +0800

    Fix print export number formatting on employee, tenant, lucky draw, CRM detail, captive portal, coupon report, issued coupon report, lucky draw number, mall, mall group list

--- app/views/printer/list-tenant-view.php
+++ app/views/printer/list-tenant-view.php
@@ -112,7 +112,7 @@
         <tr>
             <td>Total Tenants</td>
             <td>:</td>
-            <td><strong><?php echo number_format($totalRec, 0, '.', '.'); ?></strong></td>
+            <td><strong><?php echo number_format($totalRec); ?></strong></td>
         </tr>
 
         <?php if ($filterName != '') { ?>
