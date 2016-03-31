235c6912a3561019373c532027c6e977b759a04a
Qosdil A <qosdil@dominopos.com>
2016-03-31 14:16:47 +0800

    Email must be unique (PMP Accounts).

--- app/controllers/api/v1/AccountAPIController.php
+++ app/controllers/api/v1/AccountAPIController.php
@@ -368,6 +368,7 @@ class AccountAPIController extends ControllerAPI
             $rules['id'] = 'exists:users,user_id';
         } else {
             $rules['user_password'] = 'required';
+            $rules['user_email'] .= '|unique:users,user_email';
             $rules['account_name'] .= '|unique:campaign_account,account_name';
         }
 
