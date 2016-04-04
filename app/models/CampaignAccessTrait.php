ef82d9d0f226f2fecc56027fb3582be04c675479
Irianto Pratama <irianto@dominopos.com>
2016-04-04 16:17:41 +0800

    Fix allowed for pmp users dont filtering mall

--- app/models/CampaignAccessTrait.php
+++ app/models/CampaignAccessTrait.php
@@ -14,30 +14,24 @@ trait CampaignAccessTrait
      * @param string $type type of campaign: news, promotion, coupon
      * @return builder
      */
-    public function scopeAllowedForPMPUser($builder, $user, $type, $mall = null) {
+    public function scopeAllowedForPMPUser($builder, $user, $type) {
         $table_name = '';
         $field_name = '';
 
         switch ($type) {
             case 'news':
                 $table_name = 'news';
-                $relation_merchant = 'news_merchant';
                 $field_name = 'news_id';
-                $field_merchant_id = 'merchant_id';
                 break;
 
             case 'promotion':
                 $table_name = 'news';
-                $relation_merchant = 'news_merchant';
                 $field_name = 'news_id';
-                $field_merchant_id = 'merchant_id';
                 break;
 
             case 'coupon':
                 $table_name = 'promotions';
-                $relation_merchant = 'promotion_retailer';
                 $field_name = 'promotion_id';
-                $field_merchant_id = 'retailer_id';
                 break;
 
             default:
@@ -55,8 +49,6 @@ trait CampaignAccessTrait
         // the original query unaffected
         $builder->leftJoin('user_campaign', 'user_campaign.campaign_id', '=', "{$table_name}.{$field_name}")
                 ->join('campaign_account', 'campaign_account.user_id', '=', 'user_campaign.user_id')
-                ->join("{$relation_merchant}", "{$relation_merchant}.{$field_name}", '=', 'user_campaign.campaign_id')
-                ->join('merchants', 'merchants.merchant_id', '=', "{$relation_merchant}.{$field_merchant_id}")
         ->where(function ($q) use ($user) {
             $q->where('campaign_account.user_id', $user->user_id)
               ->orWhere('campaign_account.parent_user_id', $user->user_id);
@@ -64,12 +56,6 @@ trait CampaignAccessTrait
         ->Where(function ($q) use ($type) {
             if ($type !== 'coupon') {
                 $q->where('news.object_type', $type);
-            }
-        })
-        ->where(function ($q) use ($mall) {
-            if (! is_null($mall)) {
-                $q->where('merchants.merchant_id', $mall->merchant_id)
-                  ->orWhere('merchants.parent_id', $mall->merchant_id);
             }
         });
 
