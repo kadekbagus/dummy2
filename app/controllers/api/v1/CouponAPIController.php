31a45f486f764e454a8e28f256a3c0221dc964da
Firmansyah <firmansyah@dominopos.com>
2016-04-05 11:12:41 +0800

    Enable user right coupon listing

--- app/controllers/api/v1/CouponAPIController.php
+++ app/controllers/api/v1/CouponAPIController.php
@@ -2234,10 +2234,8 @@ class CouponAPIController extends ControllerAPI
 
             // Builder object
             // Addition select case and join for sorting by discount_value.
-            $coupons = Coupon::
-                // Waiting insert update proses
-                // allowedForPMPUser($user, 'coupon')->
-                with('couponRule')
+            $coupons = Coupon::allowedForPMPUser($user, 'coupon')
+                ->with('couponRule')
                 ->select(DB::raw("{$table_prefix}promotions.*, {$table_prefix}campaign_price.campaign_price_id, {$table_prefix}coupon_translations.promotion_name AS name_english,
                     CASE WHEN {$table_prefix}campaign_status.campaign_status_name = 'expired' THEN {$table_prefix}campaign_status.campaign_status_name ELSE (CASE WHEN {$table_prefix}promotions.end_date < (SELECT CONVERT_TZ(UTC_TIMESTAMP(),'+00:00', ot.timezone_name)
                                                                                 FROM {$table_prefix}merchants om
