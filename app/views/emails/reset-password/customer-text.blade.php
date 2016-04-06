8b3b2a2190a2a5cd2c5b556df02bbbb77e5bd726
Rio Astamal <rio@dominopos.com>
2016-04-06 14:27:35 +0800

    Minor update on template of reset password and activate account

--- app/views/emails/reset-password/customer-text.blade.php
+++ app/views/emails/reset-password/customer-text.blade.php
@@ -1,18 +1,14 @@
 Hi {{ $first_name }},
 
-Someone (hopefully you) recently requested a password reset.
+We received a request to reset your password for your GotoMalls account:
+{{ $email }}
 
-To reset your password, follow the link below:
+We're here to help you. Click link below to reset your password:
 
 {{ $token_url }}
 
+If you didn't ask to change your password, don't worry! Your password
+is still safe and just ignore this email.
 
-Regards,
+Cheers,
 Orbit Team
-
------------------------------------------------------------
-ORBIT CUSTOMER SERVICE
-
-Email: {{ $cs_email }}
-------------------------------------------------------------
-
