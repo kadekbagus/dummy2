f66ea13c49dd4df0642e11af3353c4b80e3f3b98
Ahmad <ahmad@dominopos.com>
2016-04-08 12:02:37 +0800

    Fix array merge on auto coupon issuance coupon_location session

--- app/models/Coupon.php
+++ app/models/Coupon.php
@@ -598,7 +598,7 @@ class Coupon extends Eloquent
             }
         }
 
-        $session->write('coupon_location', $coupon_locations + [$retailer->merchant_id]);
+        $session->write('coupon_location', array_merge($coupon_locations, [$retailer->merchant_id]));
     }
 
     protected static function flipArrayElement($source)
