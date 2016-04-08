6c5dbe044ba64df63e3ef4b603236bac92dd260d
Ahmad <ahmad@dominopos.com>
2016-04-08 18:04:23 +0800

    Fix sign in/up popup views

--- app/views/mobile-ci/commonscripts.blade.php
+++ app/views/mobile-ci/commonscripts.blade.php
@@ -1019,7 +1019,8 @@
             orbitSignUpForm.hideErrorMessageBox();
             $('#signin-form-wrapper').removeClass('hide');
             $('#forget-form-wrapper').addClass('hide');
-            $('#slogan-container, #social-media-wraper').addClass('hide');
+            $('#signup-form-wrapper').addClass('hide');
+            $('#slogan-container, #social-media-wrapper').addClass('hide');
         });
 
         $('#formModal').on('shown.bs.modal', function () {
@@ -1028,7 +1029,7 @@
         });
 
         $('#formModal').on('hide.bs.modal', function () {
-            $('#slogan-container, #social-media-wraper').removeClass('hide');
+            $('#slogan-container, #social-media-wrapper').removeClass('hide');
         });
 
         function isValidEmailAddress(emailAddress) {
@@ -1470,7 +1471,7 @@
         },
         isFromCaptiveFn = function () {
             if ('{{{ Input::get('from_captive', 'no') }}}' === 'yes') {
-                $('#social-media-wraper').addClass('hide');
+                $('#social-media-wrapper').addClass('hide');
             }
         };
 
