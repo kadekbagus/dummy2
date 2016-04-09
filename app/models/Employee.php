0a17d9494cecf1bf6c1f1f0aa4de5bfdf9934fe5
Rio Astamal <rio@dominopos.com>
2016-04-09 16:36:07 +0800

    Add new end API controller for listing customer service by coupon id

--- app/models/Employee.php
+++ app/models/Employee.php
@@ -79,4 +79,31 @@ class Employee extends Eloquent
                  ->groupBy('parent_id')
                  ->lists('parent_id');
     }
+
+    /**
+     * Scope to get list of mall employees (customer service) for particular coupon
+     *
+     * @author Rio Astamal <rio@dominopos.com>
+     * @param Builder $builder Query Builder
+     * @param string $couponId Coupon ID
+     * @param Builder $builder Query Builder
+     */
+    public function scopeByCouponId($builder, $couponId)
+    {
+        $prefix = DB::getTablePrefix();
+        $couponId = DB::connection()->getPdo()->quote($couponId);
+
+        $builder->addSelect('employee_retailer.retailer_id as mall_id', 'users.user_firstname', 'users.user_lastname', 'employees.*')
+                ->join('employee_retailer', 'employee_retailer.employee_id', '=', 'employees.employee_id')
+                ->join('users', function($q) {
+                    $q->on('users.user_id', '=', 'employees.user_id');
+                    $q->on('users.status', '!=', DB::raw("'deleted'"));
+                })
+                ->join('promotion_employee', function($q) use ($couponId) {
+                    $q->on('users.user_id', '=', 'promotion_employee.user_id');
+                    $q->on('promotion_employee.promotion_id', '=', DB::raw($couponId));
+                });
+
+        return $builder;
+    }
 }
