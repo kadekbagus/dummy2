cd15a6bc8ac956e992cfefb802917ce1b571cf03
Ahmad <ahmad@dominopos.com>
2016-04-15 17:15:41 +0800

    Fix double sign up activity

--- app/controllers/src/Orbit/Controller/API/v1/Pub/RegistrationAPIController.php
+++ app/controllers/src/Orbit/Controller/API/v1/Pub/RegistrationAPIController.php
@@ -84,17 +84,15 @@ class RegistrationAPIController extends IntermediateBaseController
                 $sessionHeader = $this->session->getSessionConfig()->getConfig('session_origin.header.name');
                 $sessionHeader = 'Set-' . $sessionHeader;
                 $this->customHeaders[$sessionHeader] = $this->session->getSessionId();
-            }
 
-            $activity_name_long = 'Sign Up';
-            OrbitInput::post('activity_name_long', function ($act) use (&$activity_name_long) {
-                $activity_name_long = $act;
-            });
-            // Successfull login
-            $activity->setUser($user)
-                     ->setActivityName('registration_ok')
-                     ->setActivityNameLong($activity_name_long)
-                     ->responseOK();
+                $activity_name_long = 'Sign Up';
+                // Successfull login
+                $activity->setUser($user)
+                         ->setActivityName('registration_ok')
+                         ->setActivityNameLong($activity_name_long)
+                         ->responseOK()
+                         ->setModuleName('Application')->save();
+            }
 
             $this->response->data = $user;
             $this->response->code = 0;
@@ -120,7 +118,8 @@ class RegistrationAPIController extends IntermediateBaseController
                      ->setActivityName('registration_failed')
                      ->setActivityNameLong('Registration Failed')
                      ->setNotes($e->getMessage())
-                     ->responseFailed();
+                     ->responseFailed()
+                     ->setModuleName('Application')->save();
         } catch (InvalidArgsException $e) {
             DB::rollback();
             $this->response->code = $e->getCode();
@@ -132,7 +131,8 @@ class RegistrationAPIController extends IntermediateBaseController
                      ->setActivityName('registration_failed')
                      ->setActivityNameLong('Registration Failed')
                      ->setNotes($e->getMessage())
-                     ->responseFailed();
+                     ->responseFailed()
+                     ->setModuleName('Application')->save();
         } catch (Exception $e) {
             DB::rollback();
             $this->response->code = Status::UNKNOWN_ERROR;
@@ -144,11 +144,9 @@ class RegistrationAPIController extends IntermediateBaseController
                      ->setActivityName('registration_failed')
                      ->setActivityNameLong('Registration Failed')
                      ->setNotes($e->getMessage())
-                     ->responseFailed();
+                     ->responseFailed()
+                     ->setModuleName('Application')->save();
         }
-
-        // Save the activity
-        $activity->setModuleName('Application')->save();
 
         return $this->render($this->response);
     }
