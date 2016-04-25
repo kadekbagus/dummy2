145570deb89ba6044bf8b8c73499929c0307c6d9
kadekbagus <kadek@dominopos.com>
2016-04-25 14:52:08 +0800

    add gotomalls favi icon on mobile ci

--- app/views/mobile-ci/head.blade.php
+++ app/views/mobile-ci/head.blade.php
@@ -5,19 +5,19 @@
 <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" />
 <meta name="format-detection" content="telephone=no">
 <meta name="mobile-web-app-capable" content="yes">
-@if(! empty($this_mall->mediaIcon))
+<!-- @if(! empty($this_mall->mediaIcon))
 <link rel="apple-touch-icon-precomposed" sizes="57x57" href="{{ asset($this_mall->mediaIcon->path) }}" />
 <link rel="apple-touch-icon-precomposed" sizes="72x72" href="{{ asset($this_mall->mediaIcon->path) }}" />
 <link rel="apple-touch-icon-precomposed" sizes="114x114" href="{{ asset($this_mall->mediaIcon->path) }}" />
 <link rel="apple-touch-icon-precomposed" sizes="144x144" href="{{ asset($this_mall->mediaIcon->path) }}" />
 <link rel="icon" type="image/ico" href="{{ asset($this_mall->mediaIcon->path) }}"/>
-@else
-<link rel="apple-touch-icon-precomposed" sizes="57x57" href="{{ asset('mobile-ci/images/orbit-icon-default.png') }}" />
-<link rel="apple-touch-icon-precomposed" sizes="72x72" href="{{ asset('mobile-ci/images/orbit-icon-default.png') }}" />
-<link rel="apple-touch-icon-precomposed" sizes="114x114" href="{{ asset('mobile-ci/images/orbit-icon-default.png') }}" />
-<link rel="apple-touch-icon-precomposed" sizes="144x144" href="{{ asset('mobile-ci/images/orbit-icon-default.png') }}" />
-<link rel="icon" type="image/ico" href="{{ asset('mobile-ci/images/orbit-icon-default.png') }}"/>
-@endif
+@else -->
+<link rel="apple-touch-icon-precomposed" sizes="57x57" href="{{ asset('mobile-ci/images/gotomalls.ico') }}" />
+<link rel="apple-touch-icon-precomposed" sizes="72x72" href="{{ asset('mobile-ci/images/gotomalls.ico') }}" />
+<link rel="apple-touch-icon-precomposed" sizes="114x114" href="{{ asset('mobile-ci/images/gotomalls.ico') }}" />
+<link rel="apple-touch-icon-precomposed" sizes="144x144" href="{{ asset('mobile-ci/images/gotomalls.ico') }}" />
+<link rel="icon" type="image/ico" href="{{ asset('mobile-ci/images/gotomalls.ico') }}"/>
+<!-- @endif -->
 
 {{-- HTML::style('mobile-ci/stylesheet/bootstrap-tour.min.css') --}}
 {{ HTML::style('mobile-ci/stylesheet/' . Orbit\Helper\Asset\Stylesheet::create()->getMallCss()) }}
