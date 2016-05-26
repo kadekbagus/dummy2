14f9acb791c1b1f06fb681bcf052218c738a5dc4
Ahmad <ahmad@dominopos.com>
2016-05-26 17:55:51 +0800

    Add api routes for TenantCIAPIController

--- app/routes/06-angular-ci/01-tenant.php
+++ app/routes/06-angular-ci/01-tenant.php
@@ -1,5 +1,15 @@
 <?php
 
+Route::get('/api/v1/cust/stores', function()
+{
+    return Orbit\Controller\API\v1\Customer\TenantCIAPIController::create()->getTenantList();
+});
+
+Route::get('/api/v1/cust/stores/{id}', function($id)
+{
+    return Orbit\Controller\API\v1\Customer\TenantCIAPIController::create()->getTenantList();
+});
+
 Route::get('/app/v1/cust/stores', ['as' => 'customer-api-store-list', 'uses' => 'IntermediateCIAuthController@TenantCI_getTenantList']);
 
 Route::get('/app/v1/cust/stores/{id}', ['as' => 'customer-api-store-detail', 'uses' => 'IntermediateCIAuthController@TenantCI_getTenantItem']);
