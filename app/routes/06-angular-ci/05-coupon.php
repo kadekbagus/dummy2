fc1ddaafb05795a7143f3705cff8f9455af3107f
Ahmad <ahmad@dominopos.com>
2016-05-31 15:35:05 +0800

    Add new route for coupon related api for angular ci

---
+++ app/routes/06-angular-ci/05-coupon.php
@@ -0,0 +1,15 @@
+<?php
+
+Route::get('/api/v1/cust/coupons', function()
+{
+    return Orbit\Controller\API\v1\Customer\CouponCIAPIController::create()->getCouponList();
+});
+
+Route::get('/api/v1/cust/coupons/detail', function()
+{
+    return Orbit\Controller\API\v1\Customer\CouponCIAPIController::create()->getCouponItem();
+});
+
+Route::get('/app/v1/cust/coupons', ['as' => 'customer-api-coupon-list', 'uses' => 'IntermediateCIAuthController@CouponCI_getCouponList']);
+
+Route::get('/app/v1/cust/coupons/detail', ['as' => 'customer-api-coupon-detail', 'uses' => 'IntermediateCIAuthController@CouponCI_getCouponItem']);
