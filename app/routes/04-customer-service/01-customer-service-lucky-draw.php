a9b4a42ec1a2685699ce2611df46e6f92dbe7e04
Firmansyah <firmansyah@dominopos.com>
2016-06-06 14:30:33 +0800

    Fix addToInbox parameter related OM-1956 Lucky draw numbers issued are not the right quantity for each receipt

--- app/routes/04-customer-service/01-customer-service-lucky-draw.php
+++ app/routes/04-customer-service/01-customer-service-lucky-draw.php
@@ -3,18 +3,15 @@
  * Routes file Customer Service lucky draw related activity.
  */
 
-Route::group(['before' => 'orbit-settings'], function()
+/**
+ * Issue lucky draw to customer
+ */
+Route::post('/api/v1/cs/lucky-draw-number/issue', function()
 {
-    /**
-     * Issue lucky draw to customer
-     */
-    Route::post('/api/v1/cs/lucky-draw-number/issue', function()
-    {
-        return LuckyDrawCSAPIController::create()->postIssueLuckyDrawNumberExternal();
-    });
-
-    /**
-     * Intermediate.
-     */
-    Route::post('/app/v1/cs/lucky-draw-number/issue', 'IntermediateAuthController@LuckyDrawCS_postIssueLuckyDrawNumberExternal');
+    return LuckyDrawCSAPIController::create()->postIssueLuckyDrawNumberExternal();
 });
+
+/**
+ * Intermediate.
+ */
+Route::post('/app/v1/cs/lucky-draw-number/issue', 'IntermediateAuthController@LuckyDrawCS_postIssueLuckyDrawNumberExternal');
