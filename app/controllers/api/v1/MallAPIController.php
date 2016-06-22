fe7faabc93b83cce86a0ca0be9641028d946c89f
Irianto Pratama <irianto.dominopos.com>
2016-06-22 16:03:03 +0800

    Fix update mall

--- app/controllers/api/v1/MallAPIController.php
+++ app/controllers/api/v1/MallAPIController.php
@@ -1477,17 +1477,17 @@ class MallAPIController extends ControllerAPI
             $ticket_footer = OrbitInput::post('ticket_footer');
             $start_date_activity = OrbitInput::post('start_date_activity');
             $end_date_activity = OrbitInput::post('end_date_activity');
-            $timezoneName = OrbitInput::post('timezone');
-            $currency = OrbitInput::post('currency');
-            $currency_symbol = OrbitInput::post('currency_symbol');
-            $sector_of_activity = OrbitInput::post('sector_of_activity');
+            // $timezoneName = OrbitInput::post('timezone');
+            // $currency = OrbitInput::post('currency');
+            // $currency_symbol = OrbitInput::post('currency_symbol');
+            // $sector_of_activity = OrbitInput::post('sector_of_activity');
             $mobile_default_language = OrbitInput::post('mobile_default_language');
             $languages = OrbitInput::post('languages');
             $floors = OrbitInput::post('floors');
             $domain = OrbitInput::post('domain');
-            $campaign_base_price_promotion = OrbitInput::post('campaign_base_price_promotion');
-            $campaign_base_price_coupon = OrbitInput::post('campaign_base_price_coupon');
-            $campaign_base_price_news = OrbitInput::post('campaign_base_price_news');
+            // $campaign_base_price_promotion = OrbitInput::post('campaign_base_price_promotion');
+            // $campaign_base_price_coupon = OrbitInput::post('campaign_base_price_coupon');
+            // $campaign_base_price_news = OrbitInput::post('campaign_base_price_news');
             $free_wifi_status = OrbitInput::post('free_wifi_status');
             $geo_point_latitude = OrbitInput::post('geo_point_latitude');
             $geo_point_longitude = OrbitInput::post('geo_point_longitude');
@@ -1511,10 +1511,10 @@ class MallAPIController extends ControllerAPI
                     'ticket_footer'                 => $ticket_footer,
                     'start_date_activity'           => $start_date_activity,
                     'end_date_activity'             => $end_date_activity,
-                    'timezone'                      => $timezoneName,
-                    'currency'                      => $currency,
-                    'currency_symbol'               => $currency_symbol,
-                    'sector_of_activity'            => $sector_of_activity,
+                    // 'timezone'                      => $timezoneName,
+                    // 'currency'                      => $currency,
+                    // 'currency_symbol'               => $currency_symbol,
+                    // 'sector_of_activity'            => $sector_of_activity,
                     'languages'                     => $languages,
                     'domain'                        => $domain,
                     'mobile_default_language'       => $mobile_default_language,
@@ -1541,8 +1541,8 @@ class MallAPIController extends ControllerAPI
                     'ticket_footer'                    => 'ticket_footer_max_length',
                     'start_date_activity'              => 'date_format:Y-m-d H:i:s',
                     'end_date_activity'                => 'date_format:Y-m-d H:i:s',
-                    'timezone'                         => 'orbit.exists.timezone',
-                    'currency'                         => 'size:3',
+                    // 'timezone'                         => 'orbit.exists.timezone',
+                    // 'currency'                         => 'size:3',
                     'vat_included'                     => 'in:yes,no',
                     'languages'                        => 'array',
                     'domain'                           => 'alpha_dash|domain_exist_but_not_me:' . $merchant_id,
@@ -1696,13 +1696,13 @@ class MallAPIController extends ControllerAPI
                 $updatedmall->logo = $logo;
             });
 
-            OrbitInput::post('currency', function($currency) use ($updatedmall) {
-                $updatedmall->currency = $currency;
-            });
+            // OrbitInput::post('currency', function($currency) use ($updatedmall) {
+            //     $updatedmall->currency = $currency;
+            // });
 
-            OrbitInput::post('currency_symbol', function($currency_symbol) use ($updatedmall) {
-                $updatedmall->currency_symbol = $currency_symbol;
-            });
+            // OrbitInput::post('currency_symbol', function($currency_symbol) use ($updatedmall) {
+            //     $updatedmall->currency_symbol = $currency_symbol;
+            // });
 
             OrbitInput::post('tax_code1', function($tax_code1) use ($updatedmall) {
                 $updatedmall->tax_code1 = $tax_code1;
@@ -1748,9 +1748,9 @@ class MallAPIController extends ControllerAPI
                 $updatedmall->contact_person_email = $contact_person_email;
             });
 
-            OrbitInput::post('sector_of_activity', function($sector_of_activity) use ($updatedmall) {
-                $updatedmall->sector_of_activity = $sector_of_activity;
-            });
+            // OrbitInput::post('sector_of_activity', function($sector_of_activity) use ($updatedmall) {
+            //     $updatedmall->sector_of_activity = $sector_of_activity;
+            // });
 
             OrbitInput::post('parent_id', function($parent_id) use ($updatedmall) {
                 if (empty(trim($parent_id))) {
@@ -1803,22 +1803,10 @@ class MallAPIController extends ControllerAPI
                 $updatedmall->ticket_footer = $ticket_footer;
             });
 
-            OrbitInput::post('timezone', function($timezoneName) use ($updatedmall) {
-                $timezone = $this->valid_timezone;
-                $updatedmall->timezone_id = $timezone->timezone_id;
-            });
-
-            OrbitInput::post('currency', function($currency) use ($updatedmall) {
-                $updatedmall->currency = $currency;
-            });
-
-            OrbitInput::post('currency_symbol', function($currency_symbol) use ($updatedmall) {
-                $updatedmall->currency_symbol = $currency_symbol;
-            });
-
-            OrbitInput::post('sector_of_activity', function($sector_of_activity) use ($updatedmall) {
-                $updatedmall->sector_of_activity = $sector_of_activity;
-            });
+            // OrbitInput::post('timezone', function($timezoneName) use ($updatedmall) {
+            //     $timezone = $this->valid_timezone;
+            //     $updatedmall->timezone_id = $timezone->timezone_id;
+            // });
 
             OrbitInput::post('mobile_default_language', function($mobile_default_language) use ($updatedmall) {
                 $updatedmall->mobile_default_language = $mobile_default_language;
@@ -1837,38 +1825,38 @@ class MallAPIController extends ControllerAPI
                 $updatedmall->facebook_uri = $fb_uri;
             });
 
-            OrbitInput::post('campaign_base_price_promotion', function($campaign_base_price_promotion) use ($updatedmall) {
-                $campaign_base_price = CampaignBasePrice::where('status', '=', 'active')
-                                                        ->where('merchant_id', '=', $updatedmall->merchant_id)
-                                                        ->where('campaign_type', '=', 'promotion')
-                                                        ->first();
-                if (! empty($campaign_base_price)) {
-                    $campaign_base_price->price = $campaign_base_price_promotion;
-                    $campaign_base_price->save();
-                }
-            });
+            // OrbitInput::post('campaign_base_price_promotion', function($campaign_base_price_promotion) use ($updatedmall) {
+            //     $campaign_base_price = CampaignBasePrice::where('status', '=', 'active')
+            //                                             ->where('merchant_id', '=', $updatedmall->merchant_id)
+            //                                             ->where('campaign_type', '=', 'promotion')
+            //                                             ->first();
+            //     if (! empty($campaign_base_price)) {
+            //         $campaign_base_price->price = $campaign_base_price_promotion;
+            //         $campaign_base_price->save();
+            //     }
+            // });
 
-            OrbitInput::post('campaign_base_price_coupon', function($campaign_base_price_coupon) use ($updatedmall) {
-                $campaign_base_price = CampaignBasePrice::where('status', '=', 'active')
-                                                        ->where('merchant_id', '=', $updatedmall->merchant_id)
-                                                        ->where('campaign_type', '=', 'coupon')
-                                                        ->first();
-                if (! empty($campaign_base_price)) {
-                    $campaign_base_price->price = $campaign_base_price_coupon;
-                    $campaign_base_price->save();
-                }
-            });
+            // OrbitInput::post('campaign_base_price_coupon', function($campaign_base_price_coupon) use ($updatedmall) {
+            //     $campaign_base_price = CampaignBasePrice::where('status', '=', 'active')
+            //                                             ->where('merchant_id', '=', $updatedmall->merchant_id)
+            //                                             ->where('campaign_type', '=', 'coupon')
+            //                                             ->first();
+            //     if (! empty($campaign_base_price)) {
+            //         $campaign_base_price->price = $campaign_base_price_coupon;
+            //         $campaign_base_price->save();
+            //     }
+            // });
 
-            OrbitInput::post('campaign_base_price_news', function($campaign_base_price_news) use ($updatedmall) {
-                $campaign_base_price = CampaignBasePrice::where('status', '=', 'active')
-                                                        ->where('merchant_id', '=', $updatedmall->merchant_id)
-                                                        ->where('campaign_type', '=', 'news')
-                                                        ->first();
-                if (! empty($campaign_base_price)) {
-                    $campaign_base_price->price = $campaign_base_price_news;
-                    $campaign_base_price->save();
-                }
-            });
+            // OrbitInput::post('campaign_base_price_news', function($campaign_base_price_news) use ($updatedmall) {
+            //     $campaign_base_price = CampaignBasePrice::where('status', '=', 'active')
+            //                                             ->where('merchant_id', '=', $updatedmall->merchant_id)
+            //                                             ->where('campaign_type', '=', 'news')
+            //                                             ->first();
+            //     if (! empty($campaign_base_price)) {
+            //         $campaign_base_price->price = $campaign_base_price_news;
+            //         $campaign_base_price->save();
+            //     }
+            // });
 
             OrbitInput::post('languages', function($languages) use ($updatedmall) {
                 if (! in_array('en', $languages)) {
