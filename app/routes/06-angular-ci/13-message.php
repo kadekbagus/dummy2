8b2d3fcb53bc1c5a28bf9d7be9153092cb6b701e
Firmansyah <firmansyah@dominopos.com>
2016-07-26 18:35:25 +0800

    Add route for unread-count my messages

--- app/routes/06-angular-ci/13-message.php
+++ app/routes/06-angular-ci/13-message.php
@@ -10,6 +10,11 @@ Route::get('/api/v1/cust/messages/detail', function()
     return Orbit\Controller\API\v1\Customer\MessageCIAPIController::create()->getMessageDetail();
 });
 
+Route::get('/app/v1/cust/messages/unread-count', function()
+{
+    return Orbit\Controller\API\v1\Customer\MessageCIAPIController::create()->getPollMessages();
+});
+
 Route::post('/api/v1/cust/messages/delete', function()
 {
     return Orbit\Controller\API\v1\Customer\MessageCIAPIController::create()->postDeleteMessage();
@@ -23,6 +28,8 @@ Route::post('/api/v1/cust/messages/read-unread', function()
 Route::get('/app/v1/cust/messages', ['as' => 'messages-list', 'uses' => 'IntermediateCIAuthController@MessageCI_getMessage']);
 
 Route::get('/app/v1/cust/messages/detail', ['as' => 'messages-detail', 'uses' => 'IntermediateCIAuthController@MessageCI_getMessageDetail']);
+
+Route::post('/app/v1/cust/messages/unread-count', 'IntermediateCIAuthController@MessageCI_getPollMessages');
 
 Route::post('/app/v1/cust/messages/delete', 'IntermediateCIAuthController@MessageCI_postDeleteMessage');
 
