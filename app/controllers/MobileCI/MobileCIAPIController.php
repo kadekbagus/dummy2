582727719a7cab4d93e66fa877639a0bfe30847d
kadekbagus <kadek@dominopos.com>
2016-08-18 18:46:41 +0800

    add validation checking coupon already added to wallet or not

--- app/controllers/MobileCI/MobileCIAPIController.php
+++ app/controllers/MobileCI/MobileCIAPIController.php
@@ -1663,12 +1663,25 @@ class MobileCIAPIController extends BaseCIController
 
             // guest cannot add to wallet
             if (! $user->isConsumer()) {
-                OrbitShopAPI::throwInvalidArgument('You must login to access this.');
+                $message = 'You must login to access this.';
+                OrbitShopAPI::throwInvalidArgument($message);
             }
+
             $retailer = $this->getRetailerInfo();
             $this->registerCustomValidation();
-
             $coupon_id = OrbitInput::post('coupon_id');
+
+            // check if coupon already add to wallet
+            $wallet = IssuedCoupon::where('promotion_id', '=', $coupon_id)
+                                  ->where('user_id', '=', $user->user_id)
+                                  ->where('issuer_retailer_id', '=', $retailer->merchant_id)
+                                  ->where('status', '=', 'active')
+                                  ->first(); 
+            
+            if (is_object($wallet)) {
+               $message = 'coupon already added to wallet';
+               OrbitShopAPI::throwInvalidArgument($message); 
+            }
 
             $validator = Validator::make(
                 array(
