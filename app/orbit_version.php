efd3c24914bc46b25d8b5d2d22ae94cea31a26a0
Rio Astamal <rio@dominopos.com>
2016-08-25 16:02:02 +0800

    Bump version to 2.3.4

--- app/orbit_version.php
+++ app/orbit_version.php
@@ -23,9 +23,9 @@ if (! defined('ORBIT_APP_BUILD_NUMBER')) {
  */
 if (! defined('ORBIT_APP_VERSION')) {
     if (Config::get('app.debug')) {
-        define('ORBIT_APP_VERSION', '2.3.3-dev b' . ORBIT_APP_BUILD_NUMBER);
+        define('ORBIT_APP_VERSION', '2.3.4-dev b' . ORBIT_APP_BUILD_NUMBER);
     } else {
-        define('ORBIT_APP_VERSION', '2.3.3');
+        define('ORBIT_APP_VERSION', '2.3.4');
     }
 }
 
