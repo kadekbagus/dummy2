402fda5c0bcd72f276bb7a319592783b6976e0ff
firmansyah <firmansyah@dominopos.com>
2016-08-25 10:45:18 +0800

    Fix showing only active store

--- app/controllers/src/Orbit/Controller/API/v1/Pub/StoreAPIController.php
+++ app/controllers/src/Orbit/Controller/API/v1/Pub/StoreAPIController.php
@@ -198,7 +198,7 @@ class StoreAPIController extends ControllerAPI
 
             // Query without searching keyword
             $mall = Mall::select('merchants.merchant_id', 'merchants.name', 'merchants.ci_domain', 'merchants.city', 'merchants.description', DB::raw("CONCAT({$prefix}merchants.ci_domain, '/customer/tenant?id=', oms.merchant_id) as store_url"))
-                    ->join(DB::raw("(select merchant_id, `name`, parent_id from {$prefix}merchants where name = {$this->quote($storename)}) as oms"), DB::raw('oms.parent_id'), '=', 'merchants.merchant_id')
+                    ->join(DB::raw("(select merchant_id, `name`, parent_id from {$prefix}merchants where name = {$this->quote($storename)} and status = 'active') as oms"), DB::raw('oms.parent_id'), '=', 'merchants.merchant_id')
                     ->active();
 
             // Query list mall based on keyword. Handling description and keyword can be different with other stores
@@ -210,9 +210,11 @@ class StoreAPIController extends ControllerAPI
                                             left join {$prefix}keyword_object on {$prefix}merchants.merchant_id = {$prefix}keyword_object.object_id
                                             left join {$prefix}keywords on {$prefix}keyword_object.keyword_id = {$prefix}keywords.keyword_id
                                             where name = {$this->quote($storename)}
+                                            and {$prefix}merchants.status = 'active'
                                             and (({$prefix}merchants.name like '%|{$value}%' escape '|' or {$prefix}merchants.description like '%|{$value}%' escape '|' or {$prefix}keywords.keyword like '%|{$value}%' escape '|'))
                                         ) as oms";
                     } else {
+                        // escaping the query
                         $word = '%' . $value . '%';
                         $value = $this->quote($word);
 
@@ -220,6 +222,7 @@ class StoreAPIController extends ControllerAPI
                                             left join {$prefix}keyword_object on {$prefix}merchants.merchant_id = {$prefix}keyword_object.object_id
                                             left join {$prefix}keywords on {$prefix}keyword_object.keyword_id = {$prefix}keywords.keyword_id
                                             where name = {$this->quote($storename)}
+                                            and {$prefix}merchants.status = 'active'
                                             and (({$prefix}merchants.name like {$value} or {$prefix}merchants.description like {$value} or {$prefix}keywords.keyword like {$value}))
                                         ) as oms";
                     }
