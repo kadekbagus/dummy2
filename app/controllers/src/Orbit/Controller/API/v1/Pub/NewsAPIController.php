85d3919245d84d2e4bf163264270d2e2ddc03e5a
Ahmad <ahmad@dominopos.com>
2016-08-29 17:19:37 +0800

    Add new controller to get news detail and get news locations

--- app/controllers/src/Orbit/Controller/API/v1/Pub/NewsAPIController.php
+++ app/controllers/src/Orbit/Controller/API/v1/Pub/NewsAPIController.php
@@ -304,6 +304,248 @@ class NewsAPIController extends ControllerAPI
         return $this->render($httpCode);
     }
 
+    public function getNewsItem()
+    {
+        $httpCode = 200;
+        $this->response = new ResponseProvider();
+
+        try{
+            // Get language_if of english
+            $languageEnId = null;
+            $language = Language::where('name', 'en')->first();
+
+            if (! empty($language)) {
+                $languageEnId = $language->language_id;
+            }
+
+            $newsId = OrbitInput::get('news_id', null);
+            $sort_by = OrbitInput::get('sortby', 'name');
+            $sort_mode = OrbitInput::get('sortmode','asc');
+
+
+            $validator = Validator::make(
+                array(
+                    'news_id' => $newsId,
+                ),
+                array(
+                    'news_id' => 'required',
+                ),
+                array(
+                    'required' => 'News ID is required',
+                )
+            );
+
+            // Run the validation
+            if ($validator->fails()) {
+                $errorMessage = $validator->messages()->first();
+                OrbitShopAPI::throwInvalidArgument($errorMessage);
+            }
+
+            $prefix = DB::getTablePrefix();
+
+            $news = News::select(
+                        'news.news_id as news_id',
+                        'news_translations.news_name as news_name',
+                        'news.object_type',
+                        'news_translations.description as description',
+                        'news.end_date',
+                        'media.path',
+                        // query for get status active based on timezone
+                        DB::raw("
+                                CASE WHEN {$prefix}campaign_status.campaign_status_name = 'expired'
+                                        THEN {$prefix}campaign_status.campaign_status_name
+                                        ELSE (CASE WHEN {$prefix}news.end_date < (SELECT min(CONVERT_TZ(UTC_TIMESTAMP(), '+00:00', ot.timezone_name))
+                                                                                    FROM {$prefix}news_merchant onm
+                                                                                        LEFT JOIN {$prefix}merchants om ON om.merchant_id = onm.merchant_id
+                                                                                        LEFT JOIN {$prefix}merchants oms on oms.merchant_id = om.parent_id
+                                                                                        LEFT JOIN {$prefix}timezones ot ON ot.timezone_id = (CASE WHEN om.object_type = 'tenant' THEN oms.timezone_id ELSE om.timezone_id END)
+                                                                                    WHERE onm.news_id = {$prefix}news.news_id)
+                                THEN 'expired' ELSE {$prefix}campaign_status.campaign_status_name END) END AS campaign_status,
+                                CASE WHEN (SELECT count(onm.merchant_id)
+                                            FROM {$prefix}news_merchant onm
+                                                LEFT JOIN {$prefix}merchants om ON om.merchant_id = onm.merchant_id
+                                                LEFT JOIN {$prefix}merchants oms on oms.merchant_id = om.parent_id
+                                                LEFT JOIN {$prefix}timezones ot ON ot.timezone_id = (CASE WHEN om.object_type = 'tenant' THEN oms.timezone_id ELSE om.timezone_id END)
+                                            WHERE onm.news_id = {$prefix}news.news_id
+                                            AND CONVERT_TZ(UTC_TIMESTAMP(), '+00:00', ot.timezone_name) between {$prefix}news.begin_date and {$prefix}news.end_date) > 0
+                                THEN 'true' ELSE 'false' END AS is_started
+                            "))
+                        ->join('news_translations', 'news_translations.news_id', '=', 'news.news_id')
+                        ->leftJoin('campaign_status', 'campaign_status.campaign_status_id', '=', 'news.campaign_status_id')
+                        ->leftJoin('media', 'media.object_id', '=', 'news_translations.news_translation_id')
+                        ->where('news.news_id', $newsId)
+                        ->where('news_translations.merchant_language_id', '=', $languageEnId)
+                        ->where('media.media_name_long', 'news_translation_image_orig')
+                        ->where('news.object_type', '=', 'news')
+                        ->where('news_translations.news_name', '!=', '')
+                        ->havingRaw("campaign_status = 'ongoing' AND is_started = 'true'")
+                        ->first();
+
+            $message = 'Request Ok';
+            if (! is_object($news)) {
+                OrbitShopAPI::throwInvalidArgument('News that you specify is not found');
+            }
+
+            $this->response->data = $news;
+            $this->response->code = 0;
+            $this->response->status = 'success';
+            $this->response->message = $message;
+
+        } catch (ACLForbiddenException $e) {
+            $this->response->code = $e->getCode();
+            $this->response->status = 'error';
+            $this->response->message = $e->getMessage();
+            $this->response->data = null;
+            $httpCode = 403;
+
+        } catch (InvalidArgsException $e) {
+            $this->response->code = $e->getCode();
+            $this->response->status = 'error';
+            $this->response->message = $e->getMessage();
+            $result['total_records'] = 0;
+            $result['returned_records'] = 0;
+            $result['records'] = null;
+
+            $this->response->data = $result;
+            $httpCode = 403;
+
+        } catch (QueryException $e) {
+            $this->response->code = $e->getCode();
+            $this->response->status = 'error';
+            // Only shows full query error when we are in debug mode
+            if (Config::get('app.debug')) {
+                $this->response->message = $e->getMessage();
+            } else {
+                $this->response->message = Lang::get('validation.orbit.queryerror');
+            }
+            $this->response->data = null;
+            $httpCode = 500;
+
+        } catch (Exception $e) {
+            $this->response->code = $this->getNonZeroCode($e->getCode());
+            $this->response->status = 'error';
+            $this->response->message = $e->getMessage();
+            $this->response->data = null;
+            $httpCode = 500;
+
+        }
+
+        return $this->render($httpCode);
+    }
+
+    public function getNewsLocations()
+    {
+        $httpCode = 200;
+        $this->response = new ResponseProvider();
+
+        try{
+            $newsId = OrbitInput::get('news_id', null);
+            $sort_by = OrbitInput::get('sortby', 'name');
+            $sort_mode = OrbitInput::get('sortmode','asc');
+
+
+            $validator = Validator::make(
+                array(
+                    'news_id' => $newsId,
+                ),
+                array(
+                    'news_id' => 'required',
+                ),
+                array(
+                    'required' => 'News ID is required',
+                )
+            );
+
+            // Run the validation
+            if ($validator->fails()) {
+                $errorMessage = $validator->messages()->first();
+                OrbitShopAPI::throwInvalidArgument($errorMessage);
+            }
+
+            $prefix = DB::getTablePrefix();
+
+            $news = NewsMerchant::select('news.begin_date as begin_date', 'news.end_date as end_date',
+                                            DB::raw("CASE WHEN {$prefix}merchants.object_type = 'tenant' THEN {$prefix}merchants.merchant_id ELSE oms.merchant_id END as merchant_id"),
+                                            DB::raw("CASE WHEN {$prefix}merchants.object_type = 'tenant' THEN CONCAT({$prefix}merchants.name, ' at ', oms.name) ELSE CONCAT('Customer Service at ', {$prefix}merchants.name) END as name"),
+                                            DB::raw("CONCAT(IF({$prefix}merchants.object_type = 'tenant', oms.ci_domain, {$prefix}merchants.ci_domain), '/customer/mallnewsdetail?id=', {$prefix}news_merchant.news_id) as news_url"),
+                                            DB::raw("( SELECT CONVERT_TZ(UTC_TIMESTAMP(),'+00:00', ot.timezone_name)
+                                                        FROM {$prefix}merchants om
+                                                        LEFT JOIN {$prefix}timezones ot on ot.timezone_id = om.timezone_id
+                                                        WHERE om.merchant_id = (CASE WHEN {$prefix}merchants.object_type = 'tenant' THEN oms.merchant_id ELSE {$prefix}merchants.merchant_id END)
+                                                    ) as tz")
+                                        )
+                                    ->leftJoin('news', 'news_merchant.news_id', '=', 'news.news_id')
+                                    ->leftJoin('merchants', 'merchants.merchant_id', '=', 'news_merchant.merchant_id')
+                                    ->leftJoin(DB::raw("{$prefix}merchants as oms"), DB::raw('oms.merchant_id'), '=', 'merchants.parent_id')
+                                    ->where('news_merchant.news_id', '=', $newsId)
+                                    ->groupBy('merchant_id')
+                                    ->havingRaw('tz <= end_date AND tz >= begin_date');
+
+            $_news = clone($news);
+
+            $take = PaginationNumber::parseTakeFromGet('news');
+            $news->take($take);
+
+            $skip = PaginationNumber::parseSkipFromGet();
+            $news->skip($skip);
+
+            $news->orderBy('name', 'asc');
+
+            $totalRec = count($_news->get());
+            $listOfRec = $news->get();
+
+            $data = new \stdclass();
+            $data->returned_records = count($listOfRec);
+            $data->total_records = RecordCounter::create($_news)->count();
+            $data->records = $listOfRec;
+
+            $this->response->data = $data;
+            $this->response->code = 0;
+            $this->response->status = 'success';
+            $this->response->message = 'Request Ok';
+
+        } catch (ACLForbiddenException $e) {
+            $this->response->code = $e->getCode();
+            $this->response->status = 'error';
+            $this->response->message = $e->getMessage();
+            $this->response->data = null;
+            $httpCode = 403;
+
+        } catch (InvalidArgsException $e) {
+            $this->response->code = $e->getCode();
+            $this->response->status = 'error';
+            $this->response->message = $e->getMessage();
+            $result['total_records'] = 0;
+            $result['returned_records'] = 0;
+            $result['records'] = null;
+
+            $this->response->data = $result;
+            $httpCode = 403;
+
+        } catch (QueryException $e) {
+            $this->response->code = $e->getCode();
+            $this->response->status = 'error';
+            // Only shows full query error when we are in debug mode
+            if (Config::get('app.debug')) {
+                $this->response->message = $e->getMessage();
+            } else {
+                $this->response->message = Lang::get('validation.orbit.queryerror');
+            }
+            $this->response->data = null;
+            $httpCode = 500;
+
+        } catch (Exception $e) {
+            $this->response->code = $this->getNonZeroCode($e->getCode());
+            $this->response->status = 'error';
+            $this->response->message = $e->getMessage();
+            $this->response->data = null;
+            $httpCode = 500;
+
+        }
+
+        return $this->render($httpCode);
+    }
+
     protected function quote($arg)
     {
         return DB::connection()->getPdo()->quote($arg);
