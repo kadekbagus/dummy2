4b910bfab4ae4173090644ebd950b129bd24aedc
Rio Astamal <rio@dominopos.com>
2016-09-09 20:28:27 +0800

    New command: Elasticsearch Mall Logo Update

---
+++ app/commands/ElasticsearchUpdateMallLogoCommand.php
@@ -0,0 +1,117 @@
+<?php
+
+use Illuminate\Console\Command;
+use Symfony\Component\Console\Input\InputOption;
+use Symfony\Component\Console\Input\InputArgument;
+use Elasticsearch\ClientBuilder as ESBuilder;
+
+class ElasticsearchUpdateMallLogoCommand extends Command
+{
+
+    protected $poster = NULL;
+
+    /**
+     * The console command name.
+     *
+     * @var string
+     */
+    protected $name = 'elasticsearch:update-mall-logo';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Update logo field in mall index';
+
+    /**
+     * Create a new command instance.
+     *
+     * @return void
+     */
+    public function __construct($poster = 'default')
+    {
+        parent::__construct();
+        if ($poster === 'default') {
+            $this->poster = ESBuilder::create()
+                                     ->setHosts(Config::get('orbit.elasticsearch.hosts'))
+                                     ->build();
+        } else {
+            $this->poster = $poster;
+        }
+    }
+
+    /**
+     * Execute the console command.
+     *
+     * @return mixed
+     */
+    public function fire()
+    {
+        $prefix = DB::getTablePrefix();
+        $esPrefix = Config::get('orbit.elasticsearch.indices_prefix');
+        $malls = Mall::active()->get();
+
+        foreach ($malls as $mall) {
+            try {
+                // check exist elasticsearch index
+                $params_search = [
+                    'index' => $esPrefix . Config::get('orbit.elasticsearch.indices.malldata.index'),
+                    'type' => Config::get('orbit.elasticsearch.indices.malldata.type'),
+                    'body' => [
+                        'query' => [
+                            'match' => [
+                                '_id' => $mall->merchant_id
+                            ]
+                        ]
+                    ]
+                ];
+
+                $params = [
+                    'index' => $esPrefix . Config::get('orbit.elasticsearch.indices.malldata.index'),
+                    'type' => Config::get('orbit.elasticsearch.indices.malldata.type'),
+                    'id' => $mall->merchant_id,
+                    'body' => []
+                ];
+
+                $response_search = $this->poster->search($params_search);
+                if ($response_search['hits']['total'] > 0) {
+                    if (! isset($mall->mediaLogoOrig[0])) {
+                        throw new Exception (sprintf('Logo of mall %s not found.' . "\n", $mall->name));
+                    }
+
+                    $params['body']['doc']['logo_url'] = $mall->mediaLogoOrig[0]->path;
+                    $response = $this->poster->update($params);
+
+                    $this->info('Updating logo of mall ' . $mall->name . '... OK');
+                } else {
+                    throw new Exception("Mall %s not found in Elasticsearch.\n", $mall->name);
+                }
+            } catch (Exception $e) {
+                $this->error('Updating logo of mall ' . $mall->name . '... FAILED.' . "\n  Message: {$e->getMessage()}");
+            }
+        }
+    }
+
+    /**
+     * Get the console command arguments.
+     *
+     * @return array
+     */
+    protected function getArguments()
+    {
+        return array(
+        );
+    }
+
+    /**
+     * Get the console command options.
+     *
+     * @return array
+     */
+    protected function getOptions()
+    {
+        return array(
+        );
+    }
+}
