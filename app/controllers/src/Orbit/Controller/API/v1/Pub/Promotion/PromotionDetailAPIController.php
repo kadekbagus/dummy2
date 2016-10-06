2e177cc877a6c3db6ad83b3b3cbc837944c41643
kadekbagus <kadek@dominopos.com>
2016-10-06 11:16:02 +0800

    refactor PromotionAPIController into smaller controller

---
+++ app/controllers/src/Orbit/Controller/API/v1/Pub/Promotion/PromotionDetailAPIController.php
@@ -0,0 +1,190 @@
+<?php namespace Orbit\Controller\API\v1\Pub\Promotion;
+
+/**
+ * @author firmansyah <firmansyah@dominopos.com>
+ * @desc Controller for promotion list and search in landing page
+ */
+
+use OrbitShop\API\v1\ControllerAPI;
+use OrbitShop\API\v1\OrbitShopAPI;
+use OrbitShop\API\v1\ResponseProvider;
+use Helper\EloquentRecordCounter as RecordCounter;
+use OrbitShop\API\v1\Helper\Input as OrbitInput;
+use \Config;
+use \Exception;
+use DominoPOS\OrbitACL\ACL;
+use DominoPOS\OrbitACL\Exception\ACLForbiddenException;
+use \DB;
+use \URL;
+use News;
+use NewsMerchant;
+use Language;
+use Validator;
+use Orbit\Helper\Util\PaginationNumber;
+use Activity;
+use Orbit\Helper\Net\SessionPreparer;
+use Orbit\Helper\Session\UserGetter;
+use Orbit\Controller\API\v1\Pub\SocMedAPIController;
+use Orbit\Controller\API\v1\Pub\Promotion\PromotionHelper;
+use Mall;
+
+class PromotionDetailAPIController extends ControllerAPI
+{
+     public function getPromotionItem()
+    {
+        $httpCode = 200;
+        $this->response = new ResponseProvider();
+        $activity = Activity::mobileci()->setActivityType('view');
+        $user = NULL;
+
+        try{
+            $this->session = SessionPreparer::prepareSession();
+            $user = UserGetter::getLoggedInUserOrGuest($this->session);
+
+            $promotionId = OrbitInput::get('promotion_id', null);
+            $sort_by = OrbitInput::get('sortby', 'name');
+            $sort_mode = OrbitInput::get('sortmode','asc');
+            $language = OrbitInput::get('language', 'id');
+
+            $promotionHelper = PromotionHelper::create();
+            $promotionHelper->registerCustomValidation();
+            $validator = Validator::make(
+                array(
+                    'promotion_id' => $promotionId,
+                    'language' => $language,
+                ),
+                array(
+                    'promotion_id' => 'required',
+                    'language' => 'required|orbit.empty.language_default',
+                ),
+                array(
+                    'required' => 'Promotion ID is required',
+                )
+            );
+
+            // Run the validation
+            if ($validator->fails()) {
+                $errorMessage = $validator->messages()->first();
+                OrbitShopAPI::throwInvalidArgument($errorMessage);
+            }
+
+            $valid_language = $promotionHelper->getValidLanguage();
+
+            $prefix = DB::getTablePrefix();
+
+            $promotion = News::select(
+                            'news.news_id as news_id',
+                            DB::Raw("
+                                CASE WHEN {$prefix}news_translations.news_name = '' THEN {$prefix}news.news_name ELSE {$prefix}news_translations.news_name END as news_name,
+                                CASE WHEN {$prefix}news_translations.description = '' THEN {$prefix}news.description ELSE {$prefix}news_translations.description END as description,
+                                CASE WHEN {$prefix}media.path is null THEN (
+                                        select m.path
+                                        from {$prefix}news_translations nt
+                                        join {$prefix}media m
+                                            on m.object_id = nt.news_translation_id
+                                            and m.media_name_long = 'news_translation_image_orig'
+                                        where nt.news_id = {$prefix}news.news_id
+                                        group by nt.news_id
+                                    ) ELSE {$prefix}media.path END as original_media_path
+                            "),
+                            'news.object_type',
+                            'news.end_date',
+                            // query for get status active based on timezone
+                            DB::raw("
+                                    CASE WHEN {$prefix}campaign_status.campaign_status_name = 'expired'
+                                            THEN {$prefix}campaign_status.campaign_status_name
+                                            ELSE (CASE WHEN {$prefix}news.end_date < (SELECT min(CONVERT_TZ(UTC_TIMESTAMP(), '+00:00', ot.timezone_name))
+                                                                                        FROM {$prefix}news_merchant onm
+                                                                                            LEFT JOIN {$prefix}merchants om ON om.merchant_id = onm.merchant_id
+                                                                                            LEFT JOIN {$prefix}merchants oms on oms.merchant_id = om.parent_id
+                                                                                            LEFT JOIN {$prefix}timezones ot ON ot.timezone_id = (CASE WHEN om.object_type = 'tenant' THEN oms.timezone_id ELSE om.timezone_id END)
+                                                                                        WHERE onm.news_id = {$prefix}news.news_id)
+                                    THEN 'expired' ELSE {$prefix}campaign_status.campaign_status_name END) END AS campaign_status,
+                                    CASE WHEN (SELECT count(onm.merchant_id)
+                                                FROM {$prefix}news_merchant onm
+                                                    LEFT JOIN {$prefix}merchants om ON om.merchant_id = onm.merchant_id
+                                                    LEFT JOIN {$prefix}merchants oms on oms.merchant_id = om.parent_id
+                                                    LEFT JOIN {$prefix}timezones ot ON ot.timezone_id = (CASE WHEN om.object_type = 'tenant' THEN oms.timezone_id ELSE om.timezone_id END)
+                                                WHERE onm.news_id = {$prefix}news.news_id
+                                                AND CONVERT_TZ(UTC_TIMESTAMP(), '+00:00', ot.timezone_name) between {$prefix}news.begin_date and {$prefix}news.end_date) > 0
+                                    THEN 'true' ELSE 'false' END AS is_started
+                            ")
+                        )
+                        ->join('news_translations', 'news_translations.news_id', '=', 'news.news_id')
+                        ->leftJoin('campaign_status', 'campaign_status.campaign_status_id', '=', 'news.campaign_status_id')
+                        ->leftJoin('media', function($q) {
+                            $q->on('media.object_id', '=', 'news_translations.news_translation_id');
+                            $q->on('media.media_name_long', '=', DB::raw("'news_translation_image_orig'"));
+                        })
+                        ->where('news.news_id', $promotionId)
+                        ->where('news_translations.merchant_language_id', '=', $valid_language->language_id)
+                        ->where('news.object_type', '=', 'promotion')
+                        ->havingRaw("campaign_status = 'ongoing' AND is_started = 'true'")
+                        ->first();
+
+            $message = 'Request Ok';
+            if (! is_object($promotion)) {
+                OrbitShopAPI::throwInvalidArgument('Promotion that you specify is not found');
+            }
+
+            $activityNotes = sprintf('Page viewed: Landing Page Promotion Detail Page');
+            $activity->setUser($user)
+                ->setActivityName('view_landing_page_promotion_detail')
+                ->setActivityNameLong('View GoToMalls Promotion Detail')
+                ->setObject($promotion)
+                ->setNews($promotion)
+                ->setModuleName('Promotion')
+                ->setNotes($activityNotes)
+                ->responseOK()
+                ->save();
+
+            // add facebook share url dummy page
+            $promotion->facebook_share_url = SocMedAPIController::getSharedUrl('promotion', $promotion->news_id, $promotion->news_name);
+
+            $this->response->data = $promotion;
+            $this->response->code = 0;
+            $this->response->status = 'success';
+            $this->response->message = $message;
+
+        } catch (ACLForbiddenException $e) {
+            $this->response->code = $e->getCode();
+            $this->response->status = 'error';
+            $this->response->message = $e->getMessage();
+            $this->response->data = null;
+            $httpCode = 403;
+
+        } catch (InvalidArgsException $e) {
+            $this->response->code = $e->getCode();
+            $this->response->status = 'error';
+            $this->response->message = $e->getMessage();
+            $result['total_records'] = 0;
+            $result['returned_records'] = 0;
+            $result['records'] = null;
+
+            $this->response->data = $result;
+            $httpCode = 403;
+
+        } catch (QueryException $e) {
+            $this->response->code = $e->getCode();
+            $this->response->status = 'error';
+            // Only shows full query error when we are in debug mode
+            if (Config::get('app.debug')) {
+                $this->response->message = $e->getMessage();
+            } else {
+                $this->response->message = Lang::get('validation.orbit.queryerror');
+            }
+            $this->response->data = null;
+            $httpCode = 500;
+
+        } catch (Exception $e) {
+            $this->response->code = $this->getNonZeroCode($e->getCode());
+            $this->response->status = 'error';
+            $this->response->message = $e->getMessage();
+            $this->response->data = null;
+            $httpCode = 500;
+
+        }
+
+        return $this->render($httpCode);
+    }
+}
