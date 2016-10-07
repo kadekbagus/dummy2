7ab2bbc3288818543ae8fac9ee49dbb3eec6bd5d
kadekbagus <kadek@dominopos.com>
2016-10-07 14:56:34 +0800

    change wording on activation email

--- app/views/emails/registration/activation-html.blade.php
+++ app/views/emails/registration/activation-html.blade.php
@@ -312,7 +312,7 @@
                                             <td style="word-break:break-word;font-size:0px;padding:10px 25px;" align="center">
                                                 <div style="cursor:auto;color:#000000;font-family:Ubuntu, Helvetica, Arial, sans-serif;font-size:13px;line-height:22px;">
                                                     <p align="justify" style="margin:0">
-                                                        Hi {{{ $first_name or $email }}},
+                                                        Hello {{{ $first_name or $email }}},
                                                         <br \> Terima kasih sudah bergabung dengan Gotomalls! Untuk mengaktifkan akun Anda, silakan klik di bawah ini.
                                                     </p>
                                                 </div>
