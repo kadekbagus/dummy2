71f7a8fbb7d1ec67bb31314874a3538c3dd22095
kadekbagus <kadek@dominopos.com>
2016-10-19 16:46:17 +0800

    finishing share campaign via email api

---
+++ app/views/emails/campaign-share-email/campaign-share-text.blade.php
@@ -0,0 +1,8 @@
+Sudahkah anda melihat gotomalls.com?
+
+Hai {{{ $email }}},
+@if (empty($name))Seorang teman anda @else {{{ $name }}} @endif ingin anda untuk melihat {{{ $campaignType }}}
+
+{{{ $campaignName }}}
+
+{{{ $campaignUrl }}}
