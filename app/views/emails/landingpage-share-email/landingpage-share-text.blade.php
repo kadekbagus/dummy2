6b6e1ca3a2b3ba0a150136c0ba939929e1faafad
kadekbagus <kadek@dominopos.com>
2016-10-25 15:54:41 +0800

    fix email share us (landing page)

--- app/views/emails/landingpage-share-email/landingpage-share-text.blade.php
+++ app/views/emails/landingpage-share-email/landingpage-share-text.blade.php
@@ -1,4 +1,4 @@
-Sudahkah anda mencoba Gotomalls.com ?
+{{{ $subject }}}
 
 Hai {{{ $email }}},
 @if (empty($name))Seorang teman anda @else {{{ $name }}} @endif ingin anda untuk mencoba Gotomalls.com.
