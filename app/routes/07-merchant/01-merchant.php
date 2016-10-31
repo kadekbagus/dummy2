5f6dc4984481ee52e4636aa296ec1560de3ea224
Ahmad <ahmad@dominopos.com>
2016-10-31 15:37:42 +0800

    Split the app/api route

--- app/routes/07-merchant/01-merchant.php
+++ app/routes/07-merchant/01-merchant.php
@@ -3,9 +3,11 @@
 /**
  * List/Search merchant
  */
-Route::get('/{app}/v1/merchant/merchant/{search}', ['as' => 'customer-api-store-detail', 'uses' => 'IntermediateMerchantAuthController@Merchant\MerchantList_getSearchMerchant'])
-	->where([
-		'app' => '(app|api)',
-		'search' => '(list|search)'
-	]
-);
+Route::get('/api/v1/merchant/merchant/{search}', function()
+{
+    return Orbit\Controller\API\v1\Merchant\Merchant\MerchantListAPIController::create()->getSearchMerchant();
+})
+->where('search', '(list|search)');
+
+Route::get('/app/v1/merchant/merchant/{search}', ['as' => 'customer-api-store-detail', 'uses' => 'IntermediateMerchantAuthController@Merchant\MerchantList_getSearchMerchant'])
+	->where('search', '(list|search)');
