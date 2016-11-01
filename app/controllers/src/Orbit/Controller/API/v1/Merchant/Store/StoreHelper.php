eb6b12567356d9ab9c7885ab68d0598194b8913b
Irianto Pratama <irianto@dominopos.com>
2016-11-01 10:58:36 +0800

    Add validation image

--- app/controllers/src/Orbit/Controller/API/v1/Merchant/Store/StoreHelper.php
+++ app/controllers/src/Orbit/Controller/API/v1/Merchant/Store/StoreHelper.php
@@ -7,6 +7,9 @@ use Validator;
 use DB;
 use OrbitShop\API\v1\OrbitShopAPI;
 use OrbitShop\API\v1\Helper\Input as OrbitInput;
+use DominoPOS\OrbitUploader\Uploader as OrbitUploader;
+use Config;
+use Lang;
 use BaseStore;
 use BaseMerchant;
 use Mall;
@@ -25,7 +28,44 @@ class StoreHelper
         return new static();
     }
 
+    public function generate_validation_image($image_name, $images, $config, $max_count = 1) {
+        $validation = [];
+        if (! empty($images)) {
+            $images_properties = OrbitUploader::simplifyFilesVar($images);
+            $image_config = Config::get($config);
+            $image_type =  "image/" . implode(",image/", $image_config['file_type']);
+            $image_units = OrbitUploader::bytesToUnits($image_config['file_size']);
+
+            $validation['data'] = [
+                $image_name => $images
+            ];
+            $validation['error'] = [
+                $image_name => 'nomore.than:' . $max_count
+            ];
+            $validation['error_message'] = [
+                $image_name . '.nomore.than' => Lang::get('validation.max.array', array('max' => $max_count))
+            ];
+
+            foreach ($images_properties as $idx => $image) {
+                $ext = strtolower(substr(strrchr($image->name, '.'), 1));
+                $idx+=1;
+
+                $validation['data'][$image_name . '_type_' . $idx] = $image->type;
+                $validation['data'][$image_name . '_' . $idx . '_size'] = $image->size;
+
+                $validation['error'][$image_name . '_type_' . $idx] = 'in:' . $image_type;
+                $validation['error'][$image_name . '_' . $idx . '_size'] = 'orbit.file.max_size:' . $image_config['file_size'];
+
+                $validation['error_message'][$image_name . '_type_' . $idx . '.in'] = Lang::get('validation.orbit.file.type', array('ext' => $ext));
+                $validation['error_message'][$image_name . '_' . $idx . '_size' . '.orbit.file.max_size'] = Lang::get('validation.orbit.file.max_size', array('name' => $image_config['name'], 'size' => $image_units['newsize'], 'unit' => $image_units['unit']));
+            }
+        }
+
+        return $validation;
+    }
+
     public function storeCustomValidator() {
+        // exist base merchant
         Validator::extend('orbit.empty.base_merchant', function ($attribute, $value, $parameters) {
             $base_merchant = BaseMerchant::excludeDeleted()
                         ->where('base_merchant_id', $value)
@@ -39,6 +79,7 @@ class StoreHelper
             return TRUE;
         });
 
+        // exist mall
         Validator::extend('orbit.empty.mall', function ($attribute, $value, $parameters) {
             $mall = Mall::excludeDeleted()
                         ->where('merchant_id', $value)
@@ -53,6 +94,7 @@ class StoreHelper
             return TRUE;
         });
 
+        // exist floor
         Validator::extend('orbit.empty.floor', function ($attribute, $value, $parameters) {
             $mall_id = $parameters[0];
             $floor = Object::excludeDeleted()
@@ -66,6 +108,28 @@ class StoreHelper
             }
 
             $valid_floor = $floor;
+            return TRUE;
+        });
+
+        Validator::extend('orbit.file.max_size', function ($attribute, $value, $parameters) {
+            $config_size = $parameters[0];
+            $file_size = $value;
+
+            if ($file_size > $config_size) {
+                return false;
+            }
+
+            return true;
+        });
+
+        // Check the images, we are allowed array of images but not more than
+        Validator::extend('nomore.than', function ($attribute, $value, $parameters) {
+            $max_count = $parameters[0];
+
+            if (is_array($value['name']) && count($value['name']) > $max_count) {
+                return FALSE;
+            }
+
             return TRUE;
         });
     }
