08baf125569fd7a8fb5d1f22a9bceb653fa8eb02
Rio Astamal <rio@dominopos.com>
2016-11-07 20:13:47 +0800

    Bump version 2.11.7

--- app/orbit_version.php
+++ app/orbit_version.php
@@ -23,9 +23,9 @@ if (! defined('ORBIT_APP_BUILD_NUMBER')) {
  */
 if (! defined('ORBIT_APP_VERSION')) {
     if (Config::get('app.debug')) {
-        define('ORBIT_APP_VERSION', '2.11.6-dev b' . ORBIT_APP_BUILD_NUMBER);
+        define('ORBIT_APP_VERSION', '2.11.7-dev b' . ORBIT_APP_BUILD_NUMBER);
     } else {
-        define('ORBIT_APP_VERSION', '2.11.6');
+        define('ORBIT_APP_VERSION', '2.11.7');
     }
 }
 
