46904a0efb0cb70fe79c4e54ed2b8624ce87f34c
firmansyah <firmansyah@dominopos.com>
2016-11-10 16:47:31 +0800

    Model for provide advert feature

---
+++ app/models/Advert.php
@@ -0,0 +1,35 @@
+<?php
+class Advert extends Eloquent
+{
+    /**
+     * Advert Model
+     *
+     * @author Firmansyah <firmansyah@dominopos.com>
+     */
+
+    /**
+     * Import trait ModelStatusTrait so we can use some common scope dealing
+     * with `status` field.
+     */
+    use ModelStatusTrait;
+
+    protected $table = 'advert';
+    protected $primaryKey = 'advert_id';
+
+    public function creator()
+    {
+        return $this->belongsTo('User', 'created_by', 'user_id');
+    }
+
+    public function modifier()
+    {
+        return $this->belongsTo('User', 'modified_by', 'user_id');
+    }
+
+    public function media()
+    {
+        return $this->hasMany('Media', 'object_id', 'advert_id')
+                    ->where('object_name', 'advert');
+    }
+
+}
