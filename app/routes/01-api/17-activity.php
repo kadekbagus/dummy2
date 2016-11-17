359087b41e2abe2bf35693c302d5c15745a6529d
Ahmad <ahmad@dominopos.com>
2016-11-17 11:42:19 +0800

    Refactor Pub\GenericActivityAPIController namespace regarding Signature auth

--- app/routes/01-api/17-activity.php
+++ app/routes/01-api/17-activity.php
@@ -115,13 +115,12 @@ Route::get('/api/v1/activity/groups', function()
 /**
  * Post new generic activity
  */
-Route::post(
-    '/{search}/v1/pub/generic-activity/new', ['as' => 'generic-activity-new', function()
-    {
-        return Orbit\Controller\API\v1\Pub\GenericActivityAPIController::create()->postNewGenericActivity();
-    }]
-)->where('search', '(api|app)');
+Route::post('/api/v1/pub/generic-activity/new', function()
+{
+    return Orbit\Controller\API\v1\Pub\GenericActivityAPIController::create()->postNewGenericActivity();
+});
 
+Route::post('/app/v1/pub/generic-activity/new', ['as' => 'generic-activity-new', 'uses' => 'IntermediatePubAuthController@GenericActivity_postNewGenericActivity']);
 
 /**
  * Get generic activity list
