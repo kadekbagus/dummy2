1480c08e6ec0634b53440a0fbcdc3c6352ad1571
firmansyah <firmansyah@dominopos.com>
2016-12-05 16:45:45 +0800

    Update partner

--- app/routes/01-api/44-partner.php
+++ app/routes/01-api/44-partner.php
@@ -11,6 +11,16 @@ Route::post('/api/v1/partner/new', function()
     return PartnerAPIController::create()->postNewPartner();
 });
 
+
+/**
+ * Update partner
+ */
+Route::post('/api/v1/partner/update', function()
+{
+    return PartnerAPIController::create()->postUpdatePartner();
+});
+
+
 /**
  * Get search partner
  */
--- app/routes/02-intermediate/44-partner.php
+++ app/routes/02-intermediate/44-partner.php
@@ -9,7 +9,11 @@
 Route::post('/app/v1/partner/new', 'IntermediateAuthController@Partner_postNewPartner');
 
 /**
+ * Update partner
+ */
+Route::post('/app/v1/partner/update', 'IntermediateAuthController@Partner_postUpdatePartner');
+
+/**
  * Get search partner
  */
 Route::get('/app/v1/partner/list', 'IntermediateAuthController@Partner_getSearchPartner');
-
