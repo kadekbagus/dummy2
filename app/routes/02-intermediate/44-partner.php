c50e0f1f386caf9e700ea16a2654eaec11f386de
kadekbagus <kadek@dominopos.com>
2016-12-06 11:38:27 +0800

    route for postDeletePartnerImage and postDeletePartnerLogo

--- app/routes/02-intermediate/44-partner.php
+++ app/routes/02-intermediate/44-partner.php
@@ -22,3 +22,23 @@ Route::get('/app/v1/partner/list', 'IntermediateAuthController@Partner_getSearch
  * Pub get partner detail
  */
 Route::get('/app/v1/pub/partner/detail', ['as' => 'pub-partner-detail', 'uses' => 'IntermediatePubAuthController@Partner\PartnerDetail_getPartnerDetail']);
+
+/**
+ * Upload partner Logo
+ */
+Route::post('/app/v1/partner/upload/logo', 'IntermediateAuthController@Upload_postUploadPartnerLogo');
+
+/**
+ * Upload partner Image
+ */
+Route::post('/app/v1/partner/upload/image', 'IntermediateAuthController@Upload_postUploadPartnerImage');
+
+/**
+ * Delete partner Logo
+ */
+Route::post('/app/v1/partner/delete/logo', 'IntermediateAuthController@Upload_postDeletePartnerLogo');
+
+/**
+ * Delete partner Logo
+ */
+Route::post('/app/v1/partner/delete/image', 'IntermediateAuthController@Upload_postDeletePartnerImage');
