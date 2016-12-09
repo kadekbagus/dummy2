665a2396404fdc855165fa5efd7afa2cf0315b9a
Irianto Pratama <irianto@dominopos.com>
2016-12-09 17:13:12 +0800

    Fix logo and image validation message

--- app/controllers/api/v1/PartnerAPIController.php
+++ app/controllers/api/v1/PartnerAPIController.php
@@ -1105,7 +1105,7 @@ class PartnerAPIController extends ControllerAPI
                 $validation['error'][$image_name . '_' . $idx . '_size'] = 'orbit.file.max_size:' . $image_config['file_size'];
 
                 $validation['error_message'][$image_name . '_' . $idx . '_type' . '.in'] = Lang::get('validation.orbit.file.type', array('ext' => $ext));
-                $validation['error_message'][$image_name . '_' . $idx . '_size' . '.orbit.file.max_size'] = ($max_count > 1) ? Lang::get('validation.orbit.file.max_size', array('size' => $image_units['newsize'], 'unit' => $image_units['unit'])) : Lang::get('validation.orbit.file.max_size_one', array('name' => str_replace('_', ' ', $image_name), 'size' => $image_units['newsize'], 'unit' => $image_units['unit']));
+                $validation['error_message'][$image_name . '_' . $idx . '_size' . '.orbit.file.max_size'] = ($max_count > 1) ? Lang::get('validation.orbit.file.max_size', array('size' => $image_units['newsize'], 'unit' => $image_units['unit'])) : Lang::get('validation.orbit.file.max_size_one', array('name' => ucfirst(str_replace('_', ' ', $image_name)), 'size' => $image_units['newsize'], 'unit' => $image_units['unit']));
             }
         }
 
