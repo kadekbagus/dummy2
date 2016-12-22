5f6ba8fa271bf264b5974dfafed3591ac5211cff
firmansyah <firmansyah@dominopos.com>
2016-12-22 18:01:37 +0800

    Fix list location for coupon and event in mall level

--- app/controllers/src/Orbit/Controller/API/v1/Pub/Coupon/CouponLocationAPIController.php
+++ app/controllers/src/Orbit/Controller/API/v1/Pub/Coupon/CouponLocationAPIController.php
@@ -136,13 +136,11 @@ class CouponLocationAPIController extends PubControllerAPI
             // filter news by mall id
             $group_by = '';
             OrbitInput::get('mall_id', function($mallid) use ($is_detail, $couponLocations, &$group_by) {
-                if ($is_detail != 'y') {
-                    $couponLocations->where(function($q) use ($mallid){
-                                        $q->where('merchants.parent_id', '=', $mallid)
-                                          ->orWhere('merchants.merchant_id', '=', $mallid);
-                                    });
-                    $group_by = 'mall';
-                }
+                $couponLocations->where(function($q) use ($mallid){
+                                    $q->where('merchants.parent_id', '=', $mallid)
+                                      ->orWhere('merchants.merchant_id', '=', $mallid);
+                                });
+                $group_by = 'mall';
             });
 
             OrbitInput::get('location', function($location) use ($couponLocations, $userLocationCookieName, $ul, $distance, $prefix) {
