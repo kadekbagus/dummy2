c7b39f26649bda648eee0e789822b2ba8fba4416
firmansyah <firmansyah@dominopos.com>
2016-12-27 16:27:36 +0800

    Fix list filter location detail of event for invisible and incative mall

--- app/controllers/src/Orbit/Controller/API/v1/Pub/News/NewsLocationAPIController.php
+++ app/controllers/src/Orbit/Controller/API/v1/Pub/News/NewsLocationAPIController.php
@@ -98,6 +98,7 @@ class NewsLocationAPIController extends PubControllerAPI
                                         DB::raw("CASE WHEN {$prefix}merchants.object_type = 'tenant' THEN {$prefix}merchants.floor ELSE '' END as floor"),
                                         DB::raw("CASE WHEN {$prefix}merchants.object_type = 'tenant' THEN {$prefix}merchants.unit ELSE '' END as unit"),
                                         DB::raw("CASE WHEN {$prefix}merchants.object_type = 'tenant' THEN oms.operating_hours ELSE '' END as operating_hours"),
+                                        DB::raw("CASE WHEN {$prefix}merchants.object_type = 'tenant' THEN oms.is_subscribed ELSE {$prefix}merchants.is_subscribed END as is_subscribed"),
                                         DB::raw("{$prefix}merchants.object_type as location_type"),
                                         DB::raw("img.path as location_logo"),
                                         DB::raw("map.path as map_image"),
@@ -128,7 +129,9 @@ class NewsLocationAPIController extends PubControllerAPI
                                             "))
                                             ->on(DB::raw('img.media_name_long'), 'IN', DB::raw("('mall_logo_orig', 'retailer_logo_orig')"));
                                     })
-                                    ->where('news_merchant.news_id', '=', $newsId);
+                                    ->where('news_merchant.news_id', '=', $newsId)
+                                    ->where('merchants.status', '=', 'active')
+                                    ->where(DB::raw('oms.status'), '=', 'active');
 
             // filter news by mall id
             $group_by = '';
