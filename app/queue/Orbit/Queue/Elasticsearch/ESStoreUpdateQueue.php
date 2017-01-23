6eb07e6982f4bcf19d3602e1ee7d519f48b23aba
Irianto Pratama <irianto@dominopos.com>
2017-01-23 16:40:47 +0800

    Fix queue update es coupon when update tenant

--- app/queue/Orbit/Queue/Elasticsearch/ESStoreUpdateQueue.php
+++ app/queue/Orbit/Queue/Elasticsearch/ESStoreUpdateQueue.php
@@ -12,6 +12,8 @@ use Orbit\Helper\Elasticsearch\ElasticsearchErrorChecker;
 use Orbit\Helper\Util\JobBurier;
 use Exception;
 use Log;
+use Queue;
+use Orbit\FakeJob;
 
 class ESStoreUpdateQueue
 {
@@ -213,6 +215,9 @@ class ESStoreUpdateQueue
             // The indexing considered successful is attribute `successful` on `_shard` is more than 0.
             ElasticsearchErrorChecker::throwExceptionOnDocumentError($response);
 
+            // update es coupon, news, and promotion
+            $this->updateESCoupon($store);
+
             // Safely delete the object
             $job->delete();
 
@@ -248,5 +253,51 @@ class ESStoreUpdateQueue
             'status' => 'fail',
             'message' => $message
         ];
+    }
+
+    protected function updateESCoupon($stores) {
+        foreach ($stores as $key => $store) {
+            $fakeJob = new FakeJob();
+
+            // find coupon relate with tenant to update ESCoupon
+            // check coupon before update elasticsearch
+            $prefix = DB::getTablePrefix();
+            $coupons = \Coupon::excludeDeleted('promotions')
+                        ->select(DB::raw("
+                            {$prefix}promotions.promotion_id,
+                            CASE WHEN {$prefix}campaign_status.campaign_status_name = 'expired'
+                                THEN {$prefix}campaign_status.campaign_status_name
+                                ELSE (CASE WHEN {$prefix}promotions.end_date < (SELECT min(CONVERT_TZ(UTC_TIMESTAMP(), '+00:00', ot.timezone_name))
+                                                                                FROM {$prefix}promotion_retailer opt
+                                                                                    LEFT JOIN {$prefix}merchants om ON om.merchant_id = opt.retailer_id
+                                                                                    LEFT JOIN {$prefix}merchants oms on oms.merchant_id = om.parent_id
+                                                                                    LEFT JOIN {$prefix}timezones ot ON ot.timezone_id = (CASE WHEN om.object_type = 'tenant' THEN oms.timezone_id ELSE om.timezone_id END)
+                                                                                WHERE opt.promotion_id = {$prefix}promotions.promotion_id
+                                                                            )
+                                THEN 'expired' ELSE {$prefix}campaign_status.campaign_status_name END)
+                            END AS campaign_status,
+                            COUNT({$prefix}issued_coupons.issued_coupon_id) as available
+                        "))
+                        ->join('promotion_rules', 'promotion_rules.promotion_id', '=', 'promotions.promotion_id')
+                        ->join('campaign_status', 'promotions.campaign_status_id', '=', 'campaign_status.campaign_status_id')
+                        ->leftJoin('issued_coupons', function($q) {
+                            $q->on('issued_coupons.promotion_id', '=', 'promotions.promotion_id')
+                                ->where('issued_coupons.status', '=', "available");
+                        })
+                        ->join('promotion_retailer', function($q) {
+                            $q->on('promotion_retailer.promotion_id', '=', 'promotions.promotion_id')
+                              ->on('promotion_retailer.object_type', '=', DB::raw("'tenant'"));
+                        })
+                        ->where('promotion_retailer.retailer_id', '=', $store->merchant_id)
+                        ->whereRaw("{$prefix}promotions.is_coupon = 'Y'")
+                        ->whereRaw("{$prefix}promotion_rules.rule_type != 'blast_via_sms'")
+                        ->groupBy('promotions.promotion_id')
+                        ->get();
+
+            foreach ($coupons as $key => $coupon) {
+                $esQueue = new \Orbit\Queue\Elasticsearch\ESCouponUpdateQueue();
+                $response = $esQueue->fire($fakeJob, ['coupon_id' => $coupon->promotion_id]);
+            }
+        }
     }
 }
