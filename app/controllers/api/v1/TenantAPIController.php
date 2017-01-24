b390ab9e5056b6b1ce865208917c6e5b8e5b590b
Irianto Pratama <irianto@dominopos.com>
2017-01-24 13:46:44 +0800

    Fix link to tenant by campaign id

--- app/controllers/api/v1/TenantAPIController.php
+++ app/controllers/api/v1/TenantAPIController.php
@@ -2271,6 +2271,16 @@ class TenantAPIController extends ControllerAPI
                                        ->where('merchants.status', '!=', 'deleted')
                                        ->whereIn('merchants.object_type', ['mall', 'tenant']);
 
+            if ($from === 'detail') {
+                if ($link_type === 'promotion' || $link_type === 'news') {
+                    $tenants = $tenants->join('news_merchant as nm', DB::raw("nm.merchant_id"), '=', 'merchants.merchant_id')
+                                    ->whereRaw("nm.news_id = {$this->quote($campaign_id)}");
+                } elseif ($link_type === 'coupon') {
+                    $tenants = $tenants->join('promotion_retailer as pr', DB::raw("pr.retailer_id"), '=', 'merchants.merchant_id')
+                                    ->whereRaw("pr.promotion_id = {$this->quote($campaign_id)}");
+                }
+            }
+
             // Need to overide the query for advert
             if ($from === 'advert') {
                 if ($link_type === 'coupon' ) {
