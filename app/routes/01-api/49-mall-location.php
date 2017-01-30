93ce035b573fddc7b1125bc126012c057698aa82
kadekbagus <kadek@dominopos.com>
2017-01-30 15:45:14 +0800

    new api for listing mall city

--- app/routes/01-api/49-mall-location.php
+++ app/routes/01-api/49-mall-location.php
@@ -6,3 +6,11 @@ Route::get('/api/v1/mall-country/{search}', function()
 {
     return MallLocationAPIController::create()->getSearchMallCountry();
 })->where('search', '(list|search)');
+
+/**
+ * List/Search mall city
+ */
+Route::get('/api/v1/mall-city/{search}', function()
+{
+    return MallLocationAPIController::create()->getSearchMallCity();
+})->where('search', '(list|search)');
