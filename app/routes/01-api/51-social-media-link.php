0de26c3e53fc8d6311ea37efb65e7e61a869474f
firmansyah <firmansyah@dominopos.com>
2017-01-31 18:54:57 +0800

    Delete sosmedlink api

--- app/routes/01-api/51-social-media-link.php
+++
@@ -1,9 +0,0 @@
-<?php
-/**
- * List and/or search social media link
- */
-
-Route::get('/api/v1/pub/social-media-link/list', function()
-{
-    return Orbit\Controller\API\v1\Pub\SocialMediaLinkAPIController::create()->getSocialMediaLink();
-});
--- app/routes/02-intermediate/51-social-media-link.php
+++
@@ -1,6 +0,0 @@
-<?php
-/**
- * List and/or search social media link
- */
-
-Route::get('/app/v1/pub/social-media-link/list', ['as' => 'pub-social-media-link', 'uses' => 'IntermediatePubAuthController@SocialMediaLink_getSocialMediaLink']);
