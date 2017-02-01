adb0a82559c5f8fed6b946d2e770e32bdc3b545a
shelgi <shelgi@dominopos.com>
2017-02-01 09:30:45 +0800

    Create new queue and artisan command for coupon to es_suggestion

---
+++ app/commands/ElasticsearchResyncCouponSuggestionCommand.php
@@ -0,0 +1,128 @@
+<?php
+
+/**
+ * Command for resync coupon data from MySQL to Elasticsearch based on coupon id
+ * @author shelgi <shelgi@dominopos.com>
+ */
+
+use Illuminate\Console\Command;
+use Symfony\Component\Console\Input\InputOption;
+use Symfony\Component\Console\Input\InputArgument;
+use Orbit\FakeJob;
+use Orbit\Queue\ElasticSearch\ESCouponSuggestionUpdateQueue;
+
+class ElasticsearchResyncCouponSuggestionCommand extends Command {
+
+    /**
+     * The console command name.
+     *
+     * @var string
+     */
+    protected $name = 'elasticsearch:resync-coupon-suggestion';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Resync coupon data from MySQL to Elasticsearch based on coupon id';
+
+    /**
+     * Prefix for message list.
+     *
+     * @var string
+     */
+    protected $stdoutPrefix = '';
+
+    /**
+     * Create a new command instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    /**
+     * Execute the console command.
+     *
+     * @return mixed
+     */
+    public function fire()
+    {
+        try {
+            $input = ! empty($this->option('id')) ? $this->option('id') : file_get_contents("php://stdin");
+            $input = trim($input);
+
+            if (empty($input)) {
+                throw new Exception("Input needed.", 1);
+            }
+
+            $job = new FakeJob();
+            $data = [
+                'coupon_id' => $input
+            ];
+            try {
+                $response = $this->syncData($job, $data);
+
+                if ($response['status'] === 'fail') {
+                    throw new Exception($response['message'], 1);
+                }
+
+                $this->info(sprintf('%sCoupon ID: "%s" has been successfully synced to Elasticsearch server (Coupon Suggestion Index)', $this->stdoutPrefix, $data['coupon_id']));
+            } catch (Exception $e) {
+                $this->error(sprintf('%sFailed to sync Coupon ID "%s", message: %s', $this->stdoutPrefix, $data['coupon_id'], $e->getMessage()));
+            }
+        } catch (\Exception $e) {
+            $this->error($e->getMessage());
+        }
+    }
+
+    /**
+     * Get the console command arguments.
+     *
+     * @return array
+     */
+    protected function getArguments()
+    {
+        return array(
+        );
+    }
+
+    /**
+     * Get the console command options.
+     *
+     * @return array
+     */
+    protected function getOptions()
+    {
+        return array(
+            array('id', null, InputOption::VALUE_OPTIONAL, 'Coupon id to sync.', null),
+            array('dry-run', null, InputOption::VALUE_NONE, 'Run in dry-run mode, no data will be sent to Elasticsearch.', null),
+        );
+    }
+
+    /**
+     * Fake response
+     *
+     * @param boolean $dryRun
+     */
+    protected function syncData($job, $data)
+    {
+        if ($this->option('dry-run')) {
+            $this->stdoutPrefix = '[DRY RUN] ';
+
+            return [
+                'status' => 'ok',
+                'message' => 'Dry run mode'
+            ];
+        }
+
+        $esQueue = new ESCouponSuggestionUpdateQueue();
+        $response = $esQueue->fire($job, $data);
+
+        return $response;
+    }
+}
+
