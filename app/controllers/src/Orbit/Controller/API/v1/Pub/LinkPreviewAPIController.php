449018a29d4cb65a001a6a96bf356af95518ad66
Ahmad <ahmad@dominopos.com>
2017-03-13 11:35:47 +0800

    Remove some unnecessary lines

--- app/controllers/src/Orbit/Controller/API/v1/Pub/LinkPreviewAPIController.php
+++ app/controllers/src/Orbit/Controller/API/v1/Pub/LinkPreviewAPIController.php
@@ -1,5 +1,8 @@
 <?php namespace Orbit\Controller\API\v1\Pub;
-
+/**
+ * API for getting preview data for shared URL that crawled by bots
+ * @author Ahmad <ahmad@dominopos.com>
+ */
 use OrbitShop\API\v1\PubControllerAPI;
 use OrbitShop\API\v1\Helper\Input as OrbitInput;
 use Orbit\Helper\Net\LinkPreview\LinkPreviewHelper;
@@ -10,24 +13,20 @@ class LinkPreviewAPIController extends PubControllerAPI
 {
     public function getDataPreview()
     {
-        # /stores/KsQZo8XoxR45pEEY/bale-nyonya?country=Indonesia&cities=Depok&sortby=created_date&sortmode=desc&order=latest
-        if (Config::get('orbit.link_preview.hashbang_enable', TRUE)) {
-            $rawQueryString = OrbitInput::get('_escaped_fragment_');
-            if (Config::get('app.debug')) {
-                $rawQueryString = '/stores/KsQZo8XoxR45pEEY/bale-nyonya?country=Indonesia&cities=Depok&sortby=created_date&sortmode=desc&order=latest&lang=en';
-            }
-            $queryString = urldecode(urldecode($rawQueryString));
-        } else {
-            $rawQueryString = $_SERVER['REQUEST_URI'];
-            dd($rawQueryString);
+
+        $rawQueryString = OrbitInput::get('_escaped_fragment_');
+        if (Config::get('app.debug')) {
+            // --- debugging url sample ---
+            // store detail inside mall
+            // $rawQueryString = '/malls/KsQZo8XoxR45pEEx/stores/KsQZo8XoxR45pEEY?country=Indonesia&cities=Depok&sortby=created_date&sortmode=desc&order=latest&lang=en';
+            // store detail
+            $rawQueryString = '/stores/KsQZo8XoxR45pEEY/bale-nyonya?country=Indonesia&cities=Depok&sortby=created_date&sortmode=desc&order=latest&lang=en';
+            // store list
+            // $rawQueryString = '/stores?country=Indonesia&cities=Depok&sortby=created_date&sortmode=desc&order=latest&lang=en';
         }
+        $queryString = urldecode(urldecode($rawQueryString));
 
         $data = LinkPreviewHelper::create($queryString)->getData();
-
-        // $pretext = isset($object_name) ? $object_name : '';
-        // $data = new stdClass();
-        // $data->title = sprintf('%sInfo diskon dan outlet terlengkap - Gotomalls.com', $pretext);
-        // $data->url =
 
         return View::make('mobile-ci.templates.sharer', compact('data'));
     }
