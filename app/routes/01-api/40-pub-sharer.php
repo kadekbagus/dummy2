783d4985c00ebaa29c7f30f113f767d3e49197b0
Ahmad <ahmad@dominopos.com>
2017-03-14 14:47:42 +0800

    Revert the detail fb share route

--- app/routes/01-api/40-pub-sharer.php
+++ app/routes/01-api/40-pub-sharer.php
@@ -1,5 +1,46 @@
 <?php
 
+Route::group(
+    array('before' => 'pub-fb-bot'),
+    function () {
+        Route::get('/pub/sharer/facebook/promotion-detail',
+            array(
+                'as' => 'pub-share-promotion',
+                function()
+                {
+                    return Orbit\Controller\API\v1\Pub\SocMedAPIController::create()->getPromotionDetailView();
+                }
+            )
+        );
+        Route::get('/pub/sharer/facebook/news-detail',
+            array(
+                'as' => 'pub-share-news',
+                function()
+                {
+                    return Orbit\Controller\API\v1\Pub\SocMedAPIController::create()->getNewsDetailView();
+                }
+            )
+        );
+        Route::get('/pub/sharer/facebook/coupon-detail',
+            array(
+                'as' => 'pub-share-coupon',
+                function()
+                {
+                    return Orbit\Controller\API\v1\Pub\SocMedAPIController::create()->getCouponDetailView();
+                }
+            )
+        );
+        Route::get('/pub/sharer/facebook/store-detail',
+            array(
+                'as' => 'pub-share-store',
+                function()
+                {
+                    return Orbit\Controller\API\v1\Pub\SocMedAPIController::create()->getStoreDetailView();
+                }
+            )
+        );
+    }
+);
 
 Route::get('/pub/sharer',
     array(
