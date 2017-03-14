545dbbe648c42deaca0dce5a169e89dd8d9a2fd7
Ahmad <ahmad@dominopos.com>
2017-03-14 12:04:16 +0800

    Add linkType list to some ObjectLinkPreview

--- app/helpers/Orbit/Helper/Net/LinkPreview/MallLinkPreview.php
+++ app/helpers/Orbit/Helper/Net/LinkPreview/MallLinkPreview.php
@@ -3,6 +3,8 @@
 use Mall;
 use DB;
 use Config;
+use Lang;
+use App;
 
 class MallLinkPreview implements ObjectLinkPreviewInterface
 {
@@ -24,41 +26,52 @@ class MallLinkPreview implements ObjectLinkPreviewInterface
         return $this->input;
     }
 
-    public function getShareData()
+    public function getPreviewData()
     {
-        $prefix = DB::getTablePrefix();
-        $usingCdn = Config::get('orbit.cdn.enable_cdn', FALSE);
-        $defaultUrlPrefix = Config::get('orbit.cdn.providers.default.url_prefix', '');
-        $urlPrefix = ($defaultUrlPrefix != '') ? $defaultUrlPrefix . '/' : '';
-        $lang = $this->input['lang'];
-
-        $image = "CONCAT({$this->quote($urlPrefix)}, {$prefix}media.path) as path";
-        if ($usingCdn) {
-            $image = "CASE WHEN ({$prefix}media.cdn_url is null or {$prefix}media.cdn_url = '') THEN CONCAT({$this->quote($urlPrefix)}, {$prefix}media.path) ELSE {$prefix}media.cdn_url END as path";
-        }
-
-        $data = Mall::select('merchants.merchant_id','merchants.name','merchants.description')
-                ->with(['mediaLogo' => function ($q) use ($image) {
-                        $q->select(
-                                DB::raw("{$image}"),
-                                'media.object_id'
-                            );
-                    }])
-                ->where('merchants.status', 'active')
-                ->where('merchants.merchant_id', $this->input['objectId'])
-                ->first();
-
-        if (is_object($data)) {
-            $title = $data->name;
-            $description = $data->description;
-            $imageUrl = $data->mediaLogo[0]->path;
-            list($width, $height) = getimagesize($imageUrl);
-            $imageDimension = [$width, $height];
+        // detail page
+        if ($this->input['linkType'] === 'detail') {
+            $prefix = DB::getTablePrefix();
+            $usingCdn = Config::get('orbit.cdn.enable_cdn', FALSE);
+            $defaultUrlPrefix = Config::get('orbit.cdn.providers.default.url_prefix', '');
+            $urlPrefix = ($defaultUrlPrefix != '') ? $defaultUrlPrefix . '/' : '';
             $lang = $this->input['lang'];
-            $url = $this->input['url'];
-            $previewData = new LinkPreviewData($title, $description, $url, $imageUrl, $imageDimension, $lang->name);
+
+            $image = "CONCAT({$this->quote($urlPrefix)}, {$prefix}media.path) as path";
+            if ($usingCdn) {
+                $image = "CASE WHEN ({$prefix}media.cdn_url is null or {$prefix}media.cdn_url = '') THEN CONCAT({$this->quote($urlPrefix)}, {$prefix}media.path) ELSE {$prefix}media.cdn_url END as path";
+            }
+
+            $data = Mall::select('merchants.merchant_id','merchants.name','merchants.description')
+                    ->with(['mediaLogo' => function ($q) use ($image) {
+                            $q->select(
+                                    DB::raw("{$image}"),
+                                    'media.object_id'
+                                );
+                        }])
+                    ->where('merchants.status', 'active')
+                    ->where('merchants.merchant_id', $this->input['objectId'])
+                    ->first();
+
+            if (is_object($data)) {
+                $title = $data->name;
+                $description = $data->description;
+                $imageUrl = $data->mediaLogo[0]->path;
+                list($width, $height) = getimagesize($imageUrl);
+                $imageDimension = [$width, $height];
+                $lang = $this->input['lang'];
+                $url = $this->input['url'];
+                $previewData = new LinkPreviewData($title, $description, $url, $imageUrl, $imageDimension, $lang->name);
+            } else {
+                $previewData = new LinkPreviewData();
+            }
         } else {
-            $previewData = new LinkPreviewData();
+            // list page
+            $lang = $this->input['lang'];
+            App::setLocale($lang->name);
+            $title = isset($this->input['mallName']) ? sprintf(Lang::get('metatags.mall_list.mall.title'), $this->input['mallName']) : Lang::get('metatags.mall_list.gtm.title');
+            $description = isset($this->input['mallName']) ? sprintf(Lang::get('metatags.mall_list.mall.description'), $this->input['mallName']) : Lang::get('metatags.mall_list.gtm.description');
+            $url = $this->input['url'];
+            $previewData = new LinkPreviewData($title, $description, $url, '', [], $lang->name);
         }
 
         return $previewData;
