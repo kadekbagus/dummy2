0cbb01f4eba59d22afa580fb198122501572d5ce
kadekbagus <kadek@dominopos.com>
2017-03-21 13:49:33 +0800

    add category on cdn s3 upload artisan command

--- app/commands/CdnS3UploadCommand.php
+++ app/commands/CdnS3UploadCommand.php
@@ -110,6 +110,20 @@ class CdnS3UploadCommand extends Command
                 }
                 break;
 
+            case 'category':
+                $categories = Category::with('media')->where('category_id', $objectId)->first();
+                $options = [
+                    'primary_key' => 'category_id',
+                    'translation_primary_key' => '',
+                    'using_translation' => FALSE,
+                    'bucket_name' => $bucketName
+                ];
+
+                foreach ($categories as $object) {
+                    $this->genericObjectUpload($object, $objectType, $options);
+                }
+                break;
+
             default:
                 throw new Exception('Unknown object-type value');
         }
