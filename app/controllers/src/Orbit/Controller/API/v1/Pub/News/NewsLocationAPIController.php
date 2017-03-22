a7a0f8d6fd3ed822802a73ca82612bac8ba68388
firmansyah <firmansyah@dominopos.com>
2017-03-22 10:57:29 +0800

    Fix operating hours in all location list

--- app/controllers/src/Orbit/Controller/API/v1/Pub/News/NewsLocationAPIController.php
+++ app/controllers/src/Orbit/Controller/API/v1/Pub/News/NewsLocationAPIController.php
@@ -145,7 +145,7 @@ class NewsLocationAPIController extends PubControllerAPI
                                         DB::raw("CASE WHEN {$prefix}merchants.object_type = 'tenant' THEN oms.address_line1 ELSE {$prefix}merchants.address_line1 END as address"),
                                         DB::raw("CASE WHEN {$prefix}merchants.object_type = 'tenant' THEN {$prefix}merchants.floor ELSE '' END as floor"),
                                         DB::raw("CASE WHEN {$prefix}merchants.object_type = 'tenant' THEN {$prefix}merchants.unit ELSE '' END as unit"),
-                                        DB::raw("CASE WHEN {$prefix}merchants.object_type = 'tenant' THEN oms.operating_hours ELSE '' END as operating_hours"),
+                                        DB::raw("CASE WHEN {$prefix}merchants.object_type = 'tenant' THEN oms.operating_hours ELSE {$prefix}merchants.operating_hours END as operating_hours"),
                                         DB::raw("CASE WHEN {$prefix}merchants.object_type = 'tenant' THEN oms.is_subscribed ELSE {$prefix}merchants.is_subscribed END as is_subscribed"),
                                         DB::raw("{$prefix}merchants.object_type as location_type"),
                                         DB::raw("{$mallLogo}"),
