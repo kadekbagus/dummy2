afecd33afddedf63f540370f38bb41145f9b0779
Irianto Pratama <irianto@dominopos.com>
2017-03-23 18:03:36 +0800

    Fix remove all sugestion on campaign you may also like

--- app/controllers/src/Orbit/Controller/API/v1/Pub/Promotion/PromotionAlsoLikeListAPIController.php
+++ app/controllers/src/Orbit/Controller/API/v1/Pub/Promotion/PromotionAlsoLikeListAPIController.php
@@ -260,33 +260,6 @@ class PromotionAlsoLikeListAPIController extends PubControllerAPI
             }
         }
 
-        // Get promotion list excluding the cities and the category
-        if (count($sameCategoryRecords) < $_GET['take']) {
-            unset($_GET['cities']);
-            unset($_GET['category_id']);
-
-            // set excluded ids
-            foreach ($sameCategoryRecords as $sameCategoryRecord) {
-                $_GET['excluded_ids'][] = $sameCategoryRecord->{$params['primary_key']};
-            }
-
-            // get promotion
-            $responseSameType = PromotionListAPIController::create('raw')
-                    ->setUser($this->api->user)
-                    ->getSearchPromotion();
-
-            if ($responseSameType->code !== 0) {
-                throw new Exception($responseSameType->message, $responseSameCategory->code);
-            }
-
-            $sameCampaignTypeRecords = $responseSameType->data->records;
-            $sameCampaignTypeRecords = $this->removeUnusedProperty($sameCampaignTypeRecords);
-
-            foreach ($sameCampaignTypeRecords as $sameCampaignTypeRecord) {
-                $sameCategoryRecords[] = $sameCampaignTypeRecord;
-            }
-        }
-
         $sameCategoryRecords = array_slice($sameCategoryRecords, 0, $_GET['take'], true);
 
         $_GET = $_OLD_GET;
