f27fa39e2efdddb81cd99c42f7151d0e32c91fb3
Rio Astamal <rio@dominopos.com>
2017-04-15 17:21:28 +0800

    ElasticsearchResyncNews: Fix wrong namespace name

--- app/commands/ElasticsearchResyncNewsCommand.php
+++ app/commands/ElasticsearchResyncNewsCommand.php
@@ -8,7 +8,7 @@ use Illuminate\Console\Command;
 use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Input\InputArgument;
 use Orbit\FakeJob;
-use Orbit\Queue\ElasticSearch\ESNewsUpdateQueue;
+use Orbit\Queue\Elasticsearch\ESNewsUpdateQueue;
 
 class ElasticsearchResyncNewsCommand extends Command {
 
