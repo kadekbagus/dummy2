f1e9324ed62b83d7116b96742830fa1fa1df4c5d
kadekbagus <kadek@dominopos.com>
2017-05-15 16:27:39 +0800

    create and listing for seo text setup admin portal

--- app/routes/02-intermediate/56-seo-text.php
+++ app/routes/02-intermediate/56-seo-text.php
@@ -7,3 +7,8 @@
  * Create new seo text
  */
 Route::post('/app/v1/seo-text/new', 'IntermediateAuthController@SeoText_postNewSeoText');
+
+/**
+ * Get search seo text
+ */
+Route::get('/app/v1/seo-text/list', 'IntermediateAuthController@SeoText_getSearchSeoText');
