e226e25c627f2b03ce9c9089b7a2e4adde4f513e
Irianto Pratama <irianto@dominopos.com>
2017-05-15 14:22:06 +0800

    Add artisan command campaign page view migration

---
+++ app/models/ObjectPageView.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Model for table `object_page_views`.
+ *
+ * @author Irianto Pratama <irianto@dominopos.com>
+ */
+class ObjectPageView extends Eloquent
+{
+    protected $primaryKey = 'object_page_view_id';
+    protected $table = 'object_page_views';
+
+}
