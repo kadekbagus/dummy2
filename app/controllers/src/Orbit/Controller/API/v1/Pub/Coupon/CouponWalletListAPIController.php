a5cb0f375251eb0e9c1e418e520254048d76e506
Ahmad <ahmad@dominopos.com>
2017-06-07 11:59:44 +0800

    Add issued_coupon_status in coupon wallet list API

--- app/controllers/src/Orbit/Controller/API/v1/Pub/Coupon/CouponWalletListAPIController.php
+++ app/controllers/src/Orbit/Controller/API/v1/Pub/Coupon/CouponWalletListAPIController.php
@@ -117,7 +117,8 @@ class CouponWalletListAPIController extends PubControllerAPI
                                     THEN 'true'
                                     ELSE 'false'
                                     END AS is_started,
-                                    {$prefix}issued_coupons.issued_coupon_id
+                                    {$prefix}issued_coupons.issued_coupon_id,
+                                    CASE WHEN {$prefix}issued_coupons.status = 'issued' THEN 'redeemable' ELSE 'redeemed' END as issued_coupon_status,
                                 ")
                             )
                             ->leftJoin('campaign_status', 'promotions.campaign_status_id', '=', 'campaign_status.campaign_status_id')
@@ -138,7 +139,7 @@ class CouponWalletListAPIController extends PubControllerAPI
                             })
                             ->join('issued_coupons', function ($join) {
                                 $join->on('issued_coupons.promotion_id', '=', 'promotions.promotion_id');
-                                $join->where('issued_coupons.status', '=', 'issued');
+                                $join->where('issued_coupons.status', '!=', 'deleted');
                             })
                             ->leftJoin(DB::raw("(SELECT m.path, m.cdn_url, ct.promotion_id
                                         FROM {$prefix}coupon_translations ct
