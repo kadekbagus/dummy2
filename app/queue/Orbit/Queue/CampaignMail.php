0a7a694372423a908e82c142645ab2e7233779f6
kadekbagus <kadek@dominopos.com>
2017-07-10 11:59:32 +0800

    add try catch on CampaignMail queue

--- app/queue/Orbit/Queue/CampaignMail.php
+++ app/queue/Orbit/Queue/CampaignMail.php
@@ -15,6 +15,7 @@ use TemporaryContent;
 use News;
 use Coupon;
 use DB;
+use Orbit\Helper\Util\JobBurier;
 
 class CampaignMail
 {
@@ -27,94 +28,109 @@ class CampaignMail
      */
     public function fire($job, $data)
     {
-        $prefix = DB::getTablePrefix();
-        // Get data information from the queue
-        switch ($data['mode']) {
-            case 'expired':
+        try {
+            $prefix = DB::getTablePrefix();
+            // Get data information from the queue
+            switch ($data['mode']) {
+                case 'expired':
+                        $mailviews = array(
+                            'html' => 'emails.campaign-auto-email.campaign-expired-html',
+                            'text' => 'emails.campaign-auto-email.campaign-expired-text'
+                        );
+                    break;
+
+                case 'update':
+                        $temporaryContentId = $data['temporaryContentId'];
+                        $tmpCampaign = TemporaryContent::where('temporary_content_id', $temporaryContentId)->first();
+
+                        if (! is_object($tmpCampaign)) {
+                            \Log::error('*** CampaignMail Queue: Temporary content not found. ***');
+                            return ;
+                        }
+
+                        switch (ucfirst($data['campaignType'])) {
+                            case 'News':
+                            case 'Promotion':
+                                $updatedCampaign = News::selectRaw("{$prefix}news.*,
+                                                            DATE_FORMAT({$prefix}news.end_date, '%d/%m/%Y %H:%i') as end_date")
+                                                        ->excludeDeleted()
+                                                        ->where('news_id', $data['campaignId'])
+                                                        ->first();
+                                break;
+
+                            case 'Coupon':
+                            default:
+                                $updatedCampaign = Coupon::selectRaw("{$prefix}promotions.*,
+                                                            DATE_FORMAT({$prefix}promotions.end_date, '%d/%m/%Y %H:%i') as end_date,
+                                                            DATE_FORMAT({$prefix}promotions.coupon_validity_in_date, '%d/%m/%Y %H:%i') as coupon_validity_in_date,
+                                                            IF({$prefix}promotions.maximum_issued_coupon = 0, 'Unlimited', {$prefix}promotions.maximum_issued_coupon) as maximum_issued_coupon
+                                                        ")
+                                                        ->excludeDeleted()
+                                                        ->where('promotion_id', $data['campaignId'])
+                                                        ->first();
+                        }
+
+                        if (! is_object($updatedCampaign)) {
+                            \Log::error('*** CampaignMail Queue: Campaign not found. ***');
+                            return ;
+                        }
+
+                        $mailviews = array(
+                            'html' => 'emails.campaign-auto-email.campaign-update-html',
+                            'text' => 'emails.campaign-auto-email.campaign-update-text'
+                        );
+
+                        $data['campaign_before'] = unserialize($tmpCampaign->contents);
+                        if ($data['campaignType'] === 'Coupon') {
+                            $data['campaign_after'] = $updatedCampaign->load([
+                                'translations.language',
+                                'translations.media',
+                                'ages.ageRange',
+                                'genders',
+                                'keywords',
+                                'campaign_status',
+                                'tenants' => function($q) use($prefix) {
+                                    $q->addSelect(DB::raw("CONCAT ({$prefix}merchants.name, ' at ', malls.name) as name"));
+                                    $q->join(DB::raw("{$prefix}merchants malls"), DB::raw("malls.merchant_id"), '=', 'merchants.parent_id');
+                                },
+                                'employee',
+                                'couponRule' => function($q) use($prefix) {
+                                    $q->select('promotion_rule_id', 'promotion_id', DB::raw("DATE_FORMAT({$prefix}promotion_rules.rule_end_date, '%d/%m/%Y %H:%i') as rule_end_date"));
+                                }
+                            ]);
+                        } else {
+                            $data['campaign_after'] = $updatedCampaign->load('translations.language', 'translations.media', 'ages.ageRange', 'genders', 'keywords', 'campaign_status');
+                        }
+
+                    break;
+
+                case 'create':
+                default:
                     $mailviews = array(
-                        'html' => 'emails.campaign-auto-email.campaign-expired-html',
-                        'text' => 'emails.campaign-auto-email.campaign-expired-text'
+                        'html' => 'emails.campaign-auto-email.campaign-html',
+                        'text' => 'emails.campaign-auto-email.campaign-text'
                     );
-                break;
+            }
 
-            case 'update':
-                    $temporaryContentId = $data['temporaryContentId'];
-                    $tmpCampaign = TemporaryContent::where('temporary_content_id', $temporaryContentId)->first();
+            $this->sendCampaignEmail($mailviews, $data);
 
-                    if (! is_object($tmpCampaign)) {
-                        \Log::error('*** CampaignMail Queue: Temporary content not found. ***');
-                        return ;
-                    }
+            // Don't care if the job success or not we will provide user
+            // another link to resend the activation
+            $job->delete();
+        } catch (Exception $e) {
+            $message = sprintf('[Job ID: `%s`] Campaign Mail; Status: FAIL; Code: %s; Message: %s',
+                    $job->getJobId(),
+                    $e->getCode(),
+                    $e->getMessage());
 
-                    switch (ucfirst($data['campaignType'])) {
-                        case 'News':
-                        case 'Promotion':
-                            $updatedCampaign = News::selectRaw("{$prefix}news.*,
-                                                        DATE_FORMAT({$prefix}news.end_date, '%d/%m/%Y %H:%i') as end_date")
-                                                    ->excludeDeleted()
-                                                    ->where('news_id', $data['campaignId'])
-                                                    ->first();
-                            break;
-
-                        case 'Coupon':
-                        default:
-                            $updatedCampaign = Coupon::selectRaw("{$prefix}promotions.*,
-                                                        DATE_FORMAT({$prefix}promotions.end_date, '%d/%m/%Y %H:%i') as end_date,
-                                                        DATE_FORMAT({$prefix}promotions.coupon_validity_in_date, '%d/%m/%Y %H:%i') as coupon_validity_in_date,
-                                                        IF({$prefix}promotions.maximum_issued_coupon = 0, 'Unlimited', {$prefix}promotions.maximum_issued_coupon) as maximum_issued_coupon
-                                                    ")
-                                                    ->excludeDeleted()
-                                                    ->where('promotion_id', $data['campaignId'])
-                                                    ->first();
-                    }
-
-                    if (! is_object($updatedCampaign)) {
-                        \Log::error('*** CampaignMail Queue: Campaign not found. ***');
-                        return ;
-                    }
-
-                    $mailviews = array(
-                        'html' => 'emails.campaign-auto-email.campaign-update-html',
-                        'text' => 'emails.campaign-auto-email.campaign-update-text'
-                    );
-
-                    $data['campaign_before'] = unserialize($tmpCampaign->contents);
-                    if ($data['campaignType'] === 'Coupon') {
-                        $data['campaign_after'] = $updatedCampaign->load([
-                            'translations.language',
-                            'translations.media',
-                            'ages.ageRange',
-                            'genders',
-                            'keywords',
-                            'campaign_status',
-                            'tenants' => function($q) use($prefix) {
-                                $q->addSelect(DB::raw("CONCAT ({$prefix}merchants.name, ' at ', malls.name) as name"));
-                                $q->join(DB::raw("{$prefix}merchants malls"), DB::raw("malls.merchant_id"), '=', 'merchants.parent_id');
-                            },
-                            'employee',
-                            'couponRule' => function($q) use($prefix) {
-                                $q->select('promotion_rule_id', 'promotion_id', DB::raw("DATE_FORMAT({$prefix}promotion_rules.rule_end_date, '%d/%m/%Y %H:%i') as rule_end_date"));
-                            }
-                        ]);
-                    } else {
-                        $data['campaign_after'] = $updatedCampaign->load('translations.language', 'translations.media', 'ages.ageRange', 'genders', 'keywords', 'campaign_status');
-                    }
-
-                break;
-
-            case 'create':
-            default:
-                $mailviews = array(
-                    'html' => 'emails.campaign-auto-email.campaign-html',
-                    'text' => 'emails.campaign-auto-email.campaign-text'
-                );
+            Log::info($message);
         }
 
-        $this->sendCampaignEmail($mailviews, $data);
-
-        // Don't care if the job success or not we will provide user
-        // another link to resend the activation
-        $job->delete();
+        // Bury the job for later inspection
+        JobBurier::create($job, function($theJob) {
+            // The queue driver does not support bury.
+            $theJob->delete();
+        })->bury();
     }
 
     /**
