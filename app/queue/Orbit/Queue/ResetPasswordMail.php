f575707d422457f741a2ca62beed41dd64b5c8ee
kadekbagus <kadek@dominopos.com>
2017-07-10 15:41:31 +0800

    add try catch and job bury on ResetPasswordMail queue

--- app/queue/Orbit/Queue/ResetPasswordMail.php
+++ app/queue/Orbit/Queue/ResetPasswordMail.php
@@ -11,6 +11,9 @@ use Token;
 use Language;
 use App;
 use Lang;
+use Log;
+use Orbit\Helper\Util\JobBurier;
+use Exception;
 
 class ResetPasswordMail
 {
@@ -23,55 +26,69 @@ class ResetPasswordMail
      */
     public function fire($job, $data)
     {
-        $language = (empty($data['languageId']))? 'id' : $data['languageId'];
-        $valid_language = Language::where('status', '=', 'active')
-                            ->where('name', $language)
-                            ->first();
+        try {
+            $language = (empty($data['languageId']))? 'id' : $data['languageId'];
+            $valid_language = Language::where('status', '=', 'active')
+                                ->where('name', $language)
+                                ->first();
 
-        if (empty($valid_language)) {
-            // get from config default language or locale language from country for sign up
-            $language = 'id'; // for a while before set a config
+            if (empty($valid_language)) {
+                // get from config default language or locale language from country for sign up
+                $language = 'id'; // for a while before set a config
+            }
+
+            App::setLocale($language);
+            $data['languageId'] = $language;
+
+            $token = Token::leftJoin('users', 'users.user_id', '=', 'tokens.user_id')
+                          ->where('token_id','=', $data['tokenId'])
+                          ->where('token_name', '=', 'reset_password')
+                          ->first();
+
+            $baseUrl = Config::get('orbit.reset_password.reset_base_url');
+            $tokenUrl = sprintf($baseUrl, $token->token_value, $token->email, $data['languageId']);
+            $contactInfo = Config::get('orbit.contact_information.customer_service');
+
+            $dataView['token'] = $token->token_value;
+            $dataView['email'] = $token->email;
+            $dataView['first_name'] = $token->user_firstname;
+            $dataView['token_url'] = $tokenUrl;
+
+            $dataView['subject']       = Lang::get('email.reset_password.subject');
+            $dataView['title']         = Lang::get('email.reset_password.title');
+            $dataView['greeting']      = Lang::get('email.reset_password.greeting');
+            $dataView['message_part1'] = Lang::get('email.reset_password.message_part1');
+            $dataView['message_part2'] = Lang::get('email.reset_password.message_part2');
+            $dataView['message_part3'] = Lang::get('email.reset_password.message_part3');
+            $dataView['button_reset']  = Lang::get('email.reset_password.button_reset');
+            $dataView['message_part4'] = Lang::get('email.reset_password.message_part4');
+            $dataView['message_part5'] = Lang::get('email.reset_password.message_part5');
+            $dataView['message_part6'] = Lang::get('email.reset_password.message_part6');
+            $dataView['team_name']     = Lang::get('email.reset_password.team_name');
+
+            $mailViews = array(
+                    'html' => 'emails.reset-password.customer-html',
+                    'text' => 'emails.reset-password.customer-text'
+            );
+
+            $this->sendResetPasswordEmail($mailViews, $dataView);
+
+            // Don't care if the job success or not we will provide user
+            // another link to resend the activation
+            $job->delete();
+        } catch (Exception $e) {
+            $message = sprintf('[Job ID: `%s`] Reset Password Mail; Status: FAIL; Code: %s; Message: %s',
+                    $job->getJobId(),
+                    $e->getCode(),
+                    $e->getMessage());
+            Log::info($message);
         }
 
-        App::setLocale($language);
-        $data['languageId'] = $language;
-
-        $token = Token::leftJoin('users', 'users.user_id', '=', 'tokens.user_id')
-                      ->where('token_id','=', $data['tokenId'])
-                      ->where('token_name', '=', 'reset_password')
-                      ->first();
-
-        $baseUrl = Config::get('orbit.reset_password.reset_base_url');
-        $tokenUrl = sprintf($baseUrl, $token->token_value, $token->email, $data['languageId']);
-        $contactInfo = Config::get('orbit.contact_information.customer_service');
-
-        $dataView['token'] = $token->token_value;
-        $dataView['email'] = $token->email;
-        $dataView['first_name'] = $token->user_firstname;
-        $dataView['token_url'] = $tokenUrl;
-
-        $dataView['subject']       = Lang::get('email.reset_password.subject');
-        $dataView['title']         = Lang::get('email.reset_password.title');
-        $dataView['greeting']      = Lang::get('email.reset_password.greeting');
-        $dataView['message_part1'] = Lang::get('email.reset_password.message_part1');
-        $dataView['message_part2'] = Lang::get('email.reset_password.message_part2');
-        $dataView['message_part3'] = Lang::get('email.reset_password.message_part3');
-        $dataView['button_reset']  = Lang::get('email.reset_password.button_reset');
-        $dataView['message_part4'] = Lang::get('email.reset_password.message_part4');
-        $dataView['message_part5'] = Lang::get('email.reset_password.message_part5');
-        $dataView['message_part6'] = Lang::get('email.reset_password.message_part6');
-        $dataView['team_name']     = Lang::get('email.reset_password.team_name');
-
-        $mailViews = array(
-                'html' => 'emails.reset-password.customer-html',
-                'text' => 'emails.reset-password.customer-text'
-        );
-
-        $this->sendResetPasswordEmail($mailViews, $dataView);
-
-        // Don't care if the job success or not we will provide user
-        // another link to resend the activation
-        $job->delete();
+        // Bury the job for later inspection
+        JobBurier::create($job, function($theJob) {
+            // The queue driver does not support bury.
+            $theJob->delete();
+        })->bury();
     }
 
     /**
