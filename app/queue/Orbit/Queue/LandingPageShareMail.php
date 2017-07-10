1b6e77bc46af88b461a98d05b0fdec7822306944
kadekbagus <kadek@dominopos.com>
2017-07-10 14:23:58 +0800

    add try catch and job bury on landing page share mail queue

--- app/queue/Orbit/Queue/LandingPageShareMail.php
+++ app/queue/Orbit/Queue/LandingPageShareMail.php
@@ -16,6 +16,7 @@ use DB;
 use Language;
 use Lang;
 use App;
+use Orbit\Helper\Util\JobBurier;
 
 class LandingPageShareMail
 {
@@ -28,47 +29,61 @@ class LandingPageShareMail
      */
     public function fire($job, $data)
     {
-        $prefix = DB::getTablePrefix();
+        try {
+            $prefix = DB::getTablePrefix();
 
-        $user = User::where('user_id','=', $data['userId'])
-                    ->first();
+            $user = User::where('user_id','=', $data['userId'])
+                        ->first();
 
-        if (! empty($data['language'])) {
-            App::setLocale($data['language']);
+            if (! empty($data['language'])) {
+                App::setLocale($data['language']);
+            }
+
+            $baseLinkUrl = Config::get('app.url') . '/?utm_source=gtm-share&utm_medium=email&utm_content=menulink#!/%s?lang=' . $data['language'];
+
+            $dataView['linkMalls']      = sprintf($baseLinkUrl, 'malls');
+            $dataView['linkStores']     = sprintf($baseLinkUrl, 'stores');
+            $dataView['linkPromotions'] = sprintf($baseLinkUrl, 'promotions');
+            $dataView['linkCoupons']    = sprintf($baseLinkUrl, 'coupons');
+            $dataView['linkEvents']     = sprintf($baseLinkUrl, 'events');
+            $dataView['email'] = $data['email'];
+            $dataView['name'] = $user->user_firstname;
+            $dataView['shareUrl'] = Config::get('orbit.landingpage_share_email.share_url');
+            $dataView['videoUrl'] = Config::get('orbit.landingpage_share_email.video_url');
+            $dataView['labelMalls'] = Lang::get('email.gotomalls_share.label_malls');
+            $dataView['labelStores'] = Lang::get('email.gotomalls_share.label_stores');
+            $dataView['labelPromotions'] = Lang::get('email.gotomalls_share.label_promotions');
+            $dataView['labelCoupons'] = Lang::get('email.gotomalls_share.label_coupons');
+            $dataView['labelEvents'] = Lang::get('email.gotomalls_share.label_events');
+            $dataView['subject'] = Lang::get('email.gotomalls_share.subject');
+            $dataView['greeting'] = Lang::get('email.gotomalls_share.greeting');
+            $dataView['message1'] = Lang::get('email.gotomalls_share.message_part1');
+            $dataView['message2'] = Lang::get('email.gotomalls_share.message_part2');
+            $dataView['button_try_now']  = Lang::get('email.gotomalls_share.button_try_now');
+
+            $mailViews = array(
+                        'html' => 'emails.landingpage-share-email.landingpage-share-html',
+                        'text' => 'emails.landingpage-share-email.landingpage-share-text'
+            );
+
+            $this->sendCampaignShareEmail($mailViews, $dataView);
+
+            // Don't care if the job success or not we will provide user
+            // another link to resend the activation
+            $job->delete();
+        } catch (Exception $e) {
+            $message = sprintf('[Job ID: `%s`] Landing Page Share Mail; Status: FAIL; Code: %s; Message: %s',
+                    $job->getJobId(),
+                    $e->getCode(),
+                    $e->getMessage());
+            Log::info($message);
         }
 
-        $baseLinkUrl = Config::get('app.url') . '/?utm_source=gtm-share&utm_medium=email&utm_content=menulink#!/%s?lang=' . $data['language'];
-
-        $dataView['linkMalls']      = sprintf($baseLinkUrl, 'malls');
-        $dataView['linkStores']     = sprintf($baseLinkUrl, 'stores');
-        $dataView['linkPromotions'] = sprintf($baseLinkUrl, 'promotions');
-        $dataView['linkCoupons']    = sprintf($baseLinkUrl, 'coupons');
-        $dataView['linkEvents']     = sprintf($baseLinkUrl, 'events');
-        $dataView['email'] = $data['email'];
-        $dataView['name'] = $user->user_firstname;
-        $dataView['shareUrl'] = Config::get('orbit.landingpage_share_email.share_url');
-        $dataView['videoUrl'] = Config::get('orbit.landingpage_share_email.video_url');
-        $dataView['labelMalls'] = Lang::get('email.gotomalls_share.label_malls');
-        $dataView['labelStores'] = Lang::get('email.gotomalls_share.label_stores');
-        $dataView['labelPromotions'] = Lang::get('email.gotomalls_share.label_promotions');
-        $dataView['labelCoupons'] = Lang::get('email.gotomalls_share.label_coupons');
-        $dataView['labelEvents'] = Lang::get('email.gotomalls_share.label_events');
-        $dataView['subject'] = Lang::get('email.gotomalls_share.subject');
-        $dataView['greeting'] = Lang::get('email.gotomalls_share.greeting');
-        $dataView['message1'] = Lang::get('email.gotomalls_share.message_part1');
-        $dataView['message2'] = Lang::get('email.gotomalls_share.message_part2');
-        $dataView['button_try_now']  = Lang::get('email.gotomalls_share.button_try_now');
-
-        $mailViews = array(
-                    'html' => 'emails.landingpage-share-email.landingpage-share-html',
-                    'text' => 'emails.landingpage-share-email.landingpage-share-text'
-        );
-
-        $this->sendCampaignShareEmail($mailViews, $dataView);
-
-        // Don't care if the job success or not we will provide user
-        // another link to resend the activation
-        $job->delete();
+        // Bury the job for later inspection
+        JobBurier::create($job, function($theJob) {
+            // The queue driver does not support bury.
+            $theJob->delete();
+        })->bury();
     }
 
     /**
