c3f2fc0955d84cdc946a7960d19a424adfada3f5
shelgi <shelgi@dominopos.com>
2017-08-24 11:53:03 +0800

    - Create new advert_slot_locations table - Create new model for advert_slot_locations table

---
+++ app/models/AdvertSlotLocation.php
@@ -0,0 +1,13 @@
+<?php
+class AdvertSlotLocation extends Eloquent
+{
+    /**
+     * Advert Slot Locations Model
+     *
+     * @author Shelgi <shelgi@dominopos.com>
+     */
+
+    protected $table = 'advert_slot_locations';
+    protected $primaryKey = 'advert_slot_location_id';
+
+}
