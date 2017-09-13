a574cd45fbca2abca03e1dcf9492342b565d51ee
shelgi <shelgi@dominopos.com>
2017-09-13 14:32:26 +0800

    - Add notification_provider in user_notifications table - Create model for user_notifications

---
+++ app/models/UserNotification.php
@@ -0,0 +1,13 @@
+<?php
+class UserNotification extends Eloquent
+{
+    /**
+     * user_notifications Model
+     *
+     * @author Shelgi <shelgi@dominopos.com>
+     */
+
+    protected $table = 'user_notifications';
+
+    protected $primaryKey = 'user_notification_id';
+}
