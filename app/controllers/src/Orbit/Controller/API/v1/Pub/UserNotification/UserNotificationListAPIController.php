7257757c53033f30edc98aebfb3381cfbd0793f8
shelgi <shelgi@dominopos.com>
2017-10-30 10:49:14 +0800

    New api to get user notification new (counter)

--- app/controllers/src/Orbit/Controller/API/v1/Pub/UserNotification/UserNotificationListAPIController.php
+++ app/controllers/src/Orbit/Controller/API/v1/Pub/UserNotification/UserNotificationListAPIController.php
@@ -121,4 +121,92 @@ class UserNotificationListAPIController extends PubControllerAPI
 
         return $this->render($httpCode);
     }
+
+    /**
+     * get - new notification counter
+     *
+     * @author shelgi <shelgi@dominopos.com>
+     *
+     * List of API Parameters
+     * ----------------------
+     * @param string notification_token
+     *
+     * @return Illuminate\Support\Facades\Response
+     */
+    public function getUserNotificationNew()
+    {
+        $httpCode = 200;
+
+        try {
+            $user = $this->getUser();
+
+            // should always check the role
+            $role = $user->role->role_name;
+            if (strtolower($role) !== 'consumer') {
+                $message = 'You have to login to continue';
+                OrbitShopAPI::throwInvalidArgument($message);
+            }
+
+            $take = PaginationNumber::parseTakeFromGet('news');
+            $skip = PaginationNumber::parseSkipFromGet();
+
+            $mongoConfig = Config::get('database.mongodb');
+            $mongoClient = MongoClient::create($mongoConfig);
+
+            $queryString = [
+                'take'      => $take,
+                'skip'      => $skip,
+                'user_id'   => $user->user_id,
+                'is_viewed' => '0'
+            ];
+
+            $response = $mongoClient->setQueryString($queryString)
+                                    ->setEndPoint('user-notifications')
+                                    ->request('GET');
+            $listOfRec = $response->data;
+
+            $data = new \stdclass();
+            $data->returned_records = $listOfRec->returned_records;
+            $data->total_records = $listOfRec->total_records;
+            $data->records = $listOfRec->records;
+
+            $this->response->data = $data;
+            $this->response->code = 0;
+            $this->response->status = 'success';
+            $this->response->message = 'Request Ok';
+        } catch (ACLForbiddenException $e) {
+
+            $this->response->code = $e->getCode();
+            $this->response->status = 'error';
+            $this->response->message = $e->getMessage();
+            $this->response->data = null;
+            $httpCode = 403;
+
+        } catch (InvalidArgsException $e) {
+
+            $this->response->code = $e->getCode();
+            $this->response->status = 'error';
+            $this->response->message = $e->getMessage();
+            $this->response->data = null;
+            $httpCode = 403;
+
+        } catch (QueryException $e) {
+
+            $this->response->code = $e->getCode();
+            $this->response->status = 'error';
+            $this->response->message = $e->getMessage();
+            $this->response->data = null;
+            $httpCode = 500;
+
+        } catch (Exception $e) {
+
+            $this->response->code = Status::UNKNOWN_ERROR;
+            $this->response->status = 'error';
+            $this->response->message = $e->getMessage();
+            $this->response->data = null;
+            $httpCode = 500;
+        }
+
+        return $this->render($httpCode);
+    }
 }
