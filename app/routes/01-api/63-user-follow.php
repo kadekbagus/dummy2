601ecc6e2384ad45106b1791787e0b4109e2cee3
firmansyah <firmansyah@dominopos.com>
2017-11-16 10:07:54 +0800

    Implement paging in user follow list store

--- app/routes/01-api/63-user-follow.php
+++ app/routes/01-api/63-user-follow.php
@@ -18,3 +18,12 @@ Route::get('/api/v1/pub/follow', function()
 {
     return Orbit\Controller\API\v1\Pub\UserFollow\FollowAPIController::create()->postFollow();
 });
+
+
+/**
+ * Get store list user follow, call with ajax
+ */
+Route::get('/api/v1/pub/user-follow-store-list', function()
+{
+    return Orbit\Controller\API\v1\Pub\UserFollow\UserFollowStoreListAPIController::create()->getUserFollowStoreList();
+});
