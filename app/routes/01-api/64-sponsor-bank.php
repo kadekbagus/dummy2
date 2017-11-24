8f15a56361030406de22e6e2321466fb9bf140e7
firmansyah <firmansyah@dominopos.com>
2017-11-24 17:41:33 +0800

    add route for sponsored provider

--- app/routes/01-api/64-sponsor-bank.php
+++ app/routes/01-api/64-sponsor-bank.php
@@ -26,3 +26,14 @@ Route::get('/api/v1/sponsor-bank/list', function()
 {
     return SponsorBankAPIController::create()->getSearchSponsorBank();
 });
+
+
+/**
+ * Also like List of news
+ */
+Route::get('/api/v1/pub/sponsor-provider/list', function()
+{
+    return Orbit\Controller\API\v1\Pub\SponsorProviderListAPIController::create()->getSponsorProviderList();
+});
+
+Route::get('/app/v1/pub/sponsor-provider/list', ['as' => 'pub-sponsor-provider-list', 'uses' => 'IntermediatePubAuthController@SponsorProviderList_getSponsorProviderList']);
