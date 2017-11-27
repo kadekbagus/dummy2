1c0d171394bc2fbcfa24f2b90904f92320865468
shelgi <shelgi@dominopos.com>
2017-11-27 15:06:25 +0800

    Model for user sponsor table

---
+++ app/models/UserSponsor.php
@@ -0,0 +1,13 @@
+<?php
+class UserSponsor extends Eloquent
+{
+    /**
+     * user_sponsor Model
+     *
+     * @author shelgi <shelgi@dominopos.com>
+     */
+
+    protected $table = 'user_sponsor';
+    protected $primaryKey = 'user_sponsor_id';
+
+}
