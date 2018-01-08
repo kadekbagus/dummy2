ae23e629772ae287f972ed6a7b11b68261dc0c54
firmansyah <firmansyah@dominopos.com>
2018-01-08 17:49:10 +0800

    Add 'is_official_user' for flag

--- app/controllers/src/Orbit/Controller/API/v1/Pub/Rating/RatingListAPIController.php
+++ app/controllers/src/Orbit/Controller/API/v1/Pub/Rating/RatingListAPIController.php
@@ -170,19 +170,27 @@ class RatingListAPIController extends PubControllerAPI
                     $image = "(CASE WHEN {$prefix}media.cdn_url IS NULL THEN CONCAT({$this->quote($urlPrefix)}, {$prefix}media.path) ELSE {$prefix}media.cdn_url END) as user_picture";
                 }
 
-                $userList = User::select('users.user_id', DB::raw("(CONCAT({$prefix}users.user_firstname, ' ', {$prefix}users.user_lastname)) as user_name"), DB::raw($image))
+                $userList = User::select('users.user_id', 'roles.role_name', DB::raw("(CONCAT({$prefix}users.user_firstname, ' ', {$prefix}users.user_lastname)) as user_name"), DB::raw($image))
                                   ->leftJoin('media', function ($q) {
                                         $q->on('media.object_id', '=', 'users.user_id')
                                           ->on('media.media_name_long', '=', DB::raw("'user_profile_picture_orig'"));
                                     })
+                                  ->join('roles', 'roles.role_id', '=', 'users.user_role_id')
                                   ->whereIn('users.user_id', $userIds)
                                   ->groupBy('users.user_id')
                                   ->get();
 
+                $roleOfficial = ['Merchant Review Admin', 'Master Review Admin'];
+                $isOfficialUser = 'n';
                 $userRating = array();
                 foreach ($userList as $list) {
                     $userRating[$list->user_id]['user_name'] = $list->user_name;
                     $userRating[$list->user_id]['user_picture'] = $list->user_picture;
+
+                    if (in_array($list->role_name, $roleOfficial)) {
+                        $isOfficialUser = 'y';
+                    }
+                    $userRating[$list->user_id]['is_official_user'] = $isOfficialUser;
                 }
 
                 foreach ($listOfRec->records as $rating) {
@@ -194,6 +202,11 @@ class RatingListAPIController extends PubControllerAPI
                     $rating->user_picture = '';
                     if (! empty($userRating[$rating->user_id]['user_picture'])) {
                         $rating->user_picture = $userRating[$rating->user_id]['user_picture'];
+                    }
+
+                    $rating->is_official_user = '';
+                    if (! empty($userRating[$rating->user_id]['is_official_user'])) {
+                        $rating->is_official_user = $userRating[$rating->user_id]['is_official_user'];
                     }
                 }
             }
