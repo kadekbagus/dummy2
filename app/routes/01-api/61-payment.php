90ab8fd478250f7eb50c4cf61579b08ff86d17a6
Ahmad <ahmad@dominopos.com>
2018-01-12 19:10:34 +0800

    Change payment activity callback route to not use IntermediatePubAuthController

--- app/routes/01-api/61-payment.php
+++ app/routes/01-api/61-payment.php
@@ -16,9 +16,7 @@ Route::get('/app/v1/pub/payment/ping', ['as' => 'ping-payment', 'uses' => 'Inter
 /**
  * Callback url that will be ping-ed by our Payment Api server.
  */
-Route::post('/api/v1/pub/payment/activity-callback', function() 
+Route::post('/app/v1/pub/payment/activity-callback', function()
 {
     return Orbit\Controller\API\v1\Pub\PaymentActivityAPIController::create()->postNewPaymentActivity();
 });
-
-Route::post('/app/v1/pub/payment/activity-callback', ['as' => 'payment-activity-callback', 'uses' => 'IntermediatePubAuthController@PaymentActivity_postNewPaymentActivity']);
