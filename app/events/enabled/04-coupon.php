b50162360f8dd5539c269af8b3debbc383041ff9
kadekbagus <kadek@dominopos.com>
2018-01-18 10:46:35 +0800

    remove count on event hook campaign ccnotif

--- app/events/enabled/04-coupon.php
+++ app/events/enabled/04-coupon.php
@@ -971,7 +971,7 @@ Event::listen('orbit.coupon.postupdatecoupon-storenotificationupdate.after.commi
             }
 
             // If there is no follower but there is user linked to credit-card/ewallet
-            if (count($userFollows->data->returned_records) === 0 && !empty($userSponsor)) {
+            if ($userFollows->data->returned_records === 0 && !empty($userSponsor)) {
                 $userIds = $userSponsor;
                 $queryStringUserNotifToken['user_ids'] = json_encode($userIds);
 
