20b24b3ebaae62fba6d421c3d559ba88c520fce7
firmansyah <firmansyah@dominopos.com>
2018-02-08 18:01:20 +0800

    Implement product_tags in all campaign detail (NG)

--- app/controllers/src/Orbit/Controller/API/v1/Pub/News/NewsDetailAPIController.php
+++ app/controllers/src/Orbit/Controller/API/v1/Pub/News/NewsDetailAPIController.php
@@ -187,6 +187,9 @@ class NewsDetailAPIController extends PubControllerAPI
                         ->with(['keywords' => function ($q) {
                                 $q->addSelect('keyword', 'object_id');
                             }])
+                        ->with(['product_tags' => function ($pt) {
+                                $pt->addSelect('product_tag', 'object_id');
+                            }])
                         ->first();
 
             // Config page_views
