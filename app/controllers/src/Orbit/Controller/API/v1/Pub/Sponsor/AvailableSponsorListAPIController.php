63c50ad47d261a412ec3f87dc8f6397ea595cb1f
zamroni <zamroni@dominopos.com>
2018-04-18 10:32:19 +0800

    remove custom validation (for now)

--- app/controllers/src/Orbit/Controller/API/v1/Pub/Sponsor/AvailableSponsorListAPIController.php
+++ app/controllers/src/Orbit/Controller/API/v1/Pub/Sponsor/AvailableSponsorListAPIController.php
@@ -42,7 +42,7 @@ class AvailableSponsorListAPIController extends PubControllerAPI
             array(
                 'object_type'   => 'required|in:bank,ewallet,credit_card',
                 'bank_id'   => 'required_if:object_type,credit_card',
-                'language' => 'required|orbit.empty.language_default',
+                'language' => 'required',
             )
         );
     }
