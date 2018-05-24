7970d868a5eef8801b81474a57bec09a9849d215
Budi Raharja <budi@dominopos.com>
2018-05-24 15:57:21 +0800

    Payment: add User relation in Payment model, add Notifiable trait in User model

--- app/models/PaymentTransaction.php
+++ app/models/PaymentTransaction.php
@@ -40,6 +40,18 @@ class PaymentTransaction extends Eloquent
     }
 
     /**
+     * Payment - User relation.
+     *
+     * @author Budi <budi@dominopos.com>
+     * 
+     * @return [type] [description]
+     */
+    public function user()
+    {
+        return $this->belongsTo('User', 'user_id', 'user_id');
+    }
+
+    /**
      * Determine if the payment is fully completed or not.
      *
      * @author Budi <budi@dominopos.com>
@@ -50,5 +62,21 @@ class PaymentTransaction extends Eloquent
     public function completed()
     {
         return in_array($this->status, ['success', 'paid', 'settlement']);
+    }
+
+    /**
+     * Determine if the payment is for Sepulsa Deals.
+     *
+     * @author Budi <budi@dominopos.com>
+     * 
+     * @return [type] [description]
+     */
+    public function forSepulsa()
+    {
+        if (! empty($this->coupon)) {
+            return $this->coupon->promotion_type === 'sepulsa';
+        }
+
+        return false;
     }
 }
