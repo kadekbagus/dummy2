07a660649e364a1bb3a0140aedcba207dd4e4159
kadekbagus <kadek@dominopos.com>
2018-06-12 10:30:10 +0800

    not showing starting purchased coupon on purchased list

--- app/controllers/src/Orbit/Controller/API/v1/Pub/Coupon/CouponPurchasedListAPIController.php
+++ app/controllers/src/Orbit/Controller/API/v1/Pub/Coupon/CouponPurchasedListAPIController.php
@@ -142,6 +142,7 @@ class CouponPurchasedListAPIController extends PubControllerAPI
                             ->where('payment_transactions.user_id', $user->user_id)
                             ->where('payment_transactions.object_type', 'coupon')
                             ->where('payment_transactions.payment_method', '!=', 'normal')
+                            ->where('payment_transactions.status', '!=', 'starting')
                             ->groupBy('payment_transactions.payment_transaction_id');
 
             OrbitInput::get('filter_name', function ($filterName) use ($coupon, $prefix) {
