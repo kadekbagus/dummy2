041b7e2dea6b0f54ff12dc32f630624c3e2efe00
Budi Raharja <budi@dominopos.com>
2018-06-18 15:01:42 +0800

    Fix inApp notifications.

--- app/notifications/Orbit/Notifications/Coupon/HotDeals/ReceiptNotification.php
+++ app/notifications/Orbit/Notifications/Coupon/HotDeals/ReceiptNotification.php
@@ -3,13 +3,17 @@
 use Orbit\Helper\Notifications\Notification;
 use Orbit\Helper\Util\JobBurier;
 use Orbit\Helper\MongoDB\Client as MongoClient;
+use Orbit\Helper\Util\LandingPageUrlGenerator as LandingPageUrlGenerator;
+use Orbit\Helper\Util\CdnUrlGenerator;
 use Carbon\Carbon;
 
+use DB;
 use Mail;
 use Config;
 use Log;
 use Queue;
 use Exception;
+use Coupon;
 
 /**
  * Receipt Notification for Customer after purchasing Hot Deals (Paid Coupon).
@@ -21,11 +25,14 @@ class ReceiptNotification extends Notification
 
     protected $contact = null;
 
+    protected $mongoConfig = null;
+
     function __construct($payment = null)
     {
         $this->payment      = $payment;
         $this->queueName    = Config::get('orbit.registration.mobile.queue_name');
         $this->contact      = Config::get('orbit.contact_information');
+        $this->mongoConfig  = Config::get('database.mongodb');
     }
 
     /**
@@ -129,9 +136,14 @@ class ReceiptNotification extends Notification
      * 
      * @return [type] [description]
      */
-    public function toWeb()
+    public function toWeb($bodyInApps = null)
     {
-        
+        if (! empty($bodyInApps)) {
+            $mongoClient = MongoClient::create($this->mongoConfig);
+            $inApps = $mongoClient->setFormParam($bodyInApps)
+                                  ->setEndPoint('user-notifications')
+                                  ->request('POST');
+        }
     }
 
     /**
@@ -149,5 +161,81 @@ class ReceiptNotification extends Notification
         );
 
         // Other notification method can be added here...
+        $bodyInApps = $this->getInAppData();
+        $this->toWeb($bodyInApps);
+    }
+
+    public function getInAppData()
+    {
+        $bodyInApps = null;
+        $userId = $this->payment->user_id;
+        $couponId = $this->payment->object_id;
+        $prefix = DB::getTablePrefix();
+        $coupon = Coupon::select(DB::raw("{$prefix}promotions.promotion_name,
+                                    CASE WHEN {$prefix}media.path is null THEN med.path ELSE {$prefix}media.path END as localPath,
+                                    CASE WHEN {$prefix}media.cdn_url is null THEN med.cdn_url ELSE {$prefix}media.cdn_url END as cdnPath
+                            "))
+                            ->join('campaign_account', 'campaign_account.user_id', '=', 'promotions.created_by')
+                            ->join('languages as default_languages', DB::raw('default_languages.name'), '=', 'campaign_account.mobile_default_language')
+                            ->leftJoin('coupon_translations', function ($q) {
+                                $q->on('coupon_translations.promotion_id', '=', 'promotions.promotion_id');
+                            })
+                            ->leftJoin('coupon_translations as default_translation', function ($q) {
+                                $q->on(DB::raw('default_translation.promotion_id'), '=', 'promotions.promotion_id')
+                                    ->on(DB::raw('default_translation.merchant_language_id'), '=', DB::raw('default_languages.language_id'));
+                            })
+                        ->leftJoin(DB::raw("(SELECT m.path, m.cdn_url, ct.promotion_id
+                                        FROM {$prefix}coupon_translations ct
+                                        JOIN {$prefix}media m
+                                            ON m.object_id = ct.coupon_translation_id
+                                            AND m.media_name_long = 'coupon_translation_image_orig'
+                                        GROUP BY ct.promotion_id) AS med"), DB::raw("med.promotion_id"), '=', 'promotions.promotion_id')
+                        ->leftJoin('media', function ($q) {
+                                $q->on('media.object_id', '=', 'coupon_translations.coupon_translation_id');
+                                $q->on('media.media_name_long', '=', DB::raw("'coupon_translation_image_orig'"));
+                            })
+                        ->where('promotions.promotion_id', '=', $couponId)
+                        ->first();
+
+        if ($coupon) {
+            $launchUrl = LandingPageUrlGenerator::create('coupon', $coupon->promotion_id, $coupon->promotion_name)->generateUrl(true);
+
+            $headings = new \stdClass();
+            $headings->en = $coupon->promotion_name;
+            $contents = new \stdClass();
+            $contents->en = 'Your voucher is ready! Click here to redeem your voucher';
+
+            $notificationData = new \stdClass();
+            $notificationData->title = $coupon->promotion_name;
+            $notificationData->launch_url = $launchUrl;
+            $notificationData->default_language = 'en';
+            $notificationData->headings = $headings;
+            $notificationData->contents = $contents;
+            $notificationData->type = 'coupon';
+
+            $timezone = 'Asia/Makassar';
+            $timestamp = date("Y-m-d H:i:s");
+            $date = Carbon::createFromFormat('Y-m-d H:i:s', $timestamp, 'UTC');
+            $dateTime = $date->toDateTimeString();
+
+            $attachmentPath = (!empty($coupon->localPath)) ? $coupon->localPath : '';
+            $cdnUrl = (!empty($coupon->cdnPath)) ? $coupon->cdnPath : '';
+            $cdnConfig = Config::get('orbit.cdn');
+            $imgUrl = CdnUrlGenerator::create(['cdn' => $cdnConfig], 'cdn');
+            $imageUrl = $imgUrl->getImageUrl($attachmentPath, $cdnUrl);
+
+            $bodyInApps = [
+                'user_id'       => $userId,
+                'token'         => null,
+                'notifications' => $notificationData,
+                'send_status'   => 'sent',
+                'is_viewed'     => false,
+                'is_read'       => false,
+                'created_at'    => $dateTime,
+                'image_url'     => $imageUrl
+            ];
+        }
+
+        return $bodyInApps;
     }
 }
