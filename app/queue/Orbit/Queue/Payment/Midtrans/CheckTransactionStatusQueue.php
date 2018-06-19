801c64782f8cfce63b18fa205e8282f37c54cdfc
Budi Raharja <budi@dominopos.com>
2018-06-19 09:39:41 +0800

    Add payment transaction status EXPIRED.

--- app/queue/Orbit/Queue/Payment/Midtrans/CheckTransactionStatusQueue.php
+++ app/queue/Orbit/Queue/Payment/Midtrans/CheckTransactionStatusQueue.php
@@ -93,6 +93,9 @@ class CheckTransactionStatusQueue
                 if ($transaction->isSuccess()) {
                     $payment->status = PaymentTransaction::STATUS_SUCCESS;
                 }
+                else if ($transaction->isExpired()) {
+                    $payment->status = PaymentTransaction::STATUS_EXPIRED;
+                }
 
                 $payment->save();
 
