6ed4f99b412120212722d8c730f2d34c96b05c2a
Budi Raharja <budi@dominopos.com>
2018-06-21 10:32:16 +0800

    Send email to customer if system can not issue coupon.

---
+++ app/views/emails/coupon/sepulsa-customer-voucher-not-available.blade.php
@@ -0,0 +1,25 @@
+<html>
+    <head>
+        <title>Unable to Get the Coupon for You</title>
+    </head>
+    <body>
+        <h1>Hi, {{ $customerName }}</h1>
+        <p>
+            We are sorry to inform you that we can not get the coupon <strong>{{ $couponName }}</strong>. 
+            Don't worry about your money, we will refund it 100% as soon as possible before {{ $maxRefundDate }}.
+            <br>
+            <br>
+            Your transaction ID is: <strong>{{ $paymentId }}</strong>
+            <br>
+            <br>
+            Meanwhile, you can still buy other coupons at our website!
+        </p>
+    
+        <br>
+        <br>
+        <p>
+            For any question regarding the refund process, feel free to contact us via our support channels below:
+            <br>
+        </p>
+    </body>
+</html>
