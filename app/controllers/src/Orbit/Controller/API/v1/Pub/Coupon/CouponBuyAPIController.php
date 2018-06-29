4631e51e832c87fa3f6ba10f9c83452164c545a7
firmansyah <firmansyah@dominopos.com>
2018-06-29 10:51:12 +0800

    Change status from issued to reserved to reserved the coupon

--- app/controllers/src/Orbit/Controller/API/v1/Pub/Coupon/CouponBuyAPIController.php
+++ app/controllers/src/Orbit/Controller/API/v1/Pub/Coupon/CouponBuyAPIController.php
@@ -21,8 +21,8 @@ use Coupon;
 use IssuedCoupon;
 use \Queue;
 use Carbon\Carbon as Carbon;
-
 use Log;
+
 class CouponBuyAPIController extends PubControllerAPI
 {
     /**
@@ -79,7 +79,7 @@ class CouponBuyAPIController extends PubControllerAPI
             // Check the user already have coupon or not
             $userIssuedCoupon = IssuedCoupon::where('user_id', $user->user_id)
                                             ->where('promotion_id', $coupon_id)
-                                            ->where('status', IssuedCoupon::STATUS_ISSUED)
+                                            ->where('status', IssuedCoupon::STATUS_RESERVED)
                                             ->first();
 
             if (! empty($userIssuedCoupon)) {
@@ -106,12 +106,12 @@ class CouponBuyAPIController extends PubControllerAPI
                     if ($coupon->promotion_type === 'sepulsa') {
 
                         $issuedCoupon = new IssuedCoupon;
-                        $issuedCoupon->promotion_id             = $coupon_id;
-                        $issuedCoupon->user_id                  = $user->user_id;
-                        $issuedCoupon->user_email               = $user->user_email;
-                        $issuedCoupon->issued_date              = date('Y-m-d H:i:s');
-                        $issuedCoupon->status                   = IssuedCoupon::STATUS_ISSUED;
-                        $issuedCoupon->record_exists            = 'Y';
+                        $issuedCoupon->promotion_id  = $coupon_id;
+                        $issuedCoupon->user_id       = $user->user_id;
+                        $issuedCoupon->user_email    = $user->user_email;
+                        $issuedCoupon->issued_date   = date('Y-m-d H:i:s');
+                        $issuedCoupon->status        = IssuedCoupon::STATUS_RESERVED;
+                        $issuedCoupon->record_exists = 'Y';
                         $issuedCoupon->save();
 
                     } elseif ($coupon->promotion_type === 'hot_deals') {
@@ -124,7 +124,7 @@ class CouponBuyAPIController extends PubControllerAPI
                         $issuedCoupon->user_id     = $user->user_id;
                         $issuedCoupon->user_email  = $user->user_email;
                         $issuedCoupon->issued_date = date('Y-m-d H:i:s');
-                        $issuedCoupon->status      = IssuedCoupon::STATUS_ISSUED;
+                        $issuedCoupon->status      = IssuedCoupon::STATUS_RESERVED;
                         $issuedCoupon->save();
 
                     }
