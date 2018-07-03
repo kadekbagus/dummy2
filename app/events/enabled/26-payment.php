8474c9e0a487a3a8a7828ad0890ce16fb6ccf010
Budi Raharja <budi@dominopos.com>
2018-07-03 18:57:08 +0800

    Clean up payment event listener: remove unused imports & listener.

--- app/events/enabled/26-payment.php
+++ app/events/enabled/26-payment.php
@@ -3,38 +3,6 @@
  * Event Listener related to Payment.
  *
  */
-use OrbitShop\API\v1\Helper\Input as OrbitInput;
-use Orbit\Helper\MongoDB\Client as MongoClient;
-use Orbit\Helper\Util\LandingPageUrlGenerator as LandingPageUrlGenerator;
-use Carbon\Carbon as Carbon;
-use Orbit\Helper\Util\CdnUrlGenerator;
-use Orbit\Helper\OneSignal\OneSignal;
-
-use Orbit\Helper\Sepulsa\API\TakeVoucher;
-use Orbit\Helper\Sepulsa\API\Responses\TakeVoucherResponse;
-
-// User Notifications
-use Orbit\Notifications\Coupon\Sepulsa\ReceiptNotification as SepulsaReceiptNotification;
-use Orbit\Notifications\Coupon\Sepulsa\TakeVoucherFailureNotification;
-use Orbit\Notifications\Coupon\Sepulsa\VoucherNotAvailableNotification as SepulsaVoucherNotAvailableNotification;
-
-use Orbit\Notifications\Coupon\HotDeals\ReceiptNotification as HotDealsReceiptNotification;
-use Orbit\Notifications\Coupon\HotDeals\CouponNotAvailableNotification as HotDealsCouponNotAvailableNotification;
-
-use Orbit\Notifications\Coupon\CouponNotAvailableNotification;
-use Orbit\Notifications\Coupon\CustomerCouponNotAvailableNotification;
-
-/**
- * Listen on:    `orbit.payment.postupdatepayment.after.save`
- *
- * @author Budi <budi@dominopos.com>
- *
- * @param PaymentTransaction $payment - Instance of PaymentTransaction model
- */
-Event::listen('orbit.payment.postupdatepayment.after.save', function(PaymentTransaction $payment, $retries = 0, $sendNotification = false)
-{
-
-});
 
 /**
  * Listen on:    `orbit.payment.postupdatepayment.after.commit`
