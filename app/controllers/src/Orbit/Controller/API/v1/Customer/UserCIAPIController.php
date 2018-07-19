f7122052a42f6ef6dc0ea892b0eb8bcf9c57e4cc
firmansyah <firmansyah@dominopos.com>
2018-07-19 11:12:13 +0800

    Fix showing gender on my account

--- app/controllers/src/Orbit/Controller/API/v1/Customer/UserCIAPIController.php
+++ app/controllers/src/Orbit/Controller/API/v1/Customer/UserCIAPIController.php
@@ -75,6 +75,7 @@ class UserCIAPIController extends BaseAPIController
             $data->role = $role->role_name;
             $data->image = $image;
             $data->phone = $userDetail->userdetail->phone;
+            $data->gender = $userDetail->userdetail->gender;
 
             $this->response->data = $data;
             $this->response->code = 0;
