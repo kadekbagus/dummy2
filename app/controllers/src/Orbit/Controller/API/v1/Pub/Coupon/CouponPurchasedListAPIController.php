ac91d205cf35c51fee7a128092113fbb7668f068
zamroni <zamroni@dominopos.com>
2018-07-27 10:11:21 +0800

    hide denied from purchased list

--- app/controllers/src/Orbit/Controller/API/v1/Pub/Coupon/CouponPurchasedListAPIController.php
+++ app/controllers/src/Orbit/Controller/API/v1/Pub/Coupon/CouponPurchasedListAPIController.php
@@ -160,7 +160,7 @@ class CouponPurchasedListAPIController extends PubControllerAPI
                             ->where('payment_transactions.user_id', $user->user_id)
                             ->where('payment_transaction_details.object_type', 'coupon')
                             ->where('payment_transactions.payment_method', '!=', 'normal')
-                            ->where('payment_transactions.status', '!=', 'starting')
+                            ->whereNotIn('payment_transactions.status', array('starting', 'denied'))
                             ->groupBy('payment_transactions.payment_transaction_id');
 
             OrbitInput::get('filter_name', function ($filterName) use ($coupon, $prefix) {
