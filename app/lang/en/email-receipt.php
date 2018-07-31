cdba142ae78184d00ba31213ecca52b65d1d7e17
Budi Raharja <budi@dominopos.com>
2018-07-31 13:33:39 +0800

    Add color to telp and email.
    Add translation key 'subject'.

--- app/lang/en/email-receipt.php
+++ app/lang/en/email-receipt.php
@@ -1,6 +1,8 @@
 <?php 
 
 return [
+    'subjectt' => 'Your Receipt from Gotomalls.com',
+
     'header' => [
         'invoice'       => 'Receipt',
         'order_number'  => 'Transaction ID: :transactionId',
@@ -13,7 +15,7 @@ return [
 
         'redeem' => 'To see your purchased item and redeem/collect at the stores, please click the button below.',
 
-        'help' => 'Please contact our customer service at :csPhone or email at <a href="mailto::csEmail">:csEmail</a> if you find any difficulties.',
+        'help' => 'Please contact our customer service at <a style="color:#f43d3c;text-decoration:none;" href="tel::csPhone">:csPhone</a> or email at <a style="color:#f43d3c;text-decoration:none;" href="mailto::csEmail">:csEmail</a> if you find any difficulties.',
         'thank_you' => 'Thank you and have a nice day.',
     ],
 
