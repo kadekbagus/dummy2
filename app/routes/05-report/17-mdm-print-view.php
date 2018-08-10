b74983478708966161d1aa85424035ce6b836f1f
kadekbagus <kadek@dominopos.com>
2018-08-10 17:02:52 +0800

    fix format for MDM store csv

--- app/routes/05-report/17-mdm-print-view.php
+++ app/routes/05-report/17-mdm-print-view.php
@@ -3,4 +3,4 @@
  * Report Coupon By Name
  */
 Route::get('/printer/mdm-store/list', 'Report\MdmStorePrinterController@getPrintMdmStoreReport');
-Route::get('/printer/mdm-merchant/list', 'Report\MdmMerchantPrinterController@getPrintMdmMerchantReport');
+Route::get('/printer/mdm-merchant-store-location/list', 'Report\MdmMerchantStoreLocationPrinterController@getPrintMdmMerchantStoreLocationReport');
