8eb9f08633bee6f2a66191449ed694433e581198
Budi <budi@dominopos.com>
2018-09-12 16:42:31 +0800

    Should just decode data payment midtrans data from table, no need to remapping.
    Use array instead of object to access payment info.

--- app/notifications/Orbit/Notifications/Traits/HasPaymentTrait.php
+++ app/notifications/Orbit/Notifications/Traits/HasPaymentTrait.php
@@ -10,49 +10,6 @@ trait HasPaymentTrait
 {
     protected $payment = null;
 
-    protected $bankMapper = [
-        'mandiri' => [
-            'label' => 'Mandiri',
-            'code' => '008',
-        ],
-        'bni' => [
-            'label' => 'BNI',
-            'code' => '009',
-        ],
-        'bca' => [
-            'label' => 'BCA',
-            'code' => '014',
-        ],
-        'permata' => [
-            'label' => 'Permata Bank',
-            'code' => '013',
-        ],
-        'cimb' => [
-            'label' => 'CIMB',
-            'code' => '022',
-        ],
-        'bri' => [
-            'label' => 'BRI',
-            'code' => '002',
-        ],
-        'danamon' => [
-            'label' => 'Danamon',
-            'code' => '011',
-        ],
-        'maybank' => [
-            'label' => 'Maybank',
-            'code' => '016',
-        ],
-        'mega' => [
-            'label' => 'Bank Mega',
-            'code' => '426',
-        ],
-        'other' => [
-            'label' => 'Other Bank',
-            'code' => '013',
-        ],
-    ];
-
     /**
      * Get the transaction data.
      *
@@ -119,52 +76,10 @@ trait HasPaymentTrait
      */
     protected function getPaymentInfo()
     {
-        $paymentMethod = null;
-        if ($this->payment->paidWith(['echannel', 'bank_transfer'])) {
-            if (! empty($this->payment->midtrans)) {
-                $paymentInfo = json_decode(unserialize($this->payment->midtrans->payment_midtrans_info));
+        $paymentMethod = [];
 
-                if (! empty($paymentInfo)) {
-                    $virtualBank = isset($paymentInfo->va_numbers) ? $paymentInfo->va_numbers : null;
-                    $billKey = isset($paymentInfo->bill_key) ? $paymentInfo->bill_key : null;
-                    $billerCode = isset($paymentInfo->biller_code) ? $paymentInfo->biller_code : null;
-                    $paymentType = isset($paymentInfo->payment_type) ? $paymentInfo->payment_type : null;
-                    $pdfUrl = isset($paymentInfo->pdf_url) ? $paymentInfo->pdf_url : null;
-
-                    $vaNumber = isset($paymentInfo->permata_va_number) ? $paymentInfo->permata_va_number : null;
-
-                    switch ($paymentInfo->payment_type) {
-                        case 'credit_card':
-                            $bank = isset($paymentInfo->bank) ? $paymentInfo->bank : null;
-                            break;
-                        case 'echannel':
-                            $bank = 'mandiri';
-                            break;
-                        case 'bank_transfer':
-                            $bank = 'permata';
-                            if (empty($vaNumber)) {
-                                $bank = ! empty($virtualBank) ? $virtualBank[0]->bank : $bank;
-                                $vaNumber = ! empty($virtualBank) ? $virtualBank[0]->va_number : null;
-                            }
-                            break;
-                        default:
-                            $bank = null;
-                            break;
-                    }
-
-                    $bankDetail = ! empty($bank) ? $this->bankMapper[$bank] : null;
-
-                    $paymentMethod = array_filter([
-                        'bank' => $bank,
-                        'va_number' => $vaNumber,
-                        'bill_key' => $billKey,
-                        'biller_code' => $billerCode,
-                        'payment_type' => $paymentType,
-                        'pdf_url' => $pdfUrl,
-                        'bank_detail' => $bankDetail,
-                    ]);
-                }
-            }
+        if (! empty($this->payment->midtrans) && $this->payment->paidWith(['echannel', 'bank_transfer'])) {
+            $paymentMethod = json_decode(unserialize($this->payment->midtrans->payment_midtrans_info), true);
         }
 
         return $paymentMethod;
