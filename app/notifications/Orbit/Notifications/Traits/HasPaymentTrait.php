e552634fc2a22dbbe251a83b8cf6176f7610513a
Budi <budi@dominopos.com>
2018-09-12 16:22:11 +0800

    Add new method to get payment info.
    Move payment and contact property to traits.

--- app/notifications/Orbit/Notifications/Traits/HasPaymentTrait.php
+++ app/notifications/Orbit/Notifications/Traits/HasPaymentTrait.php
@@ -1,19 +1,64 @@
 <?php namespace Orbit\Notifications\Traits;
 
 /**
- * A trait that indicate that the using object/model 
+ * A trait that indicate that the using object/model
  * *should* have PaymentTransaction instance as property in it.
  *
  * @author Budi <budi@dominopos.com>
  */
-trait HasPaymentTrait 
+trait HasPaymentTrait
 {
+    protected $payment = null;
+
+    protected $bankMapper = [
+        'mandiri' => [
+            'label' => 'Mandiri',
+            'code' => '008',
+        ],
+        'bni' => [
+            'label' => 'BNI',
+            'code' => '009',
+        ],
+        'bca' => [
+            'label' => 'BCA',
+            'code' => '014',
+        ],
+        'permata' => [
+            'label' => 'Permata Bank',
+            'code' => '013',
+        ],
+        'cimb' => [
+            'label' => 'CIMB',
+            'code' => '022',
+        ],
+        'bri' => [
+            'label' => 'BRI',
+            'code' => '002',
+        ],
+        'danamon' => [
+            'label' => 'Danamon',
+            'code' => '011',
+        ],
+        'maybank' => [
+            'label' => 'Maybank',
+            'code' => '016',
+        ],
+        'mega' => [
+            'label' => 'Bank Mega',
+            'code' => '426',
+        ],
+        'other' => [
+            'label' => 'Other Bank',
+            'code' => '013',
+        ],
+    ];
+
     /**
      * Get the transaction data.
      *
      * @todo  return transaction as object instead of array. (need to adjust the view/email templates)
      * @todo  use presenter helper.
-     * 
+     *
      * @return [type] [description]
      */
     protected function getTransactionData()
@@ -55,7 +100,7 @@ trait HasPaymentTrait
 
     /**
      * Get the customer data.
-     * 
+     *
      * @return [type] [description]
      */
     protected function getCustomerData()
@@ -67,4 +112,61 @@ trait HasPaymentTrait
         ];
     }
 
+    /**
+     * Get the Payment info.
+     *
+     * @return [type] [description]
+     */
+    protected function getPaymentInfo()
+    {
+        $paymentMethod = null;
+        if ($this->payment->paidWith(['echannel', 'bank_transfer'])) {
+            if (! empty($this->payment->midtrans)) {
+                $paymentInfo = json_decode(unserialize($this->payment->midtrans->payment_midtrans_info));
+
+                if (! empty($paymentInfo)) {
+                    $virtualBank = isset($paymentInfo->va_numbers) ? $paymentInfo->va_numbers : null;
+                    $billKey = isset($paymentInfo->bill_key) ? $paymentInfo->bill_key : null;
+                    $billerCode = isset($paymentInfo->biller_code) ? $paymentInfo->biller_code : null;
+                    $paymentType = isset($paymentInfo->payment_type) ? $paymentInfo->payment_type : null;
+                    $pdfUrl = isset($paymentInfo->pdf_url) ? $paymentInfo->pdf_url : null;
+
+                    $vaNumber = isset($paymentInfo->permata_va_number) ? $paymentInfo->permata_va_number : null;
+
+                    switch ($paymentInfo->payment_type) {
+                        case 'credit_card':
+                            $bank = isset($paymentInfo->bank) ? $paymentInfo->bank : null;
+                            break;
+                        case 'echannel':
+                            $bank = 'mandiri';
+                            break;
+                        case 'bank_transfer':
+                            $bank = 'permata';
+                            if (empty($vaNumber)) {
+                                $bank = ! empty($virtualBank) ? $virtualBank[0]->bank : $bank;
+                                $vaNumber = ! empty($virtualBank) ? $virtualBank[0]->va_number : null;
+                            }
+                            break;
+                        default:
+                            $bank = null;
+                            break;
+                    }
+
+                    $bankDetail = ! empty($bank) ? $this->bankMapper[$bank] : null;
+
+                    $paymentMethod = array_filter([
+                        'bank' => $bank,
+                        'va_number' => $vaNumber,
+                        'bill_key' => $billKey,
+                        'biller_code' => $billerCode,
+                        'payment_type' => $paymentType,
+                        'pdf_url' => $pdfUrl,
+                        'bank_detail' => $bankDetail,
+                    ]);
+                }
+            }
+        }
+
+        return $paymentMethod;
+    }
 }
