04340b8c6c8bc5dbf015dff0585bf8f883bec0cf
kadekbagus <kadek@dominopos.com>
2018-10-10 15:08:49 +0800

    fix filter gender cache key

--- app/controllers/src/Orbit/Controller/API/v1/Pub/Coupon/CouponListNewAPIController.php
+++ app/controllers/src/Orbit/Controller/API/v1/Pub/Coupon/CouponListNewAPIController.php
@@ -165,8 +165,7 @@ class CouponListNewAPIController extends PubControllerAPI
                 'no_total_record' => $no_total_records,
                 'take' => $take, 'skip' => $skip,
                 'country' => $countryFilter, 'cities' => $cityFilters,
-                'my_cc_filter' => $myCCFilter,
-                'gender' => $gender
+                'my_cc_filter' => $myCCFilter
             ];
 
             // Run the validation
@@ -272,6 +271,7 @@ class CouponListNewAPIController extends PubControllerAPI
                         }
                     }
                 }
+                $cacheKey['gender'] = $filterGender;
                 $this->searcher->filterByGender(strtolower($filterGender));
             }
 
