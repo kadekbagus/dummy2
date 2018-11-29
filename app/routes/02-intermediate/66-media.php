0612bd6dd7ccfd38b4c0ff051648ac360a4cbfcd
Ahmad <ahmad@dominopos.com>
2018-11-29 15:15:13 +0800

    Add media delete route

--- app/routes/02-intermediate/66-media.php
+++ app/routes/02-intermediate/66-media.php
@@ -1,3 +1,5 @@
 <?php
 
 Route::post('/app/v1/media/upload', ['as' => 'api-media-upload', 'uses' => 'IntermediateAuthController@Media_upload']);
+
+Route::post('/app/v1/media/delete', ['as' => 'api-media-delete', 'uses' => 'IntermediateAuthController@Media_delete']);
