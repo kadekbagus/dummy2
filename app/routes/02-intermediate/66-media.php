978605ae771d6919a7aabcd8cd6a4a2ddb23fff0
Ahmad <ahmad@dominopos.com>
2018-11-29 16:45:00 +0800

    Add image media list route

--- app/routes/02-intermediate/66-media.php
+++ app/routes/02-intermediate/66-media.php
@@ -3,3 +3,5 @@
 Route::post('/app/v1/media/upload', ['as' => 'api-media-upload', 'uses' => 'IntermediateAuthController@Media_upload']);
 
 Route::post('/app/v1/media/delete', ['as' => 'api-media-delete', 'uses' => 'IntermediateAuthController@Media_delete']);
+
+Route::get('/app/v1/media/list', ['as' => 'api-media-list', 'uses' => 'IntermediateAuthController@Media_get']);
