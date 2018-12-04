031e0a6989f094999423a8e4915f8a769b56bd01
firmansyah <firmansyah@dominopos.com>
2018-12-04 18:04:50 +0800

    Article model

---
+++ app/models/Article.php
@@ -0,0 +1,15 @@
+<?php
+class Article extends Eloquent
+{
+    /**
+     * Article Model
+     *
+     * @author Firmansyah <firmansyah@dominopos.com>
+     */
+
+    protected $table = 'article';
+
+    protected $primaryKey = 'article_id';
+
+
+}
