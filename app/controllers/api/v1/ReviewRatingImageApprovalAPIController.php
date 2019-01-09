6144ca7089ab267b1e0c02daedfa5897db5caabc
firmansyah <firmansyah@dominopos.com>
2019-01-09 10:42:10 +0800

    Fix multiple rejected multiple image

--- app/controllers/api/v1/ReviewRatingImageApprovalAPIController.php
+++ app/controllers/api/v1/ReviewRatingImageApprovalAPIController.php
@@ -80,7 +80,7 @@ class ReviewRatingImageApprovalAPIController extends ControllerAPI
 
             $getReview = $mongoClient->setEndPoint("reviews/$reviewId")->request('GET');
 
-            $newImages = array();
+            $newImages = '';
             // Update status image, or deleted when rejected
             foreach ($getReview->data->images as $key => $images) {
                 $status = $images[0]->approval_status;
