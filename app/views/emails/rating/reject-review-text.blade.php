9be772a4e89ce2f532e7a8d1a0ada0149845cb18
zamroni <zamroni@dominopos.com>
2019-02-13 15:23:40 +0800

    add email template for reject review

---
+++ app/views/emails/rating/reject-review-text.blade.php
@@ -0,0 +1,13 @@
+Dear, {{ $fullname }}
+
+We are sorry, your review on {{$location}} has been rejected.
+
+The details as follow:
+Date: {{ $date }} UTC
+Type: {{ $type }}
+Location: {{ $location_detail }}
+Name: {{ $name }}
+Email: {{ $email }}
+Review ID: {{ $review_id }}
+Rating: {{ $rating }}
+Review: {{ $review }}
