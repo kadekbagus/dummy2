843a8d78dae5a1ce93e3674621c897b4ba56c882
Budi <budi@dominopos.com>
2019-02-14 11:31:31 +0800

    Send notification to GTM User if their review has been rejected/deleted.

--- app/views/emails/rating/reject-review-text.blade.php
+++ app/views/emails/rating/reject-review-text.blade.php
@@ -1,13 +1,3 @@
 Dear, {{ $fullname }}
 
-We are sorry, your review on {{$location}} has been rejected.
-
-The details as follow:
-Date: {{ $date }} UTC
-Type: {{ $type }}
-Location: {{ $location_detail }}
-Name: {{ $name }}
-Email: {{ $email }}
-Review ID: {{ $review_id }}
-Rating: {{ $rating }}
-Review: {{ $review }}
+We are sorry, your review for {{ $campaignAndLocationInfo }} has been rejected.
