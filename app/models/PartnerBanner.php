4962d2d2bbe821b18aeed8732bf1c053b1e1a9ac
kadek <kadek@dominopos.com>
2019-03-18 15:55:52 +0800

    add model for partner_banners and partner_categories

---
+++ app/models/PartnerBanner.php
@@ -0,0 +1,8 @@
+<?php
+
+class PartnerBanner extends Eloquent
+{
+    protected $primaryKey = 'partner_banner_id';
+
+    protected $table = 'partner_banners';
+}
---
+++ app/models/PartnerCategory.php
@@ -0,0 +1,8 @@
+<?php
+
+class PartnerCategory extends Eloquent
+{
+    protected $primaryKey = 'partner_category_id';
+
+    protected $table = 'partner_categories';
+}
