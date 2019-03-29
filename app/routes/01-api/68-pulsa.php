f76f2739adf4e9dd5a4b8174bd88a14691ca2f0e
zamroni <zamroni@dominopos.com>
2019-03-29 16:09:09 +0800

    move route for easier access

--- app/routes/01-api/68-pulsa.php
+++ app/routes/01-api/68-pulsa.php
@@ -31,13 +31,3 @@ Route::get('/api/v1/pulsa/detail', function()
 {
     return PulsaAPIController::create()->getDetailPulsa();
 });
-
-/**
- * pulsa purchased transaction detail
- */
-Route::get('/api/v1/pub/pulsa-purchased/detail', function()
-{
-    return Orbit\Controller\API\v1\Pub\Pulsa\PulsaPurchasedDetailAPIController::create()->getPulsaPurchasedDetail();
-});
-
-Route::get('/app/v1/pub/pulsa-purchased/detail', ['as' => 'pub-pulsa-purchased-detail', 'uses' => 'IntermediatePubAuthController@Pulsa\PulsaPurchasedDetail_getPulsaPurchasedDetail']);
