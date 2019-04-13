2987a351148b49f9f9ea18a9062454b1b6fad52f
zamroni <zamroni@dominopos.com>
2019-04-13 17:30:29 +0800

    simplify code to get list

--- app/controllers/src/Orbit/Controller/API/v1/Pub/Coupon/CouponPurchasedDetailAPIController.php
+++ app/controllers/src/Orbit/Controller/API/v1/Pub/Coupon/CouponPurchasedDetailAPIController.php
@@ -166,7 +166,7 @@ class CouponPurchasedDetailAPIController extends PubControllerAPI
 
             $coupon->redeem_codes = null;
             if ($coupon->coupon_type === 'gift_n_coupon') {
-                $issuedCoupon = PaymentTransaction::select('issued_coupons.issued_coupon_code')
+                $coupon->redeem_codes = PaymentTransaction::select('issued_coupons.issued_coupon_code')
                     ->join('payment_transaction_details', 'payment_transaction_details.payment_transaction_id', '=', 'payment_transactions.payment_transaction_id')
                     ->join('issued_coupons', function ($join) {
                         $join->on('issued_coupons.promotion_id', '=', 'payment_transaction_details.object_id');
@@ -177,8 +177,8 @@ class CouponPurchasedDetailAPIController extends PubControllerAPI
                         $query->where('payment_transactions.payment_transaction_id', '=', $payment_transaction_id)
                               ->orWhere('payment_transactions.external_payment_transaction_id', '=', $payment_transaction_id);
                       })
-                    ->get();
-                $coupon->redeem_codes = $issuedCoupon->issued_coupon_code;
+                    ->get()
+                    ->lists('issued_coupon_code');
             }
 
             // Fallback to IDR by default?
