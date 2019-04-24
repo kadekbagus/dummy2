06d158ed721b670bd8e8759d61d5efe09a9186fe
Budi <budi@dominopos.com>
2019-04-24 14:24:19 +0800

    Add Bahasa Indonesian version for pending email coupon.

--- app/views/emails/pending-payment/payment-info.blade.php
+++ app/views/emails/pending-payment/payment-info.blade.php
@@ -1,7 +1,7 @@
               @if (! empty($paymentInfo) && isset($paymentInfo['bank_detail']))
                 @if (! isset($hideExpiration))
                   <p style="font-family:'Roboto', 'Arial', sans-serif;margin:0;">
-                    {{ trans('email-pending-payment.body.payment-info-line-1', compact('paymentExpiration')) }}
+                    {{ trans('email-pending-payment.body.payment-info-line-1', compact('paymentExpiration'), '', 'id') }}
                   </p>
                   <br>
                 @endif
@@ -10,20 +10,20 @@
 
                     @if ($paymentInfo['payment_type'] === 'echannel')
                       <p>
-                        <strong>{{{ trans('email-pending-payment.body.payment-info.bank_name') }}}</strong> {{ $paymentInfo['bank_detail']['label'] }}
+                        <strong>{{{ trans('email-pending-payment.body.payment-info.bank_name', [], '', 'id') }}}</strong> {{ $paymentInfo['bank_detail']['label'] }}
                         <br>
-                        <strong>{{{ trans('email-pending-payment.body.payment-info.biller_code') }}}</strong> {{ $paymentInfo['biller_code'] }}
+                        <strong>{{{ trans('email-pending-payment.body.payment-info.biller_code', [], '', 'id') }}}</strong> {{ $paymentInfo['biller_code'] }}
                         <br>
-                        <strong>{{{ trans('email-pending-payment.body.payment-info.bill_key') }}}</strong> {{ $paymentInfo['bill_key'] }}
+                        <strong>{{{ trans('email-pending-payment.body.payment-info.bill_key', [], '', 'id') }}}</strong> {{ $paymentInfo['bill_key'] }}
                         <br>
                       </p>
                     @elseif ($paymentInfo['payment_type'] === 'bank_transfer')
                       <p>
-                        <strong>{{{ trans('email-pending-payment.body.payment-info.bank_name') }}}</strong> {{ $paymentInfo['bank_detail']['label'] }}
+                        <strong>{{{ trans('email-pending-payment.body.payment-info.bank_name', [], '', 'id') }}}</strong> {{ $paymentInfo['bank_detail']['label'] }}
                         <br>
-                        <strong>{{{ trans('email-pending-payment.body.payment-info.bank_code') }}}</strong> {{ $paymentInfo['bank_detail']['code'] }}
+                        <strong>{{{ trans('email-pending-payment.body.payment-info.bank_code', [], '', 'id') }}}</strong> {{ $paymentInfo['bank_detail']['code'] }}
                         <br>
-                        <strong>{{{ trans('email-pending-payment.body.payment-info.bank_account_number') }}}</strong> {{ $paymentInfo['va_number'] }}
+                        <strong>{{{ trans('email-pending-payment.body.payment-info.bank_account_number', [], '', 'id') }}}</strong> {{ $paymentInfo['va_number'] }}
                         <br>
                       </p>
                     @endif
@@ -32,25 +32,25 @@
                 </table>
                 <br>
                 <p>
-                  {{{ trans('email-pending-payment.body.payment-info-line-2') }}}
+                  {{{ trans('email-pending-payment.body.payment-info-line-2', [], '', 'id') }}}
                 </p>
 
                 <p style="font-family:'Roboto', 'Arial', sans-serif;margin:0;">
                 @if (isset($paymentInfo['pdf_url']) && ! empty($paymentInfo['pdf_url']))
-                  <a href="{{{ $paymentInfo['pdf_url'] }}}" class="btn-redeem" style="font-family:'Roboto', 'Arial', sans-serif;border-radius:5px;background-color:#2196F3;color:#fff;font-weight:bold;font-size:16px;display:inline-block;padding:10px 0;text-decoration:none;width:35%;text-align:center;margin-right:5px;margin-top:10px;">{{{ trans('email-pending-payment.body.btn_payment_instruction') }}}</a>
+                  <a href="{{{ $paymentInfo['pdf_url'] }}}" class="btn-redeem" style="font-family:'Roboto', 'Arial', sans-serif;border-radius:5px;background-color:#2196F3;color:#fff;font-weight:bold;font-size:16px;display:inline-block;padding:10px 0;text-decoration:none;width:35%;text-align:center;margin-right:5px;margin-top:10px;">{{{ trans('email-pending-payment.body.btn_payment_instruction', [], '', 'id') }}}</a>
                 @endif
-                  <a href="{{{ $myWalletUrl }}}" class="btn-redeem" style="font-family:'Roboto', 'Arial', sans-serif;border-radius:5px;border-width:1px;border-style:solid;border-color:#2196F3;background-color:#fff;color:#2196F3;font-weight:bold;font-size:16px;display:inline-block;text-decoration:none;width:30%;padding:10px 0;margin-right:5px;text-align:center;margin-top:10px;">{{{ trans('email-pending-payment.body.btn_my_wallet') }}}</a>
+                  <a href="{{{ $myWalletUrl }}}" class="btn-redeem" style="font-family:'Roboto', 'Arial', sans-serif;border-radius:5px;border-width:1px;border-style:solid;border-color:#2196F3;background-color:#fff;color:#2196F3;font-weight:bold;font-size:16px;display:inline-block;text-decoration:none;width:30%;padding:10px 0;margin-right:5px;text-align:center;margin-top:10px;">{{{ trans('email-pending-payment.body.btn_my_wallet', [], '', 'id') }}}</a>
                   <a href="{{{ $cancelUrl }}}" class="btn-cancel" style="font-family:'Roboto', 'Arial', sans-serif;border-radius:5px;border-width:1px;border-style:solid;border-color:#666666;background-color:#fff;color:#666666;font-weight:bold;font-size:16px;display:inline-block;padding:10px 0;text-decoration:none;width:30%;text-align:center;margin-top:10px;">
-                      {{{ trans('email-pending-payment.body.btn_cancel_purchase') }}}
+                      {{{ trans('email-pending-payment.body.btn_cancel_purchase', [], '', 'id') }}}
                   </a>
                 </p>
                 <br>
               @elseif (! empty($paymentInfo))
                 <p style="font-family:'Roboto', 'Arial', sans-serif;margin:0;text-align: center;">
                 @if ($paymentInfo['payment_type'] === 'gopay')
-                  <a href="{{{ $myWalletUrl }}}" class="btn-redeem" style="font-family:'Roboto', 'Arial', sans-serif;border-radius:5px;border-width:1px;border-style:solid;border-color:#2196F3;background-color:#fff;color:#2196F3;font-weight:bold;font-size:16px;display:inline-block;text-decoration:none;width:30%;padding:10px 0;margin-right:5px;text-align:center;margin-top:10px;">{{{ trans('email-pending-payment.body.btn_my_wallet') }}}</a>
+                  <a href="{{{ $myWalletUrl }}}" class="btn-redeem" style="font-family:'Roboto', 'Arial', sans-serif;border-radius:5px;border-width:1px;border-style:solid;border-color:#2196F3;background-color:#fff;color:#2196F3;font-weight:bold;font-size:16px;display:inline-block;text-decoration:none;width:30%;padding:10px 0;margin-right:5px;text-align:center;margin-top:10px;">{{{ trans('email-pending-payment.body.btn_my_wallet', [], '', 'id') }}}</a>
                   <a href="{{{ $cancelUrl }}}" class="btn-cancel" style="font-family:'Roboto', 'Arial', sans-serif;border-radius:5px;border-width:1px;border-style:solid;border-color:#666666;background-color:#fff;color:#666666;font-weight:bold;font-size:16px;display:inline-block;padding:10px 0;text-decoration:none;width:30%;text-align:center;margin-top:10px;">
-                      {{{ trans('email-pending-payment.body.btn_cancel_purchase') }}}
+                      {{{ trans('email-pending-payment.body.btn_cancel_purchase', [], '', 'id') }}}
                   </a>
                 @endif
                 </p>
