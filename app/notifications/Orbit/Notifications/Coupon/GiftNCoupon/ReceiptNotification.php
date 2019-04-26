2be658f5247607c7c3f5a974617966e23bec76d8
Budi <budi@dominopos.com>
2019-04-26 14:32:37 +0800

    Use default campaign image if giftncoupon image is empty.

--- app/notifications/Orbit/Notifications/Coupon/GiftNCoupon/ReceiptNotification.php
+++ app/notifications/Orbit/Notifications/Coupon/GiftNCoupon/ReceiptNotification.php
@@ -67,9 +67,7 @@ class ReceiptNotification extends BaseReceiptNotification
      */
     private function getCouponImage()
     {
-        // Default fallback image.
-        $couponImage = 'https://www.gotomalls.com/images/campaign-default.png';
-
+        $couponImage = '';
         $couponId = $this->payment->details->first()->object_id;
         $prefix = DB::getTablePrefix();
         $coupon = Coupon::select(DB::raw("{$prefix}promotions.promotion_id,
@@ -106,6 +104,11 @@ class ReceiptNotification extends BaseReceiptNotification
             $cdnConfig = Config::get('orbit.cdn');
             $imgUrl = CdnUrlGenerator::create(['cdn' => $cdnConfig], 'cdn');
             $couponImage = $imgUrl->getImageUrl($attachmentPath, $cdnUrl);
+        }
+
+        // Default fallback image.
+        if (empty($couponImage)) {
+            $couponImage = 'https://www.gotomalls.com/themes/default/images/campaign-default.png';
         }
 
         return $couponImage;
