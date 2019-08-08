f40f0115805a8be34fd980a0a79da078a662f086
zamroni <zamroni@dominopos.com>
2019-08-08 14:26:20 +0800

    add additional check if promo codes is reserved for current user

--- app/controllers/src/Orbit/Controller/API/v1/Pub/PromoCode/Repositories/Validators/PromoCodeValidator.php
+++ app/controllers/src/Orbit/Controller/API/v1/Pub/PromoCode/Repositories/Validators/PromoCodeValidator.php
@@ -11,6 +11,14 @@ use Pulsa;
 
 class PromoCodeValidator implements ValidatorInterface
 {
+    private $currentUser;
+
+    public function user($currentUser)
+    {
+        $this->currentUser = $currentUser;
+        return $this;
+    }
+
     private function registerCustomValidationRule()
     {
         Validator::extend('active_discount', function($attribute, $value, $parameters) {
@@ -25,6 +33,16 @@ class PromoCodeValidator implements ValidatorInterface
             $discount = DiscountCode::where('discount_code', $value)
                 ->available()
                 ->first();
+
+            if (empty($discount)) {
+                //no more promo code is available, try if current user has
+                //reserved promo code
+                $discount = $this->currentUser
+                    ->discountCodes()
+                    ->where('discount_code', $value)
+                    ->reserved()
+                    ->first();
+            }
             return !empty($discount);
         });
 
