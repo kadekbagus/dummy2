588833eed80e35c07eba53d8c7e073a8a955dacb
zamroni <zamroni@dominopos.com>
2019-08-20 15:39:25 +0800

    fix error user not set when validating unreserve promo code

--- app/controllers/src/Orbit/Controller/API/v1/Pub/PromoCode/Repositories/ReservationRepository.php
+++ app/controllers/src/Orbit/Controller/API/v1/Pub/PromoCode/Repositories/ReservationRepository.php
@@ -66,7 +66,7 @@ class ReservationRepository implements ReservationRepositoryInterface
         $user = $this->authorizer->getUser();
 
         //validate input data or else throws InvalidArgsException
-        $this->validator->validate();
+        $this->validator->user($user)->validate();
 
         $promoData = (object) [
             'promo_code' => OrbitInput::post('promo_code'),
