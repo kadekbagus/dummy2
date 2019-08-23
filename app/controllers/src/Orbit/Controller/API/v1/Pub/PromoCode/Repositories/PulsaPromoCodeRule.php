fd0d932777b89d20bfec4fab2a07a0f74914074b
zamroni <zamroni@dominopos.com>
2019-08-23 13:47:25 +0800

    implement pulsa promo code rule

--- app/controllers/src/Orbit/Controller/API/v1/Pub/PromoCode/Repositories/PulsaPromoCodeRule.php
+++ app/controllers/src/Orbit/Controller/API/v1/Pub/PromoCode/Repositories/PulsaPromoCodeRule.php
@@ -4,6 +4,7 @@ use Orbit\Controller\API\v1\Pub\PromoCode\Repositories\Contracts\RuleInterface;
 use Discount;
 use DiscountCode;
 use ObjectDiscount;
+use Pulsa;
 
 /**
  * Class which determine if user is eligible to get promo code when purchasing
@@ -18,6 +19,36 @@ class PulsaPromoCodeRule extends AbstractPromoCodeRule implements RuleInterface
     protected function isEligibleForObjectType($discountId, $objectId, $objectType)
     {
         return $this->promo->type === 'pulsa';
+    }
+
+    /**---------------------------------------------
+     * get allowed purchase per user between
+     * promo code vs coupon. The less value will be returned
+     *----------------------------------------------
+     * @param Discount $promo
+     * @param Pulsa $pulsa
+     * @return int least value between two
+     * ---------------------------------------------
+     */
+    protected function getMaxAllowedQtyPerUser($promo, $pulsa)
+    {
+        //pulsa does not have limit per user, only promo
+        return (int) $promo->max_per_user;
+    }
+
+    /**---------------------------------------------
+     * get allowed purchase per transaction between
+     * promo code vs coupon. The less value will be returned
+     *----------------------------------------------
+     * @param Discount $promo
+     * @param Pulsa $pulsa
+     * @return int least value between two
+     * ---------------------------------------------
+     */
+    protected function getMaxAllowedQtyPerTransaction($promo, $pulsa)
+    {
+        //pulsa does not have limit per transaction, only promo
+        return $promo->max_per_transaction;
     }
 
     /**---------------------------------------------
@@ -43,11 +74,46 @@ class PulsaPromoCodeRule extends AbstractPromoCodeRule implements RuleInterface
             $promoData->object_type
         );
 
-        $allowedQty = $promoData->quantity;
+        $allowedQty = 0;
+        $adjustedQty = 0;
 
-        if (! $eligible) {
+        if ($eligible) {
+            $pulsa = Pulsa::find($promoData->object_id);
+            $qtyEligible = $this->isEligibleForQuantity(
+                $promo,
+                $pulsa,
+                $pulsa->pulsa_item_id,
+                'pulsa',
+                $user,
+                (int) $promoData->quantity,
+                $promoData->is_final_check
+            );
+
+            $availQtyEligible = $this->isEligibleForAvailQuantity(
+                $promo,
+                $promoData->object_id,
+                $user,
+                (int) $promoData->quantity
+            );
+
+            if (! $promoData->is_final_check) {
+                $allowedQty = min($qtyEligible, $availQtyEligible);
+            } else {
+                $allowedQty = $qtyEligible;
+            }
+            $eligible = ($allowedQty > 0);
+            if (! $eligible) {
+                $rejectReason = 'REMAINING_AVAIL_DISCOUNT_CODE_LESS_THAN_REQUESTED_QTY';
+            }
+        } else {
             $rejectReason = 'DISCOUNT_CODE_NOT_APPLICABLE_TO_PURCHASED_ITEM';
         }
+
+        //if asked quantity > allowed quantity
+        //adjust qty and if adjustedQty is greater than zero assume eligible
+        $adjustedQty = min($allowedQty, $promoData->quantity);
+        $eligible = $eligible || ($adjustedQty > 0);
+
         return $this->buildEligibleStatusResponse(
             $this->promo,
             $user,
