bd57a65a34299780bda115c2e739ab38133880fa
Budi <budi@dominopos.com>
2019-09-25 10:50:44 +0800

    Remove temporary fix.

--- app/notifications/Orbit/Notifications/Pulsa/CustomerPulsaNotAvailableNotification.php
+++ app/notifications/Orbit/Notifications/Pulsa/CustomerPulsaNotAvailableNotification.php
@@ -97,15 +97,6 @@ class CustomerPulsaNotAvailableNotification extends CustomerNotification impleme
     public function toEmail($job, $data)
     {
         try {
-            $blacklistedEmails = [
-                'sputraqu@yahoo.com'
-            ];
-
-            if (in_array($data['recipientEmail'], $blacklistedEmails)) {
-                $job->delete();
-                return;
-            }
-            
             Mail::send($data['template'], $data, function($mail) use ($data) {
                 $emailConfig = Config::get('orbit.registration.mobile.sender');
 
