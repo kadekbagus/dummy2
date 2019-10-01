49d0ecbc16917be5aaa83b4108b0ce4bed7c47d6
Budi <budi@dominopos.com>
2019-10-01 20:00:09 +0800

    Add command to send pulsa pricelist email to customers.

---
+++ app/notifications/Orbit/Notifications/Pulsa/Subscription/PulsaPriceListNotification.php
@@ -0,0 +1,106 @@
+<?php namespace Orbit\Notifications\Pulsa\Subscription;
+
+use DB;
+use Mail;
+use Config;
+use Log;
+use Exception;
+use User;
+use Mall;
+use Carbon\Carbon;
+
+use Orbit\Notifications\Traits\HasContactTrait;
+
+use Orbit\Helper\Notifications\CustomerNotification;
+use Orbit\Helper\Notifications\Contracts\EmailNotificationInterface;
+
+/**
+ * Notify GTM User for Pulsa/Data Plan price list.
+ *
+ * @author Budi <budi@dominopos.com>
+ */
+class PulsaPriceListNotification extends CustomerNotification implements EmailNotificationInterface
+{
+    protected $shouldQueue = true;
+
+    use HasContactTrait;
+
+    /**
+     * Get the email templates.
+     *
+     * @return [type] [description]
+     */
+    public function getEmailTemplates()
+    {
+        return [
+            'html' => 'emails.subscription.pulsa-data-plan-list-html',
+            'text' => 'emails.subscription.pulsa-data-plan-list-text',
+        ];
+    }
+
+    public function getRecipientEmail()
+    {
+        return $this->notifiable->user_email;
+    }
+
+    /**
+     * Get the email data.
+     *
+     * @return [type] [description]
+     */
+    public function getEmailData()
+    {
+        $emailData = [
+            'subject' => trans('email-pulsa-subscription.pulsa-data-plan-list.subject', [], '', 'id'),
+            'customerName' => $this->notifiable->user_firstname . ' ' . $this->notifiable->user_lastname,
+            'cs' => $this->getContactData(),
+            'recipientEmail' => $this->getRecipientEmail(),
+            'pulsaListUrl' => $this->getPulsaListUrl(),
+        ];
+
+        return $emailData;
+    }
+
+    /**
+     * Notify via email.
+     * This method act as custom Queue handler.
+     *
+     * @param  [type] $notifiable [description]
+     * @return [type]             [description]
+     */
+    public function toEmail($job, $data)
+    {
+        try {
+            Mail::send($this->getEmailTemplates(), $data, function($mail) use ($data) {
+                $emailConfig = Config::get('orbit.registration.mobile.sender');
+
+                $mail->subject($data['subject']);
+                $mail->from($emailConfig['email'], $emailConfig['name']);
+                $mail->to($data['recipientEmail']);
+            });
+        } catch (Exception $e) {
+            Log::debug('PulsaListNotification: Email exception. Line:' . $e->getLine() . ', Message: ' . $e->getMessage());
+        }
+
+        $job->delete();
+    }
+
+    /**
+     * Get GTM Pulsa/Data Plan listing page url.
+     *
+     * @return [type] [description]
+     */
+    private function getPulsaListUrl()
+    {
+        $pulsaListUrl = Config::get('orbit.base_landing_page_url', 'https://www.gotomalls.com');
+        $pulsaListUrlConfig = Config::get('orbit.pulsa_list_url', []);
+        if (! empty($pulsaListUrlConfig)) {
+            $pulsaListUrl .= $pulsaListUrlConfig['base_url'];
+            foreach($pulsaListUrlConfig['utm_params'] as $key => $value) {
+                $pulsaListUrl .= "&{$key}={$value}";
+            }
+        }
+
+        return $pulsaListUrl;
+    }
+}
