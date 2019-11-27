f1f74e22e73de19b9433a5e8ea878702b10bed3d
Ahmad <ahmad@dominopos.com>
2019-11-27 12:26:55 +0800

    Change tabs to spaces

--- app/controllers/src/Orbit/Controller/API/v1/RGP/PulsaReportAllUserAPIController.php
+++ app/controllers/src/Orbit/Controller/API/v1/RGP/PulsaReportAllUserAPIController.php
@@ -16,41 +16,45 @@ use RgpUser;
 
 class PulsaReportAllUserAPIController extends ControllerAPI
 {
-	public function get()
-	{
-		try {
-			$httpCode = 200;
-			// authenticate
-			$sessionKey = Config::get('orbit.session.app_list.rgp_portal', 'X-OMS-RGP');
-			$sessionString = OrbitInput::get($sessionKey);
+    public function get()
+    {
+        try {
+            $httpCode = 200;
+            // authenticate
+            $sessionKey = Config::get('orbit.session.app_list.rgp_portal', 'X-OMS-RGP');
+            $sessionString = OrbitInput::get($sessionKey);
 
-			$session = DB::table('sessions')
-				->where('session_id', $sessionString)
-				->first();
+            if (empty($sessionKey) || empty($sessionString)) {
+                throw new Exception("Error Processing Request", 1);
+            }
 
-			if (! is_object($session)) {
-				throw new Exception("You need to login to continue.", 1);
-			}
+            $session = DB::table('sessions')
+                ->where('session_id', $sessionString)
+                ->first();
 
-			$sessionData = unserialize($session->session_data);
+            if (! is_object($session)) {
+                throw new Exception("You need to login to continue.", 1);
+            }
 
-			if (! isset($sessionData->value['email'])) {
-				throw new Exception("You need to login to continue.", 1);
-			}
+            $sessionData = unserialize($session->session_data);
 
-			$userEmail = $sessionData->value['email'];
-			$user = RgpUser::active()->where('email', $userEmail)->first();
+            if (! isset($sessionData->value['email'])) {
+                throw new Exception("You need to login to continue.", 1);
+            }
 
-			if (! is_object($user)) {
-				throw new Exception("You need to login to continue.", 1);
-			}
+            $userEmail = $sessionData->value['email'];
+            $user = RgpUser::active()->where('email', $userEmail)->first();
 
-			// get inputs
-			$startDateInput = OrbitInput::get('start_date');
-			$endDateInput = OrbitInput::get('end_date');
-			$page = OrbitInput::get('page', 1);
+            if (! is_object($user)) {
+                throw new Exception("You need to login to continue.", 1);
+            }
 
-			$validator = Validator::make(
+            // get inputs
+            $startDateInput = OrbitInput::get('start_date');
+            $endDateInput = OrbitInput::get('end_date');
+            $page = OrbitInput::get('page', 1);
+
+            $validator = Validator::make(
                 array(
                     'start_date' => $startDateInput,
                     'end_date' => $endDateInput,
@@ -68,42 +72,42 @@ class PulsaReportAllUserAPIController extends ControllerAPI
             }
 
             $startDate = new Carbon($startDateInput);
-			$endDate = new Carbon($endDateInput);
-			$take = 20;
-			$skip = ($page - 1) * $take;
+            $endDate = new Carbon($endDateInput);
+            $take = 20;
+            $skip = ($page - 1) * $take;
 
-			// query
-			$prefix = DB::getTablePrefix();
-			$result = DB::select(
-				DB::raw(
-					"select user_id, user_email, count(pt.payment_transaction_id) as total_transactions, sum(amount) as total_amount
-					from {$prefix}payment_transactions pt
-					LEFT JOIN
-						{$prefix}payment_transaction_details ptd
-					    ON ptd.payment_transaction_id = pt.payment_transaction_id
-					where pt.status = 'success'
-					and ptd.object_type = 'pulsa'
-					and pt.created_at between {$this->quote($startDate)} and {$this->quote($endDate)}
-					group by user_id
-					order by total_transactions desc
-					limit {$skip}, {$take}"
-				)
-			);
+            // query
+            $prefix = DB::getTablePrefix();
+            $result = DB::select(
+                DB::raw(
+                    "select user_id, user_email, count(pt.payment_transaction_id) as total_transactions, sum(amount) as total_amount
+                    from {$prefix}payment_transactions pt
+                    LEFT JOIN
+                        {$prefix}payment_transaction_details ptd
+                        ON ptd.payment_transaction_id = pt.payment_transaction_id
+                    where pt.status = 'success'
+                    and ptd.object_type = 'pulsa'
+                    and pt.created_at between {$this->quote($startDate)} and {$this->quote($endDate)}
+                    group by user_id
+                    order by total_transactions desc
+                    limit {$skip}, {$take}"
+                )
+            );
 
-			$this->response->data = $result;
+            $this->response->data = $result;
 
-		} catch (Exception $e) {
-			$this->response->code = $e->getCode();
+        } catch (Exception $e) {
+            $this->response->code = $e->getCode();
             $this->response->status = 'error';
             $this->response->message = $e->getMessage();
             $this->response->data = null;
             $httpCode = 500;
-		}
+        }
 
         return $this->render($httpCode);
-	}
+    }
 
-	private function quote($arg)
+    private function quote($arg)
     {
         return DB::connection()->getPdo()->quote($arg);
     }
