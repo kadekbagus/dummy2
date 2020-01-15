7e6c9721d42bc4b5cbd281b453816a234397177c
Budi <budi@dominopos.com>
2020-01-15 10:16:18 +0800

    (feature) digital product: add relation digital product to games.

--- app/models/DigitalProduct.php
+++ app/models/DigitalProduct.php
@@ -2,6 +2,8 @@
 
 /**
  * Digital Product Model.
+ *
+ * @author Budi <budi@gotomalls.com>
  */
 class DigitalProduct extends Eloquent
 {
@@ -15,13 +17,34 @@ class DigitalProduct extends Eloquent
 
     protected $primaryKey = 'digital_product_id';
 
+    /**
+     * Filter/scope that make sure Digital Product is available.
+     *
+     * @param  [type] $query [description]
+     * @return [type]        [description]
+     */
     public function scopeAvailable($query)
     {
         return $query->where('status', 'active')->where('is_displayed', 'yes');
     }
 
+    /**
+     * Filter that Digital Product is in Promo.
+     *
+     * @param  [type] $query [description]
+     * @return [type]        [description]
+     */
     public function scopeIsPromo($query)
     {
         return $query->where('is_promo', 'yes');
+    }
+
+    /**
+     * Many to many relationship with Game.
+     * @return [type] [description]
+     */
+    public function games()
+    {
+        return $this->belongsToMany(Game::class, 'digital_product_game')->withTimestamps();
     }
 }
