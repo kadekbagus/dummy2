da0730b82cff70072fffe574a6adeca3a5f49089
Budi <budi@dominopos.com>
2020-01-20 11:20:03 +0800

    (feature) digital product: update routes and humanize product_type, e.g. 'Game Voucher' instead of 'game_voucher'

--- app/routes/11-product/04-digital-product.php
+++ app/routes/11-product/04-digital-product.php
@@ -1,30 +1,13 @@
 <?php
 
 // Route for Digital Product listing
-// Route::get('/api/v1/digital-product/{search}', function()
-// {
-//     return Orbit\Controller\API\v1\Product\DigitalProduct\DigitalProductListAPIController::create()->getList();
-// })->where('search', '(list|search)');
-
 Route::get('/app/v1/digital-product/{search}', ['as' => 'digital-product-list', 'uses' => 'IntermediateProductAuthController@DigitalProduct\DigitalProductList_getList'])->where('search', '(list|search)');
 
-// route for Digital Product new/create
-// Route::post('/api/v1/digital-product/new', function()
-// {
-//     return Orbit\Controller\API\v1\Product\DigitalProduct\DigitalProductNewAPIController::create()->postNewGame();
-// });
-// Route::post('/app/v1/digital-product/new', ['as' => 'digital-product-new', 'uses' => 'IntermediateProductAuthController@Game\GameNew_postNewGame']);
+// Route for Digital Product new/create
+Route::post('/app/v1/digital-product/new', ['as' => 'digital-product-new', 'uses' => 'IntermediateProductAuthController@DigitalProduct\DigitalProductNew_postNew']);
 
 // route for Digital Product detail
-// Route::get('/api/v1/digital-product/detail', function()
-// {
-//     return Orbit\Controller\API\v1\Product\DigitalProduct\DigitalProductDetailAPIController::create()->getDetailGame();
-// });
-Route::get('/app/v1/digital-product/detail', ['as' => 'digital-product-detail', 'uses' => 'IntermediateProductAuthController@DigitalProduct\DigitalProductDetail_getDetailGame']);
+Route::get('/app/v1/digital-product/detail', ['as' => 'digital-product-detail', 'uses' => 'IntermediateProductAuthController@DigitalProduct\DigitalProductDetail_getDetail']);
 
 // route for Digital Product update
-// Route::post('/api/v1/digital-product/update', function()
-// {
-//     return Orbit\Controller\API\v1\Product\DigitalProduct\DigitalProductUpdateAPIController::create()->postUpdateGame();
-// });
-// Route::post('/app/v1/digital-product/update', ['as' => 'digital-product-update', 'uses' => 'IntermediateProductAuthController@DigitalProduct\DigitalProductUpdate_postUpdateGame']);
+Route::post('/app/v1/digital-product/update', ['as' => 'digital-product-update', 'uses' => 'IntermediateProductAuthController@DigitalProduct\DigitalProductUpdate_postUpdate']);
