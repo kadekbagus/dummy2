d78c0638f9f2459fb5fbd151a78a1e307b3167a5
Budi <budi@dominopos.com>
2020-01-20 11:23:10 +0800

    (feature) digital product: add handler for product detail.

---
+++ app/controllers/src/Orbit/Controller/API/v1/Product/DigitalProduct/Resource/DigitalProductResource.php
@@ -0,0 +1,72 @@
+<?php namespace Orbit\Controller\API\v1\Product\DigitalProduct\Resource;
+
+use DigitalProduct;
+use Orbit\Helper\Resource\ResourceAbstract as Resource;
+
+/**
+ * Single Digital Product resource.
+ *
+ * @author Budi <budi@gotomalls.com>
+ */
+class DigitalProductResource extends Resource
+{
+    private $resource = null;
+
+    public function __construct($resource)
+    {
+        $this->resource = $resource;
+    }
+
+    /**
+     * Transform Digital Product object into response data array.
+     *
+     * @return [type] [description]
+     */
+    public function toArray()
+    {
+        if ( empty($this->resource)) {
+            return [];
+        }
+
+        return [
+            'id' => $this->resource->digital_product_id,
+            'type' => $this->resource->product_type,
+            'name' => $this->resource->product_name,
+            'code' => $this->resource->code,
+            'price' => $this->resource->selling_price,
+            'provider_id' => $this->resource->selected_provider_product_id,
+            'provider_name' => $this->resource->provider_name,
+            'status' => $this->resource->status,
+            'displayed' => $this->resource->is_displayed,
+            'promo' => $this->resource->is_promo,
+            'description' => $this->resource->description,
+            'notes' => $this->resource->notes,
+            'extra_field_metadata' => $this->resource->extra_field_metadata,
+            'games' => $this->transformGames(),
+        ];
+    }
+
+    /**
+     * Transform related games info.
+     *
+     * @return [type] [description]
+     */
+    private function transformGames()
+    {
+        if (! isset($this->resource->games)) {
+            $this->resource->load(['games' => function($query) {
+                $query->select('games.game_id', 'game_name');
+            }]);
+        }
+
+        $games = null;
+        foreach($this->resource->games as $game) {
+            $games[] = [
+                'id' => $game->game_id,
+                'name' => $game->game_name,
+            ];
+        }
+
+        return $games;
+    }
+}
