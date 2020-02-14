e1fbb99c70b2ebc6b99431456f6e67f23875bf2a
zamroni <zamroni@dominopos.com>
2020-02-14 10:21:37 +0800

    add validation to game list

---
+++ app/controllers/src/Orbit/Controller/API/v1/Pub/DigitalProduct/Request/GameListRequest.php
@@ -0,0 +1,34 @@
+<?php namespace Orbit\Controller\API\v1\Pub\DigitalProduct\Request;
+
+use Validator;
+use Orbit\Helper\Request\ValidateRequest;
+
+/**
+ * Game List Request
+ *
+ * @author Zamroni <zamroni@gotomalls.com>
+ */
+class GameListRequest extends ValidateRequest
+{
+    /**
+     * Allowed roles to access this request.
+     * @var array
+     */
+    protected $roles = ['guest', 'consumer'];
+
+    /**
+     * Get validation rules.
+     *
+     * @return array array of rules for this request.
+     */
+    public function rules()
+    {
+        return [
+            'skip' => 'integer',
+            'take' => 'integer',
+            'sortby' => 'in:game_id,game_name',
+            'sortmode' => 'in:asc,desc',
+        ];
+    }
+
+}
