fd67f409f92c1638547ab99d765c7a0a7eb40b8a
Budi <budi@dominopos.com>
2020-02-21 16:43:10 +0800

    (hotfix) update GMP params to game_voucher.

--- app/queue/Orbit/Queue/DigitalProduct/GetDigitalProductQueue.php
+++ app/queue/Orbit/Queue/DigitalProduct/GetDigitalProductQueue.php
@@ -143,7 +143,7 @@ class GetDigitalProductQueue
                         'cid' => $cid,
                         't' => 'event',
                         'ea' => 'Purchase Digital Product Successful',
-                        'ec' => 'digital_product',
+                        'ec' => 'game_voucher',
                         'el' => $digitalProductName,
                         'cs' => $payment->utm_source,
                         'cm' => $payment->utm_medium,
@@ -175,7 +175,7 @@ class GetDigitalProductQueue
                             'ip' => $detail->price,
                             'iq' => $detail->quantity,
                             'ic' => $productCode,
-                            'iv' => 'digital_product',
+                            'iv' => 'game_voucher',
                             'cu' => $payment->currency,
                         ])
                         ->request();
@@ -270,7 +270,7 @@ class GetDigitalProductQueue
                         'cid' => time(),
                         't' => 'event',
                         'ea' => 'Purchase Digital Product Failed',
-                        'ec' => 'digital_product',
+                        'ec' => 'game_voucher',
                         'el' => $digitalProductName,
                         'cs' => $payment->utm_source,
                         'cm' => $payment->utm_medium,
