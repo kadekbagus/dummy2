3142b09fd83330792b3975387abc4196611bc711
Budi <budi@dominopos.com>
2020-02-26 11:10:52 +0800

    (hotfix) implement method injection into digital product portal api (3) - add forgotten file ListRequest

---
+++ app/controllers/src/Orbit/Controller/API/v1/Product/DigitalProduct/Request/ListRequest.php
@@ -0,0 +1,32 @@
+<?php namespace Orbit\Controller\API\v1\Product\DigitalProduct\Request;
+
+use Orbit\Helper\Request\ValidateRequest;
+
+/**
+ * Digital Product List Request
+ *
+ * @author Budi <budi@gotomalls.com>
+ */
+class ListRequest extends ValidateRequest
+{
+    /**
+     * Allowed roles to access this request.
+     * @var array
+     */
+    protected $roles = ['product manager'];
+
+    /**
+     * Get validation rules.
+     *
+     * @return array array of rules for this request.
+     */
+    public function rules()
+    {
+        return [
+            'skip' => 'required|integer',
+            'take' => 'required|integer',
+            'sortby' => 'sometimes|in:status,product_type,product_name,selling_price,updated_at',
+            'sortmode' => 'sometimes|in:asc,desc',
+        ];
+    }
+}
