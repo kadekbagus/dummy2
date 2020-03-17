d4297117d25716ef5a21462ac2ef4b0e41f0a413
kadek <kadek@dominopos.com>
2020-03-17 18:13:54 +0800

    artisan command for creating bpp user

--- app/models/BppUser.php
+++ app/models/BppUser.php
@@ -2,7 +2,14 @@
 
 class BppUser extends Eloquent
 {
+	use ModelStatusTrait;
+
     protected $primaryKey = 'bpp_user_id';
 
     protected $table = 'bpp_users';
+
+    public function apikey()
+    {
+        return $this->hasOne('Apikey', 'user_id', 'bpp_user_id')->where('apikeys.status','=','active');
+    }
 }
