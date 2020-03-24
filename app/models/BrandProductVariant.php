2790528b159c15d32f86f039351a4451113c86ba
Budi Raharja <budi@dominopos.com>
2020-03-24 13:57:36 +0800

    feature(4.23): add support for saving variants and brand product variants when creating brand product.

--- app/models/BrandProductVariant.php
+++ app/models/BrandProductVariant.php
@@ -11,6 +11,11 @@ class BrandProductVariant extends Eloquent
 
     protected $primaryKey = 'brand_product_variant_id';
 
+    protected $fillable = [
+        'brand_product_variant_id', 'sku', 'product_code',
+        'original_price', 'selling_price', 'qty',
+    ];
+
     public function variants()
     {
         return $this->hasMany(Variant::class);
