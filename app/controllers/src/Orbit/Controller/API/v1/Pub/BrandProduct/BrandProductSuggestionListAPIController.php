20792c581c0de2cfc8676b05c2d93834dc986a9e
Budi Raharja <budi@dominopos.com>
2020-04-02 20:00:36 +0800

    feature(4.23): add brand product suggestion list api/endpoint.

---
+++ app/controllers/src/Orbit/Controller/API/v1/Pub/BrandProduct/BrandProductSuggestionListAPIController.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace Orbit\Controller\API\v1\Pub\BrandProduct;
+
+use BrandProduct;
+use Exception;
+use OrbitShop\API\v1\PubControllerAPI;
+use Orbit\Controller\API\v1\Pub\BrandProduct\Request\ListRequest;
+use Orbit\Controller\API\v1\Pub\BrandProduct\Resource\BrandProductCollection;
+
+/**
+ * Brand product suggestion (you may also like) list controller.
+ *
+ * @author Budi <budi@gotomalls.com>
+ */
+class BrandProductSuggestionListAPIController extends PubControllerAPI
+{
+    /**
+     * Handle product list request.
+     *
+     * @param  BrandProduct   $brandProduct    [description]
+     * @param  ListRequest    $request         [description]
+     * @return [type]                          [description]
+     */
+    public function handle(BrandProduct $brandProduct, ListRequest $request)
+    {
+        try {
+            // Search/get list of brand products.
+            $brandProducts = $brandProduct->search($request);
+
+            // Map product list result from search provider
+            // to a client-ready collection.
+            $this->response->data = new BrandProductCollection(
+                $brandProducts['hits'],
+                $brandProducts['total']
+            );
+
+        } catch (Exception $e) {
+            return $this->handleException($e, false);
+        }
+
+        return $this->render();
+    }
+}
