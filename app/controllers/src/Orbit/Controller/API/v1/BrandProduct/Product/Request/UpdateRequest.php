e786991d89a624e55649af73c6ace65a97cd329a
Budi Raharja <budi@dominopos.com>
2020-04-03 17:36:12 +0800

    feature(4.23):
    - Add more validations when updating brand product.
    
    - Move update and reservation handler into its own file so the repo less cluttered.

--- app/controllers/src/Orbit/Controller/API/v1/BrandProduct/Product/Request/UpdateRequest.php
+++ app/controllers/src/Orbit/Controller/API/v1/BrandProduct/Product/Request/UpdateRequest.php
@@ -2,7 +2,9 @@
 
 namespace Orbit\Controller\API\v1\BrandProduct\Product\Request;
 
+use Orbit\Controller\API\v1\BrandProduct\Product\Validator\BrandProductValidator;
 use Orbit\Helper\Request\ValidateRequest;
+use Validator;
 
 /**
  * Brand Product Update request.
@@ -15,8 +17,25 @@ class UpdateRequest extends ValidateRequest
     {
         return [
             'brand_product_id' => 'required',
-            'variants' => 'sometimes|required',
-            'brand_product_variants' => 'sometimes|required',
+            'product_name' => 'sometimes|required',
+            'category_id' => 'sometimes|required',
+            'variants' => 'sometimes|required|orbit.brand_product.variants',
+            'brand_product_variants' =>
+                'sometimes|required|orbit.brand_product.product_variants',
+            'brand_product_main_photo' => 'sometimes|required|image|max:1024',
         ];
+    }
+
+    protected function registerCustomValidations()
+    {
+        Validator::extend(
+            'orbit.brand_product.variants',
+            BrandProductValidator::class . '@variants'
+        );
+
+        Validator::extend(
+            'orbit.brand_product.product_variants',
+            BrandProductValidator::class . '@productVariants'
+        );
     }
 }
