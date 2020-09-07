8035812e2b6bbbe930407cd972c6889afef0fe9a
Budi Raharja <budi@dominopos.com>
2020-09-07 15:48:37 +0800

    Add upoint dtu provider.

---
+++ app/controllers/src/Orbit/Controller/API/v1/Pub/Purchase/DigitalProduct/UPointHelper.php
@@ -0,0 +1,131 @@
+<?php namespace Orbit\Controller\API\v1\Pub\Purchase\DigitalProduct;
+
+use Log;
+use Exception;
+use Illuminate\Support\Facades\App;
+use Orbit\Helper\DigitalProduct\Providers\PurchaseProviderInterface;
+
+trait UPointHelper
+{
+    /**
+     * Create a UPoint purchase request, then add its response (user detail) into
+     * purchase object.
+     */
+    protected function createUPointPurchase(&$purchase, $digitalProduct, $providerProduct, $request)
+    {
+        if ($providerProduct->provider_name === 'upoint-dtu') {
+            $this->createUPointDTUPurchase($purchase, $digitalProduct, $providerProduct, $request);
+        }
+        else if ($providerProduct->provider_name === 'upoint-voucher') {
+            $this->createUPointVoucherPurchase($purchase, $digitalProduct, $providerProduct, $request);
+        }
+    }
+
+    /**
+     * Create DTU purchase.
+     */
+    protected function createUPointDTUPurchase(&$purchase, $digitalProduct, $providerProduct, $request)
+    {
+        Log::info("Initiating DTU purchase request to UPoint...");
+
+        $purchaseParams = [
+            'trx_id' => $purchase->payment_transaction_id,
+            'product' => $this->getUPointProductCode($digitalProduct, $request),
+            'item' => $providerProduct->code,
+            'user_info' => $this->getUPointUserInfo($providerProduct, $request),
+        ];
+
+        $purchaseResponse = App::make(PurchaseProviderInterface::class, [
+                'providerId' => $providerProduct->provider_name
+            ])->purchase($purchaseParams);
+
+        // Info should contain user information associated with game user_id,
+        // e.g. game nickname and the server name.
+        if ($purchaseResponse->isSuccess()) {
+            $purchase->upoint_info = $this->transformUPointPurchaseInfo(
+                $purchaseResponse
+            );
+
+            Log::info("DTU Purchase created for trxID: {$purchase->payment_transaction_id}");
+        }
+        else {
+            throw new Exception("DTU Purchase request failed!");
+        }
+    }
+
+    /**
+     * Create voucher purchase.
+     */
+    protected function createUPointVoucherPurchase(&$purchase, $digitalProduct, $providerProduct, $request)
+    {
+        Log::info("Initiating Voucher purchase request to UPoint...");
+
+        $purchaseParams = [
+            'trx_id' => $purchase->payment_transaction_id,
+            'product' => $this->getUPointProductCode($digitalProduct, $request),
+            'item' => $digitalProduct->code,
+            'user_info' => $this->getUPointUserInfo($providerProduct, $request),
+        ];
+
+        $purchaseResponse = app(PurchaseProviderInterface::class)->purchase($purchaseParams);
+
+        // Info should contain user information associated with game user_id,
+        // e.g. game nickname and the server name.
+        if ($purchaseResponse->isSuccess()) {
+            $purchase->upoint_info = $this->transformUPointPurchaseInfo(
+                $purchaseResponse->info
+            );
+        }
+        else {
+            throw new Exception("Voucher Purchase request failed!");
+        }
+    }
+
+    /**
+     * Incase we need custom logic.
+     *
+     * @return string
+     */
+    protected function getUPointUserInfo($providerProduct, $request)
+    {
+        $decodedUserInfo = json_decode($request->upoint_user_info);
+        $userInfo = [];
+
+        unset($decodedUserInfo->product_code);
+
+        foreach($decodedUserInfo as $key => $value) {
+            $userInfo[$key] = $value;
+        }
+
+        return json_encode($userInfo);
+    }
+
+    /**
+     * @return string
+     */
+    protected function getUPointProductCode($digitalProduct, $request)
+    {
+        $decodedUserInfo = json_decode($request->upoint_user_info);
+
+        return $decodedUserInfo->product_code ?: '';
+    }
+
+    /**
+     * In-case we need custom logic once we get the real response
+     * from 3rd party server.
+     *
+     * @return object
+     */
+    protected function transformUPointPurchaseInfo($response)
+    {
+        return $response->info;
+    }
+
+    /**
+     * @return string json encoded payment info.
+     */
+    protected function getUPointPaymentInfo($purchase, $request)
+    {
+        return $request->payment_info;
+    }
+}
---
+++ app/helpers/Orbit/Helper/DigitalProduct/Providers/UPoint/API/DTU/ConfirmAPI.php
@@ -0,0 +1,138 @@
+<?php namespace Orbit\Helper\DigitalProduct\Providers\UPoint\API\DTU;
+
+use Orbit\Helper\DigitalProduct\Providers\UPoint\API\DTU\UPointDTUAPI;
+use Orbit\Helper\DigitalProduct\Providers\UPoint\Response\DTU\ConfirmAPIResponse;
+
+/**
+ * Purchase API wrapper for ayopay client.
+ *
+ * @todo  create a proper base api client and response.
+ *
+ * @author Budi <budi@gotomalls.com>
+ */
+class ConfirmAPI extends UPointDTUAPI
+{
+    protected $endPoint = '/dtu/payment';
+
+    protected $shouldMockResponse = true;
+
+    /**
+     * Build purchase api request param (body).
+     * Should return text/xml string.
+     *
+     * @return [type] [description]
+     */
+    protected function buildRequestParam()
+    {
+        return array_merge(
+            $this->requestData,
+            $this->buildPaymentInfo(),
+            [
+                'partner_id' => $this->config['partner_id'],
+                'signature' => $this->generateSignature(),
+            ]
+        );
+    }
+
+    /**
+     * Build payment info data. Later we might need to map payment info based
+     * on game name (some game might need special treatment).
+     *
+     * @return array $paymentInfo
+     */
+    private function buildPaymentInfo()
+    {
+        $paymentInfo = [];
+
+        if (isset($this->requestData['payment_info'])) {
+            $paymentInfo['payment_info'] = $this->requestData['payment_info'];
+        }
+
+        // TODO: Might need to map the payment info based on the game name.
+        // switch ($gameSlug) {
+        //     case 'free-fire':
+        //     case 'arena-of-valor':
+        //     case 'call-of-duty':
+        //     case 'speed-drifters':
+        //         $paymentInfo = [
+        //             'packed_role_id' => $this->requestData['packed_role_id'],
+        //         ];
+        //         break;
+
+        //     case 'mobile-legends':
+        //     case 'roh':
+        //     case 'life-after':
+        //     case 'marvel':
+        //         $paymentInfo = [
+        //             'username' => $this->requestData['username'],
+        //         ];
+        //         break;
+
+        //     case 'point-blank':
+
+        //         break;
+
+        //     default:
+        //         $paymentInfo = json_decode($this->requestData['payment_info'])
+        //         break;
+        // }
+
+        return $paymentInfo;
+    }
+
+    protected function mockResponseData()
+    {
+        $this->randomizeResponseChance[0] = 1;
+
+        if ($this->randomizeResponseChance[0] === 1) {
+            $this->mockResponse = '{
+                "status":100,
+                "status_msg":"OK",
+                "trx_id":"' . $this->requestData['trx_id'] . '",
+                "t_id":"12345678",
+                "info":{
+                    "product":"productCode",
+                    "item":"itemCode",
+                    "amount":"120000",
+                    "user_info":"UserInfo",
+                    "time":"' . date('Y-m-d H:i:s') . '",
+                    "details":{
+                        "packed_role_id":"packedRoleId",
+                        "server_name":"Eternal Love",
+                        "role_name":"arshav"
+                    }
+                }
+            }';
+        }
+        else {
+            $this->mockResponse = '{
+                "status":200,
+                "status_msg":"Bad Request"
+            }';
+        }
+    }
+
+    /**
+     * Format/map the api response.
+     *
+     * @param  [type] $response [description]
+     * @return [type]           [description]
+     */
+    protected function response($response)
+    {
+        return new ConfirmAPIResponse($response);
+    }
+
+    /**
+     * Generate pass/auth code for the Purchase request.
+     *
+     * @return [type] [description]
+     */
+    private function generateSignature()
+    {
+        return sha1(
+            $this->requestData['trx_id']
+            . $this->config['secret_token']
+        );
+    }
+}
