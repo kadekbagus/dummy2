bd600c8ed1f356f7df0ea39f10f942fae3cf9b01
Budi <budi@dominopos.com>
2020-12-16 10:04:40 +0800

    Reindent upoint voucher receipt code.

--- app/views/emails/digital-product/upoint/voucher-receipt.blade.php
+++ app/views/emails/digital-product/upoint/voucher-receipt.blade.php
@@ -22,19 +22,19 @@ Receipt from Gotomalls.com
               <table width="640" cellpadding="0" cellspacing="0" border="0" class="container mobile-full-width">
                 <tr>
                   <td align="center" valign="middle" height="184" class="greeting-title-container" style="border-radius: 5px 5px 0 0;">
-                      <h1 class="greeting-title">
-                        {{ trans('email-receipt.header.invoice', [], '', 'id') }}
-                      </h1>
+                    <h1 class="greeting-title">
+                      {{ trans('email-receipt.header.invoice', [], '', 'id') }}
+                    </h1>
                   </td>
                 </tr>
               </table>
 
               <table width="600" cellpadding="0" cellspacing="0" border="0" class="container">
                 <tr>
-                    <td width="600" align="right" valign="top" class="transaction-date"><strong>{{{ $transactionDateTime }}}</strong></td>
+                  <td width="600" align="right" valign="top" class="transaction-date"><strong>{{{ $transactionDateTime }}}</strong></td>
                 </tr>
                 <tr>
-                    <td height="20" align="center">&nbsp;</td>
+                  <td height="20" align="center">&nbsp;</td>
                 </tr>
 
                 <tr>
@@ -52,112 +52,112 @@ Receipt from Gotomalls.com
                 </tr>
                 <tr>
                   <td width="600" class="mobile center" valign="middle" style="text-align: center;">
-                      <table width="100%">
-                          <tr>
-                              <td colspan="2" class="invoice-body" style="font-family:'Roboto', 'Arial', sans-serif;padding-top:10px;padding-bottom:10px;mso-table-lspace:0pt !important;mso-table-rspace:0pt !important;">
-                                  <table class="no-border customer" width="100%" style="line-height:1.7em;font-size:14px;color:#222;width:100%;border:0;margin-top:30px;border-spacing:0 !important;border-collapse:collapse !important;table-layout:fixed !important;margin:0 auto !important;mso-table-lspace:0pt !important;mso-table-rspace:0pt !important;">
-                                      <tbody>
-                                          <tr>
-                                              <td class="mobile inline-mobile customer-info-block">
-                                                  <span class="customer-info-label">{{{ trans('email-receipt.table_customer_info.header.trx_id', [], '', $lang) }}}</span>
-                                                  <span class="customer-info-value">{{{ $transaction['id'] }}}</span>
-                                              </td>
-                                              <td class="mobile inline-mobile customer-info-block">
-                                                  <span class="customer-info-label">{{{ trans('email-receipt.table_customer_info.header.customer', [], '', $lang) }}}</span>
-                                                  <span class="customer-info-value">{{{ $customerName }}}</span>
-                                              </td>
-                                              <td class="mobile inline-mobile customer-info-block">
-                                                  <span class="customer-info-label">{{{ trans('email-receipt.table_customer_info.header.email', [], '', $lang) }}}</span>
-                                                  <span class="customer-info-value">{{{ $customerEmail }}}</span>
-                                              </td>
-                                              <td class="mobile inline-mobile customer-info-block">
-                                                  <span class="customer-info-label">{{{ trans('email-receipt.table_customer_info.header.phone', [], '', $lang) }}}</span>
-                                                  <span class="customer-info-value">{{{ $customerPhone }}}</span>
-                                              </td>
-                                          </tr>
-                                      </tbody>
-                                  </table>
-                                  <br>
-                                  <br>
-                                  <br>
-                                  <table class="no-border transaction" width="100%">
-                                      <thead class="bordered">
-                                          <tr>
-                                              <th class="transaction-item-name">{{{ trans('email-receipt.table_transaction.header.item', [], '', $lang) }}}</th>
-                                              <th class="transaction-qty">{{{ trans('email-receipt.table_transaction.header.quantity', [], '', $lang) }}}</th>
-                                              <th class="transaction-amount">{{{ trans('email-receipt.table_transaction.header.price', [], '', $lang) }}}</th>
-                                              <th class="transaction-subtotal">{{{ trans('email-receipt.table_transaction.header.subtotal', [], '', $lang) }}}</th>
-                                          </tr>
-                                      </thead>
-                                      <tbody class="transaction-items">
-                                          @foreach($transaction['items'] as $item)
-                                          <tr class="transaction-item">
-                                              <td class="transaction-item item-name">{{ $item['name'] }}
+                    <table width="100%">
+                      <tr>
+                        <td colspan="2" class="invoice-body" style="font-family:'Roboto', 'Arial', sans-serif;padding-top:10px;padding-bottom:10px;mso-table-lspace:0pt !important;mso-table-rspace:0pt !important;">
+                          <table class="no-border customer" width="100%" style="line-height:1.7em;font-size:14px;color:#222;width:100%;border:0;margin-top:30px;border-spacing:0 !important;border-collapse:collapse !important;table-layout:fixed !important;margin:0 auto !important;mso-table-lspace:0pt !important;mso-table-rspace:0pt !important;">
+                              <tbody>
+                                <tr>
+                                  <td class="mobile inline-mobile customer-info-block">
+                                    <span class="customer-info-label">{{{ trans('email-receipt.table_customer_info.header.trx_id', [], '', $lang) }}}</span>
+                                    <span class="customer-info-value">{{{ $transaction['id'] }}}</span>
+                                  </td>
+                                  <td class="mobile inline-mobile customer-info-block">
+                                    <span class="customer-info-label">{{{ trans('email-receipt.table_customer_info.header.customer', [], '', $lang) }}}</span>
+                                    <span class="customer-info-value">{{{ $customerName }}}</span>
+                                  </td>
+                                  <td class="mobile inline-mobile customer-info-block">
+                                    <span class="customer-info-label">{{{ trans('email-receipt.table_customer_info.header.email', [], '', $lang) }}}</span>
+                                    <span class="customer-info-value">{{{ $customerEmail }}}</span>
+                                  </td>
+                                  <td class="mobile inline-mobile customer-info-block">
+                                    <span class="customer-info-label">{{{ trans('email-receipt.table_customer_info.header.phone', [], '', $lang) }}}</span>
+                                    <span class="customer-info-value">{{{ $customerPhone }}}</span>
+                                  </td>
+                                </tr>
+                              </tbody>
+                          </table>
+                          <br>
+                          <br>
+                          <br>
+                          <table class="no-border transaction" width="100%">
+                            <thead class="bordered">
+                              <tr>
+                                <th class="transaction-item-name">{{{ trans('email-receipt.table_transaction.header.item', [], '', $lang) }}}</th>
+                                <th class="transaction-qty">{{{ trans('email-receipt.table_transaction.header.quantity', [], '', $lang) }}}</th>
+                                <th class="transaction-amount">{{{ trans('email-receipt.table_transaction.header.price', [], '', $lang) }}}</th>
+                                <th class="transaction-subtotal">{{{ trans('email-receipt.table_transaction.header.subtotal', [], '', $lang) }}}</th>
+                              </tr>
+                            </thead>
+                            <tbody class="transaction-items">
+                              @foreach($transaction['items'] as $item)
+                              <tr class="transaction-item">
+                                <td class="transaction-item item-name">{{ $item['name'] }}
 
-                                                  @if (isset($gameName) && ! empty($gameName))
-                                                      <p>( {{ $gameName }} )</p>
-                                                  @endif
-
-                                              </td>
-                                              <td class="transaction-item" style="text-align: center;">{{{ $item['quantity'] }}}</td>
-                                              <td class="transaction-item">{{{ $item['price'] }}}</td>
-                                              <td class="transaction-item">{{{ $item['total'] }}}</td>
-                                          </tr>
-                                          @endforeach
-                                          @foreach($transaction['discounts'] as $item)
-                                          <tr class="transaction-item">
-                                              <td class="transaction-item">{{{ trans('label.discount', [], '', $lang) }}} {{{ $item['name'] }}}</td>
-                                              <td class="transaction-item" style="text-align: center;">{{{ $item['quantity'] }}}</td>
-                                              <td class="transaction-item">{{{ $item['price'] }}}</td>
-                                              <td class="transaction-item">{{{ $item['total'] }}}</td>
-                                          </tr>
-                                          @endforeach
-                                      </tbody>
-                                      <tfoot class="transaction-footer">
-                                          <tr>
-                                              <td colspan="2" class="transaction-item transaction-total"></td>
-                                              <td class="transaction-item transaction-total"><strong>{{{ trans('email-receipt.table_transaction.footer.total', [], '', $lang) }}}</strong></td>
-                                              <td class="transaction-item transaction-total">{{{ $transaction['total'] }}}</td>
-                                          </tr>
-                                      </tfoot>
-                                  </table>
-
-                                  @if (isset($voucherData) && ! empty($voucherData))
-                                      <br>
-                                      <br>
-                                      <br>
-                                      <div class="voucher-data-label">
-                                          {{ trans('email-receipt.body.voucher_code', [], '', $lang) }}
-                                      </div>
-                                      <div class="voucher-data-container">
-                                          @foreach($voucherData as $voucherDataKey => $data)
-                                              <div class="voucher-data-item">
-                                                  {{ $voucherDataKey }}: {{ $data }}
-                                              </div>
-                                          @endforeach
-                                      </div>
+                                  @if (isset($gameName) && ! empty($gameName))
+                                      <p>( {{ $gameName }} )</p>
                                   @endif
 
-                                  <br>
-                                  <br>
-                                  <p class="help-text">
-                                      {{ trans('email-receipt.body.view_my_purchases', [], '', $lang) }}
-                                  </p>
-                                  <br>
-                                  <p class="text-center" style="font-family:'Roboto', 'Arial', sans-serif;margin:0;text-align:center;">
-                                      <a href="{{{ $myWalletUrl }}}" class="btn btn-block">{{{ trans('email-receipt.buttons.my_purchases', [], '', $lang) }}}</a>
-                                  </p>
-                                  <br>
-                                  <br>
-                                  <p class="help-text">
-                                      {{ trans('email-receipt.body.help', ['csPhone' => $cs['phone'], 'csEmail' => $cs['email']], '', $lang) }}
-                                      <br>
-                                      <br>
-                                      {{{ trans('email-receipt.body.thank_you', [], '', $lang) }}}
-                                  </p>
-                              </td>
-                          </tr>
-                      </table>
+                                </td>
+                                <td class="transaction-item" style="text-align: center;">{{{ $item['quantity'] }}}</td>
+                                <td class="transaction-item">{{{ $item['price'] }}}</td>
+                                <td class="transaction-item">{{{ $item['total'] }}}</td>
+                              </tr>
+                              @endforeach
+                              @foreach($transaction['discounts'] as $item)
+                              <tr class="transaction-item">
+                                <td class="transaction-item">{{{ trans('label.discount', [], '', $lang) }}} {{{ $item['name'] }}}</td>
+                                <td class="transaction-item" style="text-align: center;">{{{ $item['quantity'] }}}</td>
+                                <td class="transaction-item">{{{ $item['price'] }}}</td>
+                                <td class="transaction-item">{{{ $item['total'] }}}</td>
+                              </tr>
+                              @endforeach
+                            </tbody>
+                            <tfoot class="transaction-footer">
+                              <tr>
+                                <td colspan="2" class="transaction-item transaction-total"></td>
+                                <td class="transaction-item transaction-total"><strong>{{{ trans('email-receipt.table_transaction.footer.total', [], '', $lang) }}}</strong></td>
+                                <td class="transaction-item transaction-total">{{{ $transaction['total'] }}}</td>
+                              </tr>
+                            </tfoot>
+                          </table>
+
+                          @if (isset($voucherData) && ! empty($voucherData))
+                            <br>
+                            <br>
+                            <br>
+                            <div class="voucher-data-label">
+                              {{ trans('email-receipt.body.voucher_code', [], '', $lang) }}
+                            </div>
+                            <div class="voucher-data-container">
+                              @foreach($voucherData as $voucherDataKey => $data)
+                                <div class="voucher-data-item">
+                                    {{ $voucherDataKey }}: {{ $data }}
+                                </div>
+                              @endforeach
+                            </div>
+                          @endif
+
+                          <br>
+                          <br>
+                          <p class="help-text">
+                            {{ trans('email-receipt.body.view_my_purchases', [], '', $lang) }}
+                          </p>
+                          <br>
+                          <p class="text-center" style="font-family:'Roboto', 'Arial', sans-serif;margin:0;text-align:center;">
+                            <a href="{{{ $myWalletUrl }}}" class="btn btn-block">{{{ trans('email-receipt.buttons.my_purchases', [], '', $lang) }}}</a>
+                          </p>
+                          <br>
+                          <br>
+                          <p class="help-text">
+                            {{ trans('email-receipt.body.help', ['csPhone' => $cs['phone'], 'csEmail' => $cs['email']], '', $lang) }}
+                            <br>
+                            <br>
+                            {{{ trans('email-receipt.body.thank_you', [], '', $lang) }}}
+                          </p>
+                        </td>
+                      </tr>
+                    </table>
                   </td>
                 </tr>
               </table>
