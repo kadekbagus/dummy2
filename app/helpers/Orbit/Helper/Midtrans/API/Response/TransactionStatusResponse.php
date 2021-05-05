0528e7a15a98e6e4ed87762a7d406a1f4bd41533
Budi Raharja <budi@dominopos.com>
2021-05-05 10:35:33 +0800

    Update payment method on check status queue.
    Use request all() so 'sometimes' rule works for payment method.

--- app/helpers/Orbit/Helper/Midtrans/API/Response/TransactionStatusResponse.php
+++ app/helpers/Orbit/Helper/Midtrans/API/Response/TransactionStatusResponse.php
@@ -172,6 +172,23 @@ class TransactionStatusResponse
     }
 
     /**
+     * Determine if current transaction was paid by specific payment method(s).
+     *
+     * @return bool
+     */
+    public function paidWith($method = [])
+    {
+        if (! empty($this->data)
+            && isset($this->data->payment_type)
+            && ! empty($method)
+        ) {
+            return in_array($this->data->payment_type, $method);
+        }
+
+        return false;
+    }
+
+    /**
      * Map midtrans response to our internal payment status.
      * This method MUST BE synced to Frontend transaction status/midtrans notification response mapper.
      *
