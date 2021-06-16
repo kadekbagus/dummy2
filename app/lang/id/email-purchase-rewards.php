335abd6abc0beac4bf899c24b7551113c5033e54
Budi Raharja <budi@dominopos.com>
2021-06-16 12:17:51 +0800

    Show auto-issued free coupons on receipt email.

--- app/lang/id/email-purchase-rewards.php
+++ app/lang/id/email-purchase-rewards.php
@@ -4,6 +4,6 @@ return [
     'coupon' => [
         'heading' => 'Kupon Gratis Untuk Anda',
         'section_1' => 'Anda mendapatkan :rewardsCount kupon gratis dari Gotomalls. Temukan dan gunakan melalui laman Dompet Saya/Kupon.',
-        'btn_my_coupon' => 'Dompet Saya',
+        'btn_my_wallet' => 'Kupon Saya',
     ],
 ];
--- app/views/emails/components/purchase-rewards-coupon.blade.php
+++ app/views/emails/components/purchase-rewards-coupon.blade.php
@@ -1,20 +1,45 @@
-<h2>
-    {!! trans('email-purchase-rewards.coupon.heading', [], '', $lang) !!}
-</h2>
+              <table width="640" class="container mobile-full-width">
+                <tr>
+                  <td class="rewards-container" align="center">
+                    <table width="600" class="container text-center" cellspacing="0" cellpadding="0" border="0">
+                      <tr>
+                        <td class="text-center">
+                          <h2 class="rewards-heading">
+                            {{{ trans('email-purchase-rewards.coupon.heading', [], '', $lang) }}}
+                          </h2>
 
-<p>
-    {!! trans('email-purchase-rewards.coupon.section_1', ['rewardsCount' => count($rewards)], '', $lang ) !!}
-</p>
+                          <p class="rewards-section greeting-text">
+                            {{{ trans('email-purchase-rewards.coupon.section_1', ['rewardsCount' => count($rewards)], '', $lang ) }}}
+                          </p>
+                          <br>
 
-<a href="{!! $redeemUrl !!}" class="btn btn-light" role="button">
-    {!! trans('email-purchase-rewards.coupon.btn_my_wallet', [], '', $lang) !!}
-</a>
+                          <a href="http://gotomalls.budi/my/coupons" class="btn btn-light" role="button">
+                              {{{ trans('email-purchase-rewards.coupon.btn_my_wallet', [], '', $lang) }}}
+                          </a>
+                        </td>
+                      </tr>
 
-<div>
-    @foreach($rewards as $reward)
-        <div>
-            <img src="{!! $reward['image_url'] !!}" alt="{!! $reward['name'] !!}">
-            <span>{!! $reward['name'] !!}</span>
-        </div>
-    @endforeach
-</div>
+                      <tr>
+                        <td>
+                          <br>
+                          <br>
+
+                          <div class="rewards-list-container">
+                            @foreach(array_chunk($rewards, 3) as $rewardChunks)
+                              @foreach($rewardChunks as $reward)
+                                <div class="rewards-item text-left">
+                                  <img class="rewards-item-img" src="{{ $reward['image_url'] }}">
+                                  <div class="rewards-item-name">
+                                    {{{ $reward['name'] }}}
+                                  </div>
+                                </div>
+                              @endforeach
+                              <div class="desktop-spacer"></div>
+                            @endforeach
+                          </div>
+                        </td>
+                      </tr>
+                    </table>
+                  </td>
+                </tr>
+              </table>
