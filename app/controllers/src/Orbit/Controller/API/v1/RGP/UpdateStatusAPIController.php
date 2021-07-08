63a5a221d511217273286e213ed0388936e24824
Ahmad <ahmad@dominopos.com>
2021-07-08 16:56:19 +0800

    Add more status to update transaction status from RGP

--- app/controllers/src/Orbit/Controller/API/v1/RGP/UpdateStatusAPIController.php
+++ app/controllers/src/Orbit/Controller/API/v1/RGP/UpdateStatusAPIController.php
@@ -74,7 +74,7 @@ class UpdateStatusAPIController extends ControllerAPI
             // query, use builder so that updated_at is not touched
             DB::table('payment_transactions')
                 ->where('payment_transaction_id', $transactionId)
-                ->whereIn('status', ['success_no_pulsa_failed', 'success_no_product_failed'])
+                ->whereIn('status', ['success_no_pulsa_failed', 'success_no_product_failed', 'success_no_product'])
                 ->update(['status' => 'success']);
 
         } catch (Exception $e) {
