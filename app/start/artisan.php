1d4c615dac171909524dde36131e367aa2699d85
Ahmad <ahmad@dominopos.com>
2021-09-24 10:46:48 +0700

    Add new command to artisan

--- app/start/artisan.php
+++ app/start/artisan.php
@@ -368,3 +368,6 @@ Artisan::add(new UpdateBPPUserMerchantsCommand);
 // Update pulsa status
 Artisan::add(new PulsaStatusUpdateCommand);
 
+// Update digital product status
+Artisan::add(new DigitalProductStatusUpdateCommand);
+
