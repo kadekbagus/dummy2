ed69556e298890d7cf1142760cd35d81293de90b
Budi Raharja <budi@dominopos.com>
2021-12-03 11:39:23 +0800

    Add pdam bill pending and receipt notif.
    Move some repeated methods to base PayBillQueue.

---
+++ app/notifications/Orbit/Notifications/DigitalProduct/WaterBill/ReceiptNotification.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace Orbit\Notifications\DigitalProduct\WaterBill;
+
+use Orbit\Notifications\DigitalProduct\BillNotification;
+
+/**
+ * electricity bill receipt notification.
+ *
+ * @author Budi <budi@gotomalls.com>
+ */
+class ReceiptNotification extends BillNotification
+{
+    protected $signature = 'electricity-bill-receipt-notification';
+
+    public function getEmailTemplates()
+    {
+        return [
+            'html' => 'emails.digital-product.electricity-bill.receipt',
+            'text' => 'emails.digital-product.electricity-bill.receipt-text',
+        ];
+    }
+
+    public function getEmailSubject()
+    {
+        return trans('email-receipt.subject', [], '', 'id');
+    }
+
+    protected function prepareEmailData($data = [])
+    {
+        return array_merge(parent::prepareEmailData($data), [
+            'myWalletUrl' => $this->getMyPurchasesUrl('/bills'),
+            'bill' => $this->getBillInformation(),
+        ]);
+    }
+}
--- app/queue/Orbit/Queue/Bill/PayElectricityBillQueue.php
+++ app/queue/Orbit/Queue/Bill/PayElectricityBillQueue.php
@@ -2,15 +2,9 @@
 
 namespace Orbit\Queue\Bill;
 
-use Config;
-use Orbit\Controller\API\v1\Pub\Purchase\Activities\PurchaseFailedProductActivity;
-use Orbit\Controller\API\v1\Pub\Purchase\Activities\PurchaseSuccessActivity;
 use Orbit\Helper\MCash\API\Bill;
-use Orbit\Notifications\DigitalProduct\CustomerDigitalProductNotAvailableNotification;
-use Orbit\Notifications\DigitalProduct\DigitalProductNotAvailableNotification;
 use Orbit\Notifications\DigitalProduct\ElectricityBill\ReceiptNotification;
 use Orbit\Queue\Bill\PayBillQueue;
-use User;
 
 /**
  * A job to pay electricity bill after payment completed.
@@ -26,34 +20,5 @@ class PayElectricityBillQueue extends PayBillQueue
     protected function notifyReceipt($payment)
     {
         $payment->user->notify(new ReceiptNotification($payment));
-    }
-
-    protected function notifyFailed($payment, $e)
-    {
-        $adminEmails = Config::get('orbit.transaction.notify_emails', ['developer@dominopos.com']);
-
-        // Notify admin for this failure.
-        foreach($adminEmails as $email) {
-            $admin              = new User;
-            $admin->email       = $email;
-            $admin->notify(new DigitalProductNotAvailableNotification($payment, $e->getMessage()));
-        }
-
-        // Notify customer that coupon is not available.
-        $payment->user->notify(new CustomerDigitalProductNotAvailableNotification($payment));
-    }
-
-    protected function recordSuccessActivity($payment)
-    {
-        $payment->user->activity(new PurchaseSuccessActivity($payment, $this->objectType));
-    }
-
-    protected function recordFailedActivity($payment, $notes)
-    {
-        $payment->user->activity(new PurchaseFailedProductActivity(
-            $payment,
-            $this->objectType,
-            $notes
-        ));
     }
 }
