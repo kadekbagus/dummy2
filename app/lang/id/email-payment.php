71471d96211f48a3e88d38ecdc7c4f1dac0f28cb
Budi Raharja <budi@dominopos.com>
2021-12-06 10:31:26 +0800

    Add more bill type translation for notifications.

--- app/lang/id/email-payment.php
+++ app/lang/id/email-payment.php
@@ -8,5 +8,10 @@ return [
         'data_plan' => 'Paket Data',
         'game_voucher' => 'Voucher Game',
         'electricity' => 'Token Listrik',
+        'electricity_bill' => 'Tagihan Listrik',
+        'pdam_bill' => 'Tagihan PDAM',
+        'pbb_tax' => 'Tagihan Pajak Bumi dan Bangunan',
+        'bpjs_bill' => 'Tagihan BPJS',
+        'internet_provider_bill' => 'Tagihan Internet',
     ],
 ];
