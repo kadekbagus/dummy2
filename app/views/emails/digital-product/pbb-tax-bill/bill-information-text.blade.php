dab584f55347a7113c9f0d730e096191a1fc83b0
Budi Raharja <budi@dominopos.com>
2021-12-08 16:56:44 +0800

    pbbtaxbill: fix undefined properties.

--- app/views/emails/digital-product/pbb-tax-bill/bill-information-text.blade.php
+++ app/views/emails/digital-product/pbb-tax-bill/bill-information-text.blade.php
@@ -3,8 +3,5 @@
 {{ trans('email-bill.labels.billing_id', [], '', $lang) }}: {{ $bill->billing_id }}]
 {{ trans('email-bill.labels.billing_name', [], '', $lang) }}: {{ $bill->customer_name }}
 {{ trans('email-bill.labels.water_bill.periode', [], '', $lang) }}: {{ $bill->period }}
-{{ trans('email-bill.labels.water_bill.meter_start', [], '', $lang) }}: {{ $bill->meter_start }}
-{{ trans('email-bill.labels.water_bill.meter_end', [], '', $lang) }}: {{ $bill->meter_end }}
-{{ trans('email-bill.labels.water_bill.usage', [], '', $lang) }}: {{ $bill->usage }}
 {{ trans('email-bill.labels.billing_amount', [], '', $lang) }}: {{ $bill->amount }}
 @endif
